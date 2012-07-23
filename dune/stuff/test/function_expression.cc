9eb43c235bf8b0944bb0bd7a6168abb770c7317b
Rene Milk <rene.milk@uni-muenster.de>
2012-07-23 21:41:54 +0200

    [tests] rename function expression source/test

--- dune/stuff/test/function_expression.cc
+++ dune/stuff/test/function_expression.cc
@@ -1,124 +1,141 @@
+/**
+  \file   examples/grid/provider.cc
+  \brief  Demonstrates the capabilities of some Dune::RB::Grid::Providers.
+  **/
+
+#ifdef HAVE_CMAKE_CONFIG
 #include "cmake_config.h"
+#elif defined(HAVE_CONFIG_H)
+#include <config.h>
+#endif // ifdef HAVE_CMAKE_CONFIG
 
-#include <cstdio>
-#if defined(USE_PARDG_ODE_SOLVER) && defined(USE_BFG_CG_SCHEME)
-#warning("USE_PARDG_ODE_SOLVER enabled, might conflict with custom solvers")
-#endif
-
-// the adaption manager might be troublesome with certain gridparts/spaces, so we needed a easy way to disable it
-#ifndef ENABLE_ADAPTIVE
-#define ENABLE_ADAPTIVE 1
-#endif
-
-#if defined(UGGRID) && defined(DEBUG)
-#warning("UGGRID in debug mode is likely to produce a segfault")
-#endif
-
-#if !defined(POLORDER)
-#define POLORDER 0
-#warning("using default polorder 0 for all spaces")
-#endif // if !defined (POLORDER)
-
-#if !defined(PRESSURE_POLORDER)
-#define PRESSURE_POLORDER POLORDER
-#endif
-
-#if !defined(VELOCITY_POLORDER)
-#define VELOCITY_POLORDER POLORDER
-#endif
-
-#if !defined(TESTCASE)
-#define TESTCASE TestCase3D
-#endif
-
-#define TESTCASE_NAME "TESTCASE"
-
-#if ((defined(SGRID) || defined(ALUGRID_SIMPLEX) || defined(ALUGRID_CUBE)) && (GRIDDIM == 3)) || defined(UGGRID)       \
-    || defined(YASPGRID)
-// this is no mistake, ALU is indeed only incompatible in 3d
-#define OLD_DUNE_GRID_VERSION
-#endif // if ( ( defined (SGRID) || defined (ALUGRID_SIMPLEX) || defined (ALUGRID_CUBE ) ) && (GRIDDIM == 3 ) ) ||
-// defined (UGGRID) || defined (YASPGRID)
-
-#define USE_GRPAE_VISUALISATION (HAVE_GRAPE && !defined(AORTA_PROBLEM))
-
-#include <vector>
-#include <string>
-
-#include <iostream>
-#include <cmath>
-#include <dune/fem/misc/mpimanager.hh> // An initializer of MPI
-#include <dune/common/exceptions.hh> // We use exceptions
-
-// !ATTENTION: undef's GRIDDIM
-
-#include <dune/grid/io/file/dgfparser/dgfgridtype.hh> // for the grid
-
-#include <dune/fem/solver/oemsolver/oemsolver.hh>
-#include <dune/fem/space/dgspace.hh>
-#include <dune/fem/space/combinedspace.hh>
-#include <dune/fem/space/dgspace/dgadaptiveleafgridpart.hh>
-#include <dune/fem/function/adaptivefunction.hh> // for AdaptiveDiscreteFunction
-#include <dune/fem/operator/projection/l2projection.hh>
-
-#include <dune/stuff/printing.hh>
-#include <dune/stuff/misc.hh>
-#include <dune/stuff/logging.hh>
-#include <dune/stuff/parametercontainer.hh>
-#include <dune/stuff/profiler.hh>
-#include <dune/stuff/datawriter.hh>
-#include <dune/stuff/customprojection.hh>
-#include <dune/common/collectivecommunication.hh>
-
-#include <stdio.h>
-#include <dune/fem/misc/mpimanager.hh> // An initializer of MPI
+//#if HAVE_DUNE_FEM
+#include <dune/fem/misc/mpimanager.hh>
 #include <dune/fem/space/fvspace/fvspace.hh>
-#include <dune/stuff/parametercontainer.hh>
-#include <dune/stuff/misc.hh>
-#include <dune/stuff/runtimefunction.hh>
-#include <dune/stuff/expressions/mathexpr.h>
-// do whatever you like to this file to test out simple and small stuff
+#include <dune/fem/space/dgspace.hh>
+#include <dune/fem/gridpart/adaptiveleafgridpart.hh>
+#include <dune/fem/function/adaptivefunction.hh>
+#include <dune/fem/operator/projection/l2projection.hh>
+#include <dune/fem/io/file/datawriter.hh>
+#include <dune/stuff/fem/customprojection.hh>
+//#endif
 
+// system
+#include <iostream>
+#include <fstream>
+
+// boost
+#include <boost/filesystem.hpp>
+
+
+#include <dune/common/mpihelper.hh>
+#include <dune/common/parametertreeparser.hh>
+#include <dune/common/fvector.hh>
+
+#include <dune/stuff/function/expression.hh>
+
+/**
+  \brief      Creates a parameter file if it does not exist.
+
+              Nothing is done if the file already exists. If not, a parameter file will be created with all neccessary
+              keys and values.
+  \param[in]  filename
+              (Relative) path to the file.
+  **/
+void ensureParamFile(std::string filename)
+{
+  // only write param file if there is none
+  if (!boost::filesystem::exists(filename)) {
+    std::ofstream file;
+    file.open(filename);
+    file << "[rb.examples.parameter.function]" << std::endl;
+    file << "variable = mu" << std::endl;
+    file << "expression.0 = sin(mu[0]) + 2" << std::endl;
+    file << "expression.1 = mu[0]^2 + mu[1]" << std::endl;
+    file << "expression.2 = 0.5 * mu[2]" << std::endl;
+    file << std::endl;
+    file.close();
+  } // only write param file if there is none
+} // void ensureParamFile()
+
+/**
+  \brief      Fills a Dune::ParameterTree given a parameter file or command line arguments.
+  \param[in]  argc
+              From \c main()
+  \param[in]  argv
+              From \c main()
+  \param[out] paramTree
+              The Dune::ParameterTree that is to be filled.
+  **/
+void initParamTree(int argc, char** argv, Dune::ParameterTree& paramTree)
+{
+  if (argc == 1) {
+    Dune::ParameterTreeParser::readINITree("function.param", paramTree);
+  } else if (argc == 2) {
+    Dune::ParameterTreeParser::readINITree(argv[1], paramTree);
+  } else {
+    Dune::ParameterTreeParser::readOptions(argc, argv, paramTree);
+  }
+  if (paramTree.hasKey("paramfile")) {
+    Dune::ParameterTreeParser::readINITree(paramTree.get<std::string>("paramfile"), paramTree, false);
+  }
+}
+
+/**
+  \brief  Main routine.
+  **/
 int main(int argc, char** argv)
 {
-  Dune::MPIManager::initialize(argc, argv);
-  if (!(Parameters().ReadCommandLine(argc, argv))) {
-    return 1;
+  try {
+    // mpi
+    Dune::MPIHelper::instance(argc, argv);
+    // parameter
+    ensureParamFile("function.param");
+    Dune::ParameterTree paramTree;
+    initParamTree(argc, argv, paramTree);
+    typedef Dune::GridSelector::GridType GridType;
+    // parameter domain
+    const int dimDomain = GridType::dimensionworld;
+    typedef Dune::FieldVector<double, dimDomain> DomainType;
+    DomainType mu(1.0);
+    const int dimRange = 2;
+    typedef Dune::FieldVector<double, dimRange> RangeType;
+    RangeType result(0.0);
+    // parameter function
+    std::cout << "creating Dune::RB::Paramter::Function< double, " << dimDomain << ", double, " << dimRange << " >... ";
+    typedef Dune::Stuff::Function::Expression<double, dimDomain, double, dimRange> ParameterFunctionType;
+    ParameterFunctionType parameterFunction(paramTree.sub("rb.examples.parameter.function"));
+    std::cout << "done" << std::endl;
+    std::cout << "variable is:     " << parameterFunction.variable() << std::endl;
+    std::cout << "expressions are: " << parameterFunction.expression(0) << std::endl;
+    for (int i = 1; i < dimRange; ++i) {
+      std::cout << "                 " << parameterFunction.expression(i) << std::endl;
+    }
+    parameterFunction.evaluate(mu, result);
+    std::cout << "evaluate(" << mu << ") = " << result << std::endl;
+    mu = 0.5;
+    parameterFunction.evaluate(mu, result);
+    std::cout << "evaluate(" << mu << ") = " << result << std::endl;
+
+    //#if HAVE_DUNE_FEM
+    Dune::GridPtr<GridType> gridPtr(paramTree.get("dgf_file", "dummy"));
+    typedef Dune::AdaptiveLeafGridPart<GridType> GridPartType;
+    GridPartType gridPart_(*gridPtr);
+
+    typedef ParameterFunctionType::FunctionSpaceType FSpace;
+    typedef Dune::DiscontinuousGalerkinSpace<FSpace, GridPartType, 1> DiscreteFunctionSpaceType;
+    typedef Dune::AdaptiveDiscreteFunction<DiscreteFunctionSpaceType> DiscreteFunctionType;
+    DiscreteFunctionSpaceType disc_space(gridPart_);
+    DiscreteFunctionType rf_disc("rf", disc_space);
+    typedef Dune::tuple<const DiscreteFunctionType*> OutputTupleType;
+    typedef Dune::DataWriter<GridPartType::GridType, OutputTupleType> DataWriterType;
+    Dune::Stuff::Fem::BetterL2Projection::project(parameterFunction, rf_disc);
+    OutputTupleType out(&rf_disc);
+    DataWriterType dt(gridPart_.grid(), out);
+    dt.write();
+    //#endif
+  } catch (Dune::Exception& e) {
+    std::cout << e.what() << std::endl;
   }
-  const bool useLogger = true;
-  Logger().Create(Parameters().getParam("loglevel", 62, useLogger),
-                  Parameters().getParam("logfile", std::string("dune_stokes"), useLogger),
-                  Parameters().getParam("fem.io.logdir", std::string(), useLogger));
-
-  typedef Dune::FunctionSpace<double, double, 2, 2> FSpace;
-  FSpace fSpace;
-  typedef Stuff::RuntimeFunction<FSpace> RF;
-  RF rf("func", fSpace);
-
-  /* ********************************************************************** *
-    * initialize the grid                                                    *
-    * ********************************************************************** */
-  Logging::LogStream& infoStream  = Logger().Info();
-  Logging::LogStream& debugStream = Logger().Dbg();
-  infoStream << "\n- initialising grid" << std::endl;
-  const int gridDim = GridType::dimensionworld;
-  Dune::GridPtr<GridType> gridPtr(Parameters().DgfFilename(gridDim));
-  int refine_level = (Parameters().getParam("minref", 0)) * Dune::DGFGridInfo<GridType>::refineStepsForHalf();
-  gridPtr->globalRefine(refine_level);
-
-  typedef Dune::AdaptiveLeafGridPart<GridType> GridPartType;
-  GridPartType gridPart_(*gridPtr);
-
-  typedef Dune::DiscontinuousGalerkinSpace<FSpace, GridPartType, 1> DiscreteFunctionSpaceType;
-  typedef Dune::AdaptiveDiscreteFunction<DiscreteFunctionSpaceType> DiscreteFunctionType;
-  DiscreteFunctionSpaceType disc_space(gridPart_);
-  DiscreteFunctionType rf_disc("rf", disc_space);
-  typedef Dune::Tuple<const DiscreteFunctionType*> OutputTupleType;
-  typedef Dune::DataWriter<GridPartType::GridType, OutputTupleType> DataWriterType;
-  Dune::L2Projection<double, double, RF, DiscreteFunctionType>()(rf, rf_disc);
-  OutputTupleType out(&rf_disc);
-  DataWriterType dt(gridPart_.grid(), "dummy", out, 0, 0);
-  dt.write(0.0, 0);
-
   return 0;
-} // main
+}
