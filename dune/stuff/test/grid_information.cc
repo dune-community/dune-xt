df3b3d26cd09f7d7de793bab02d2b2f0354976af
Rene Milk <rene.milk@uni-muenster.de>
2012-08-16 16:53:54 +0200

    [tests] grid::information test for up to 10-d yasp grids

--- dune/stuff/test/grid_information.cc
+++ dune/stuff/test/grid_information.cc
@@ -2,8 +2,11 @@
 
 #include <dune/stuff/grid/information.hh>
 #include <dune/stuff/grid/provider/cube.hh>
+#include <dune/stuff/common/math.hh>
+#include <dune/stuff/common/logstreams.hh>
 
 using namespace Dune::Stuff;
+using namespace Dune::Stuff::Grid::Information;
 using namespace std;
 
 template <int i>
@@ -12,27 +15,55 @@ struct Int
   static const int value = i;
 };
 
-typedef testing::Types<Int<1>, Int<2>, Int<3>> GridDims;
+typedef testing::Types<Int<1>, Int<2>, Int<3>, Int<4>, Int<5>, Int<6>, Int<7>, Int<8>, Int<9>, Int<10>> GridDims;
 
 template <class T>
 struct GridInfoTest : public ::testing::Test
 {
-  static const int dim = T::value;
-  typedef Dune::YaspGrid<dim, dim> GridType;
+  static const int griddim = T::value;
+  static const int level   = 1;
+  typedef Dune::YaspGrid<griddim> GridType;
   Dune::shared_ptr<GridType> gridPtr;
   GridInfoTest()
-    : gridPtr(Grid::Provider::UnitCube(/*level=*/5).gridPtr())
+    : gridPtr(Grid::Provider::UnitCube<GridType>(level).gridPtr())
   {
   }
 
-  void checkDimension()
+  void check()
   {
+    const Dimensions<GridType> dim(*gridPtr);
+    const auto gv      = gridPtr->leafView();
+    const int entities = gv.size(0);
+    EXPECT_DOUBLE_EQ(1.0 / double(entities), dim.entity_volume.min());
+    EXPECT_DOUBLE_EQ(dim.entity_volume.min(), dim.entity_volume.max());
+    EXPECT_DOUBLE_EQ(dim.entity_volume.min(), dim.entity_volume.average());
+    EXPECT_DOUBLE_EQ(1.0, dim.volumeRelation());
+    const auto& dl = dim.coord_limits;
+    for (int i : Common::Math::range(griddim)) {
+      EXPECT_DOUBLE_EQ(dl[i].max(), 1.0);
+      EXPECT_DOUBLE_EQ(dl[i].min(), 0.0);
+      EXPECT_DOUBLE_EQ(dl[i].average(), 0.5);
+    }
+    const Statistics st(gv);
+    const int line = std::pow(2, level);
+    EXPECT_EQ(std::pow(line, griddim - 1) * 2 * griddim, st.numberOfBoundaryIntersections);
+    EXPECT_EQ(entities * (2 * griddim), st.numberOfIntersections);
+    EXPECT_EQ(st.numberOfIntersections - st.numberOfBoundaryIntersections, st.numberOfInnerIntersections);
+    EXPECT_EQ(griddim * 2, maxNumberOfNeighbors(gv));
+  }
+
+  void print(std::ostream& out)
+  {
+    const auto& gv = gridPtr->leafView();
+    ::print(gv, out);
   }
 };
 
 TYPED_TEST_CASE(GridInfoTest, GridDims);
-TYPED_TEST(GridInfoTest, Dimension)
+TYPED_TEST(GridInfoTest, Misc)
 {
+  this->check();
+  this->print(Common::dev_null);
 }
 
 // TEST_F(GridInfoTest, Dimension) {
