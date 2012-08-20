d524c8a0dbd19cba5b9e513353d0273bdc85ab97
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-08-20 12:55:10 +0200

    [grid.boundaryinfo] added AllDirichlet and IdBased

---
+++ dune/stuff/grid/boundaryinfo.hh
@@ -0,0 +1,73 @@
+
+#ifndef DUNE_STUFF_GRID_BOUNDARYINFO_HH
+#define DUNE_STUFF_GRID_BOUNDARYINFO_HH
+
+// system
+#include <map>
+#include <set>
+
+// dune-common
+#include <dune/common/shared_ptr.hh>
+
+namespace Dune {
+
+namespace Stuff {
+
+namespace Grid {
+
+namespace BoundaryInfo {
+
+class AllDirichlet
+{
+public:
+  template <class IntersectionType>
+  bool dirichlet(const IntersectionType& intersection) const
+  {
+    return true;
+  }
+}; // class AllDirichlet
+
+class IdBased
+{
+public:
+  typedef int IdType;
+
+  typedef std::set<IdType> IdSetType;
+
+  typedef std::map<std::string, IdSetType> IdSetMapType;
+
+  IdBased(const Dune::shared_ptr<const IdSetMapType> boundaryInfoMap)
+    : boundaryInfoMap_(boundaryInfoMap)
+    , hasDirichlet_(boundaryInfoMap_->find("dirichlet") != boundaryInfoMap_->end())
+  {
+  }
+
+  template <class IntersectionType>
+  bool dirichlet(const IntersectionType& intersection) const
+  {
+    if (hasDirichlet_ && intersection.boundary()) {
+      // get boundary id
+      const IdType boundaryId = intersection.boundaryId();
+      // get set of dirichlet ids (has to be found, otherwise hasDirichlet_ would be false)
+      const typename IdSetMapType::const_iterator result = boundaryInfoMap_->find("dirichlet");
+      assert(result != boundaryInfoMap_->end());
+      const IdSetType& idSet = result->second;
+      return (idSet.count(boundaryId) > 0);
+    } else
+      return false;
+  } // bool dirichlet(const IntersectionType& intersection) const
+
+private:
+  const Dune::shared_ptr<const IdSetMapType> boundaryInfoMap_;
+  const bool hasDirichlet_;
+}; // class IdBased
+
+} // namespace BoundaryInfo
+
+} // namespace Grid
+
+} // namespace Stuff
+
+} // namespace Dune
+
+#endif // DUNE_STUFF_GRID_BOUNDARYINFO_HH
