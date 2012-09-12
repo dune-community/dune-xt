f213882c9ca644d68205798147857787d20cbf87
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-09-12 12:19:05 +0200

    [grid.boundaryinfo] added neumann

--- dune/stuff/grid/boundaryinfo.hh
+++ dune/stuff/grid/boundaryinfo.hh
@@ -36,6 +36,25 @@ public:
   }
 }; // class AllDirichlet
 
+class AllNeumann
+{
+public:
+  template <class IntersectionType>
+  bool dirichlet(const IntersectionType& intersection) const
+  {
+    return false;
+  }
+
+  template <class IntersectionType>
+  bool neumann(const IntersectionType& intersection) const
+  {
+    if (intersection.boundary())
+      return true;
+    else
+      return false;
+  }
+}; // class AllNeumann
+
 class IdBased
 {
 public:
@@ -48,6 +67,7 @@ public:
   IdBased(const Dune::shared_ptr<const IdSetMapType> boundaryInfoMap)
     : boundaryInfoMap_(boundaryInfoMap)
     , hasDirichlet_(boundaryInfoMap_->find("dirichlet") != boundaryInfoMap_->end())
+    , hasNeumann_(boundaryInfoMap_->find("neumann") != boundaryInfoMap_->end())
   {
   }
 
@@ -66,9 +86,24 @@ public:
       return false;
   } // bool dirichlet(const IntersectionType& intersection) const
 
+  template <class IntersectionType>
+  bool neumann(const IntersectionType& intersection) const
+  {
+    if (hasNeumann_ && intersection.boundary()) {
+      // get boundary id
+      const IdType boundaryId = intersection.boundaryId();
+      // get set of neumann ids (has to be found, otherwise hasNeumann_ would be false)
+      const typename IdSetMapType::const_iterator result = boundaryInfoMap_->find("neumann");
+      assert(result != boundaryInfoMap_->end());
+      const IdSetType& idSet = result->second;
+      return (idSet.count(boundaryId) > 0);
+    } else
+      return false;
+  } // bool neumann(const IntersectionType& intersection) const
 private:
   const Dune::shared_ptr<const IdSetMapType> boundaryInfoMap_;
   const bool hasDirichlet_;
+  const bool hasNeumann_;
 }; // class IdBased
 
 } // namespace BoundaryInfo
