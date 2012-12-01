c9f516a8a8681dd5da9934d0c2655b692d661ceb
Felix Albrecht <mail@felixalbrecht.de>
2012-12-01 17:35:29 +0100

    [grid.provider.gmsh] removed gridPtr(), changed constructor, added createFromParamTree()

--- dune/stuff/grid/provider/gmsh.hh
+++ dune/stuff/grid/provider/gmsh.hh
@@ -7,32 +7,28 @@
 #include "config.h"
 #endif // ifdef HAVE_CMAKE_CONFIG
 
-// system
+#if HAVE_DUNE_GRID
+
 #include <sstream>
 #include <type_traits>
+
 #include <boost/assign/list_of.hpp>
 
-// dune-common
 #include <dune/common/parametertree.hh>
 #include <dune/common/shared_ptr.hh>
 #include <dune/common/exceptions.hh>
 #include <dune/common/fvector.hh>
 
-// dune-grid
 #include <dune/grid/utility/structuredgridfactory.hh>
 #include <dune/grid/yaspgrid.hh>
 #ifdef HAVE_ALUGRID
 #include <dune/grid/alugrid.hh>
 #endif
 #include <dune/grid/sgrid.hh>
-//#include <dune/grid/common/mcmgmapper.hh>
-//#include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/grid/io/file/gmshreader.hh>
 
-// dune-stuff
 #include <dune/stuff/common/parameter/tree.hh>
 
-// local
 #include "interface.hh"
 
 namespace Dune {
@@ -64,61 +60,76 @@ public:
   //! Type of the grids coordinates.
   typedef typename BaseType::CoordinateType CoordinateType;
 
-  Gmsh(const Dune::Stuff::Common::ExtendedParameterTree& paramTree)
-  {
-    dune_static_assert(!(Dune::is_same<GridType, Dune::YaspGrid<dim>>::value), "GmshReader doesn't work with YaspGrid");
-    dune_static_assert(!(Dune::is_same<GridType, Dune::SGrid<2, 2>>::value), "GmshReader doesn't work with SGrid");
-    paramTree.assert("mshfile");
-    const std::string filename = paramTree.get("mshfile", "sample.msh");
-    // read gmshfile
-    grid_ = Dune::shared_ptr<GridType>(GmshReader<GridType>::read(filename));
-  }
-
   //! Unique identifier: \c stuff.grid.provider.gmsh
   static const std::string id()
   {
     return BaseType::id() + ".gmsh";
   }
 
-  /**
-    \brief  Provides access to the created grid.
-    \return Reference to the grid.
-    **/
-  virtual GridType& grid()
+  Gmsh(const std::string filename)
   {
-    return *grid_;
+    dune_static_assert(!(Dune::is_same<GridType, Dune::YaspGrid<dim>>::value),
+                       "GmshReader does not work with YaspGrid!");
+    dune_static_assert(!(Dune::is_same<GridType, Dune::SGrid<2, 2>>::value), "GmshReader does not work with SGrid!");
+    grid_ = Dune::shared_ptr<GridType>(GmshReader<GridType>::read(filename));
   }
 
-  /**
-   *  \brief  Provides const access to the created grid.
-   *  \return Reference to the grid.
-   **/
-  virtual const GridType& grid() const
+  Gmsh(ThisType& other)
+    : grid_(other.grid_)
   {
-    return *grid_;
+  }
+
+  Gmsh(const ThisType& other)
+    : grid_(other.grid_)
+  {
+  }
+
+  static ThisType createFromParamTree(const Dune::ParameterTree& paramTree, const std::string subName = id())
+  {
+    // get correct paramTree
+    Dune::Stuff::Common::ExtendedParameterTree extendedParamTree;
+    if (paramTree.hasSub(subName))
+      extendedParamTree = paramTree.sub(subName);
+    else
+      extendedParamTree = paramTree;
+    // create and return
+    if (!extendedParamTree.hasKey("filename"))
+      DUNE_THROW(Dune::RangeError,
+                 "\nMissing key 'filename' in the following Dune::ParameterTree:\n"
+                     << extendedParamTree.reportString("  "));
+    const std::string filename = extendedParamTree.get("filename", "meaningless_default_value");
+    return Gmsh(filename);
+  }
+
+  ThisType& operator=(ThisType& other)
+  {
+    if (this != &other) {
+      grid_ = other.grid();
+    }
+    return this;
   }
 
   //! access to shared ptr
-  virtual Dune::shared_ptr<GridType> gridPtr()
+  virtual Dune::shared_ptr<GridType> grid()
   {
     return grid_;
   }
 
   //! const access to shared ptr
-  virtual const Dune::shared_ptr<const GridType> gridPtr() const
+  virtual const Dune::shared_ptr<const GridType> grid() const
   {
     return grid_;
   }
 
 private:
-  const std::string filename_;
   Dune::shared_ptr<GridType> grid_;
 }; // class Gmsh
-
 
 } // namespace Provider
 } // namespace Grid
 } // namespace Stuff
 } // namespace Dune
+
+#endif // HAVE_DUNE_GRID
 
 #endif // DUNE_STUFF_GRID_PROVIDER_GMSH_HH
