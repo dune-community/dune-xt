05a31b55e518f7614440a088967989939a7c1288
Felix Albrecht <mail@felixalbrecht.de>
2012-12-04 17:07:10 +0100

    [grid.provider] moved create() out of interface.hh

---
+++ dune/stuff/grid/provider.hh
@@ -0,0 +1,52 @@
+#ifndef DUNE_STUFF_GRID_PROVIDER_HH
+#define DUNE_STUFF_GRID_PROVIDER_HH
+
+#ifdef HAVE_CMAKE_CONFIG
+#include "cmake_config.h"
+#else
+#include "config.h"
+#endif // ifdef HAVE_CMAKE_CONFIG
+
+#if HAVE_DUNE_GRID
+
+#include "provider/interface.hh"
+#include "provider/cube.hh"
+#if HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
+#include "provider/gmsh.hh"
+#endif // HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
+
+namespace Dune {
+namespace Stuff {
+namespace Grid {
+namespace Provider {
+
+#if defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
+template <class GridType = Dune::GridSelector::GridType>
+#else // defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
+template <class GridType = Dune::SGrid<2, 2>>
+#endif // defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
+Interface<GridType>* create(const std::string& type, const Dune::ParameterTree paramTree = Dune::ParameterTree())
+{
+  // choose provider
+  if (type == "stuff.grid.provider.cube") {
+    typedef Dune::Stuff::Grid::Provider::Cube<GridType> CubeProviderType;
+    CubeProviderType* cubeProvider = new CubeProviderType(CubeProviderType::createFromParamTree(paramTree));
+    return cubeProvider;
+#if HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
+  } else if (type == "stuff.grid.provider.gmsh") {
+    typedef Dune::Stuff::Grid::Provider::Gmsh<GridType> GmshProviderType;
+    GmshProviderType* gmshProvider = new GmshProviderType(GmshProviderType::createFromParamTree(paramTree));
+    return gmshProvider;
+#endif // HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
+  } else
+    DUNE_THROW(Dune::RangeError, "\nERROR: unknown grid provider '" << type << "' requested!");
+} // Interface< GridImp >* create(const std::string& type, const Dune::ParameterTree paramTree = Dune::ParameterTree())
+
+} // namespace Provider
+} // namespace Grid
+} // namespace Stuff
+} // namespace Dune
+
+#endif // HAVE_DUNE_GRID
+
+#endif // DUNE_STUFF_GRID_PROVIDER_HH
--- dune/stuff/grid/provider/interface.hh
+++ dune/stuff/grid/provider/interface.hh
@@ -130,44 +130,6 @@ private:
 } // namespace Stuff
 } // namespace Dune
 
-#include "cube.hh"
-#if HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
-#include "gmsh.hh"
-#endif // HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
-
-namespace Dune {
-namespace Stuff {
-namespace Grid {
-namespace Provider {
-
-#if defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
-template <class GridImp = Dune::GridSelector::GridType>
-#else // defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
-template <class GridImp = Dune::SGrid<2, 2>>
-#endif // defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
-Interface<GridImp>* create(const std::string& type, const Dune::ParameterTree paramTree = Dune::ParameterTree())
-{
-  // choose provider
-  if (type == "stuff.grid.provider.cube") {
-    typedef Dune::Stuff::Grid::Provider::Cube<> CubeProviderType;
-    CubeProviderType* cubeProvider = new CubeProviderType(CubeProviderType::createFromParamTree(paramTree));
-    return cubeProvider;
-#if HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
-  } else if (type == "stuff.grid.provider.gmsh") {
-    typedef Dune::Stuff::Grid::Provider::Gmsh<> GmshProviderType;
-    GmshProviderType* gmshProvider = new GmshProviderType(GmshProviderType::createFromParamTree(paramTree));
-    return gmshProvider;
-#endif // HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
-  } else
-    DUNE_THROW(Dune::RangeError, "\nError: unknown grid provider '" << type << "' requested");
-
-} // Interface< GridImp >* create(const std::string& type, const Dune::ParameterTree paramTree = Dune::ParameterTree())
-
-} // namespace Provider
-} // namespace Grid
-} // namespace Stuff
-} // namespace Dune
-
 #endif // HAVE_DUNE_GRID
 
 #endif // DUNE_STUFF_GRID_PROVIDER_INTERFACE_HH
