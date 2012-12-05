416265668499d10f62d256461dc1f8e413935c16
Felix Albrecht <mail@felixalbrecht.de>
2012-12-05 12:34:51 +0100

    [grid.provider.create] made cube default

--- dune/stuff/grid/provider.hh
+++ dune/stuff/grid/provider.hh
@@ -25,7 +25,8 @@ template <class GridType = Dune::GridSelector::GridType>
 #else // defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
 template <class GridType = Dune::SGrid<2, 2>>
 #endif // defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
-Interface<GridType>* create(const std::string& type, const Dune::ParameterTree paramTree = Dune::ParameterTree())
+Interface<GridType>* create(const std::string& type = "stuff.grid.provider.cube",
+                            const Dune::ParameterTree paramTree = Dune::ParameterTree())
 {
   // choose provider
   if (type == "stuff.grid.provider.cube") {
