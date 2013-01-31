bd0b00ca98cc73a1ddfc14777879faef3f8bc786
Rene Milk <rene.milk@uni-muenster.de>
2013-01-31 15:37:21 +0100

    [tests] narrow test dim range for grid info

--- dune/stuff/test/grid_information.cc
+++ dune/stuff/test/grid_information.cc
@@ -19,7 +19,7 @@ struct Int
   static const int value = i;
 };
 
-typedef testing::Types<Int<1>, Int<2>, Int<3>, Int<4>, Int<5>, Int<6>, Int<7>, Int<8>, Int<9>, Int<10>> GridDims;
+typedef testing::Types<Int<1>, Int<2>, Int<3>> GridDims;
 
 template <class T>
 struct GridInfoTest : public ::testing::Test
