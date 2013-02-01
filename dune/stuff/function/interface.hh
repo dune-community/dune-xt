17b21e67d4edb1d32000962e4785743ec1793c70
Felix Albrecht <mail@felixalbrecht.de>
2013-02-01 17:23:06 +0100

    [function.interface] major rewrite to account for parametric and nonparametric functions

--- dune/stuff/function/interface.hh
+++ dune/stuff/function/interface.hh
@@ -1,192 +1,221 @@
-#ifndef DUNE_STUFF_FUNCTION_INTERFACE_hh
-#define DUNE_STUFF_FUNCTION_INTERFACE_hh
+﻿#ifndef DUNE_STUFF_FUNCTION_INTERFACE_HH
+#define DUNE_STUFF_FUNCTION_INTERFACE_HH
 
-#include <dune/common/exceptions.hh>
+#ifdef HAVE_CMAKE_CONFIG
+#include "cmake_config.h"
+#elif defined(HAVE_CONFIG_H)
+#include <config.h>
+#endif // ifdef HAVE_CMAKE_CONFIG
+
+#include <vector>
+
+#include <dune/common/shared_ptr.hh>
 #include <dune/common/fvector.hh>
+#include <dune/common/dynvector.hh>
 
-#if HAVE_EIGEN
-#include <Eigen/Core>
-#endif // HAVE_EIGEN
+#include <dune/stuff/common/color.hh>
 
-#ifdef HAVE_DUNE_FEM
-#include <dune/fem/function/common/function.hh>
-#include <dune/fem/space/common/functionspace.hh>
-#endif
+//#if HAVE_EIGEN
+//  #include <Eigen/Core>
+//#endif // HAVE_EIGEN
 
 namespace Dune {
 namespace Stuff {
 namespace Function {
 
-#ifdef HAVE_DUNE_FEM
-template <class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim>
-class Interface : public Dune::Fem::Function<Dune::FunctionSpace<DomainFieldImp, RangeFieldImp, domainDim, rangeDim>,
-                                             Interface<DomainFieldImp, domainDim, RangeFieldImp, rangeDim>>
-{
-  typedef Dune::Fem::Function<Dune::FunctionSpace<DomainFieldImp, RangeFieldImp, domainDim, rangeDim>,
-                              Interface<DomainFieldImp, domainDim, RangeFieldImp, rangeDim>> BaseType;
 
-public:
-  typedef typename BaseType::DomainFieldType DomainFieldType;
+// forward
+template <class ParamFieldImp, int maxParamDim, class RangeFieldImp>
+class Coefficient;
 
-  static const int dimDomain = domainDim;
 
-  typedef typename BaseType::DomainType DomainType;
+template <class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim, class ParamFieldImp = double,
+          int maxParamDim                                                                             = 0>
+class Interface;
 
-  static const int dimRange = rangeDim;
 
-  typedef typename BaseType::RangeFieldType RangeFieldType;
-
-  typedef typename BaseType::RangeType RangeType;
-
-#if HAVE_EIGEN
-  typedef typename Eigen::Matrix<RangeFieldType, Eigen::Dynamic, 1> EigenVectorType;
-#endif // HAVE_EIGEN
-
-  virtual void evaluate(const DomainType&, RangeType&) const = 0;
-
-#if HAVE_EIGEN
-  virtual void evaluate(const EigenVectorType& arg, EigenVectorType& ret) const
-  {
-    // process input
-    assert(arg.size() == dimDomain);
-    DomainType arg_fvector;
-    for (size_t i = 0; i < dimDomain; ++i)
-      arg_fvector[i] = arg(i);
-    // evaluate
-    RangeType ret_fvector;
-    evaluate(arg_fvector, ret_fvector);
-    // process output
-    assert(ret.size() == dimRange);
-    for (size_t i = 0; i < dimRange; ++i)
-      ret(i) = ret_fvector[i];
-  }
-
-  virtual EigenVectorType evaluate(const EigenVectorType& arg) const
-  {
-    EigenVectorType ret;
-    evaluate(arg, ret);
-    return ret;
-  }
-
-  virtual void evaluate(const DomainType& arg, EigenVectorType& ret) const
-  {
-    // evaluate
-    RangeType ret_fvector;
-    evaluate(arg, ret_fvector);
-    // process output
-    assert(ret.size() == dimRange);
-    for (size_t i = 0; i < dimRange; ++i)
-      ret(i) = ret_fvector[i];
-  }
-
-  virtual void evaluate(const EigenVectorType& arg, RangeType& ret) const
-  {
-    // process input
-    assert(arg.size() == dimDomain);
-    DomainType arg_fvector;
-    for (size_t i = 0; i < dimDomain; ++i)
-      arg_fvector[i] = arg(i);
-    // evaluate
-    RangeType ret_fvector;
-    evaluate(arg_fvector, ret);
-  }
-#endif // HAVE_EIGEN
-
-  virtual RangeType evaluate(const DomainType& arg) const
-  {
-    RangeType ret;
-    evaluate(arg, ret);
-    return ret;
-  }
-};
-
-#else // HAVE_DUNE_FEM
-
-/**
- *  \todo This should have been replaced by Dune::Function? Investigate further!
- */
-template <class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim>
-class Interface
+template <class DomainFieldImp, int domainDim, class RangeFieldImp, class ParamFieldImp, int maxParamDim>
+class Interface<DomainFieldImp, domainDim, RangeFieldImp, 1, ParamFieldImp, maxParamDim>
 {
 public:
+  typedef Interface<DomainFieldImp, domainDim, RangeFieldImp, 1, ParamFieldImp, maxParamDim> ThisType;
+
   typedef DomainFieldImp DomainFieldType;
-
   static const int dimDomain = domainDim;
-
   typedef Dune::FieldVector<DomainFieldType, dimDomain> DomainType;
 
   typedef RangeFieldImp RangeFieldType;
-
-  static const int dimRange = rangeDim;
-
+  static const int dimRange = 1;
   typedef Dune::FieldVector<RangeFieldType, dimRange> RangeType;
 
-#if HAVE_EIGEN
-  typedef typename Eigen::Matrix<RangeFieldType, Eigen::Dynamic, 1> EigenVectorType;
-#endif // HAVE_EIGEN
+  typedef ParamFieldImp ParamFieldType;
+  static const int maxDimParam = maxParamDim;
+  typedef Dune::DynamicVector<ParamFieldType> ParamType;
+  typedef typename ParamType::size_type size_type;
 
-  virtual void evaluate(const DomainType&, RangeType&) const = 0;
+  typedef Interface<DomainFieldType, dimDomain, RangeFieldType, dimRange> ComponentType;
+  typedef Coefficient<ParamFieldType, maxParamDim, RangeFieldType> CoefficientType;
 
-  virtual RangeType evaluate(const DomainType& arg) const
+  /** \defgroup purevirtual-type ´´These methods have to be implemented and determine the type of the function (and
+   * also, which of the below methods have to be implemented).'' */
+  /* @{ */
+  virtual bool parametric() const = 0;
+
+  virtual bool separable() const = 0;
+  /* @} */
+
+  /** \defgroup purevirtual ´´These methods have to be implemented.'' */
+  /* @{ */
+  virtual std::string name() const = 0;
+
+  virtual int order() const = 0;
+  /* @} */
+
+  /** \defgroup nonparametric-must ´´These methods have to be implemented, if parametric() == false.'' */
+  /* @{ */
+  virtual void evaluate(const DomainType& /*_x*/, RangeType& /*_ret*/) const
   {
-    RangeType ret;
-    evaluate(arg, ret);
-    return ret;
+    DUNE_THROW(Dune::NotImplemented,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " implement me if parametric() == false!");
+  }
+  /* @} */
+
+  /** \defgroup nonparametric-can ´´These methods are provided by the interface, but not optimal. You can provide an
+   * implementation if parametric() == false'' */
+  /* @{ */
+  virtual void evaluate(const ParamType& /*_mu*/, RangeType& /*_ret*/) const
+  {
+    assert(false && "I should be implemented!");
+  }
+  /* @} */
+
+  /** \defgroup parametric-must ´´These methods have to be implemented, if parametric() == true.'' */
+  /* @{ */
+  virtual size_type paramSize() const
+  {
+    DUNE_THROW(Dune::NotImplemented,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " implement me if parametric() == true!");
   }
 
-#if HAVE_EIGEN
-  virtual void evaluate(const EigenVectorType& arg, EigenVectorType& ret) const
+  virtual const std::vector<ParamType>& paramRange() const
   {
-    // process input
-    assert(arg.size() == dimDomain);
-    DomainType arg_fvector;
-    for (int i = 0; i < dimDomain; ++i)
-      arg_fvector[i] = arg(i);
-    // evaluate
-    RangeType ret_fvector;
-    evaluate(arg_fvector, ret_fvector);
-    // process output
-    assert(ret.size() == dimRange);
-    for (int i = 0; i < dimRange; ++i)
-      ret(i) = ret_fvector[i];
+    DUNE_THROW(Dune::NotImplemented,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " implement me if parametric() == true!");
   }
 
-  virtual EigenVectorType evaluate(const EigenVectorType& arg) const
+  virtual const std::vector<std::string>& paramExplanation() const
   {
-    EigenVectorType ret;
-    evaluate(arg, ret);
-    return ret;
+    DUNE_THROW(Dune::NotImplemented,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " implement me if parametric() == true!");
   }
 
-  virtual void evaluate(const DomainType& arg, EigenVectorType& ret) const
+  virtual void evaluate(const DomainType& /*_x*/, const ParamType& /*_mu*/, RangeType& /*_ret*/) const
   {
-    // evaluate
-    RangeType ret_fvector;
-    evaluate(arg, ret_fvector);
-    // process output
-    assert(ret.size() == dimRange);
-    for (int i = 0; i < dimRange; ++i)
-      ret(i) = ret_fvector[i];
+    DUNE_THROW(Dune::NotImplemented,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " implement me if parametric() == true!");
+  }
+  /* @} */
+
+  /** \defgroup separable ´´These methods have to be implemented, if separable() == true.'' */
+  /* @{ */
+  virtual size_type numComponents() const
+  {
+    DUNE_THROW(Dune::NotImplemented,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " implement me if separable() == true!");
   }
 
-  virtual void evaluate(const EigenVectorType& arg, RangeType& ret) const
+  virtual const std::vector<Dune::shared_ptr<const ComponentType>>& components() const
   {
-    // process input
-    assert(arg.size() == dimDomain);
-    DomainType arg_fvector;
-    for (int i = 0; i < dimDomain; ++i)
-      arg_fvector[i] = arg(i);
-    // evaluate
-    RangeType ret_fvector;
-    evaluate(arg_fvector, ret);
+    DUNE_THROW(Dune::NotImplemented,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " implement me if separable() == true!");
   }
-#endif // HAVE_EIGEN
+
+  virtual unsigned int numCoefficients() const
+  {
+    DUNE_THROW(Dune::NotImplemented,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " implement me if separable() == true!");
+  }
+
+  virtual const std::vector<Dune::shared_ptr<const CoefficientType>>& coefficients() const
+  {
+    DUNE_THROW(Dune::NotImplemented,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " implement me if separable() == true!");
+  }
+  /* @} */
+
+  /** \defgroup functional ´´These methods are provided by the interface itself.'' */
+  /* @{ */
+  //  // first of all, import all the nonparametric evaluate()s
+  //  using ComponentType::evaluate;
+
+  //  virtual RangeType evaluate(const DomainType& x, const ParamType& mu) const
+  //  {
+  //    RangeType ret;
+  //    evaluate(x, mu, ret);
+  //    return ret;
+  //  }
+
+  //  virtual void evaluate(const ParamType& x, const ParamType& mu, RangeType& ret) const
+  //  {
+  //    // process input
+  //    assert(x.size() == dimDomain);
+  //    DomainType x_fvector;
+  //    for (int i = 0; i < dimDomain; ++i)
+  //      x_fvector[i] = x(i);
+  //    // evaluate
+  //    evaluate(x_fvector, mu, ret);
+  //  }
+
+  //  virtual void evaluate(const DomainType& x, const ParamType& mu, ParamType& ret) const
+  //  {
+  //    // evaluate
+  //    RangeType ret_fvector;
+  //    evaluate(x, mu, ret_fvector);
+  //    // process output
+  //    assert(ret.size() == dimRange);
+  //    for (int i = 0; i < dimRange; ++i)
+  //      ret(i) = ret_fvector[i];
+  //  }
+
+  //  virtual void evaluate(const ParamType& x, const ParamType& mu, ParamType& ret) const
+  //  {
+  //    // process input
+  //    assert(x.size() == dimDomain);
+  //    DomainType x_fvector;
+  //    for (int i = 0; i < dimDomain; ++i)
+  //      x_fvector[i] = x(i);
+  //    // evaluate
+  //    RangeType ret_fvector;
+  //    evaluate(x_fvector, mu, ret_fvector);
+  //    // process output
+  //    assert(ret.size() == dimRange);
+  //    for (int i = 0; i < dimRange; ++i)
+  //      ret(i) = ret_fvector[i];
+  //  }
+
+  //  virtual ParamType evaluate(const ParamType& x, const ParamType& mu) const
+  //  {
+  //    ParamType ret;
+  //    evaluate(x, mu, ret);
+  //    return ret;
+  //  }
+
+  //  void report(std::ostream& out = std::cout, std::string prefix = "") const
+  //  {
+  //    out << prefix << "parameter explanation:" << std::endl;
+  //    assert(paramExplanation().size() == paramSize());
+  //    assert(paramRange().size() == 2);
+  //    assert(paramRange()[0].size() == paramSize());
+  //    assert(paramRange()[1].size() == paramSize());
+  //    for (unsigned int pp = 0; pp < paramSize(); ++pp)
+  //      out << prefix << "  " << paramExplanation()[pp] << ", between " << paramRange()[0](pp) << " and " <<
+  //      paramRange()[1](pp) << std::endl;
+  //  }
+  /* @} */
 }; // class Interface
-
-#endif // HAVE_DUNE_FEM
 
 } // namespace Function
 } // namespace Stuff
 } // namespace Dune
 
-#endif // DUNE_STUFF_FUNCTION_INTERFACE_hh
+#endif // DUNE_STUFF_FUNCTION_INTERFACE_HH
