ccf89e940281974119b5e930e6cb827f5db9b025
Felix Albrecht <mail@felixalbrecht.de>
2013-02-08 16:31:18 +0100

    [grid.provider] return shared instead of naked ptr

--- dune/stuff/grid/provider.hh
+++ dune/stuff/grid/provider.hh
@@ -24,30 +24,30 @@ template <class GridType = Dune::GridSelector::GridType>
 #else // defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
 template <class GridType = Dune::SGrid<2, 2>>
 #endif // defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
-Interface<GridType>* create(const std::string& type = "stuff.grid.provider.cube",
-                            const Dune::ParameterTree paramTree = Dune::ParameterTree())
+Dune::shared_ptr<Interface<GridType>> create(const std::string type = "stuff.grid.provider.cube",
+                                             const Dune::ParameterTree description = Dune::ParameterTree())
 {
   // choose provider
   if (type == "stuff.grid.provider.cube") {
-    typedef Dune::Stuff::Grid::Provider::Cube<GridType> CubeProviderType;
-    CubeProviderType* cubeProvider = new CubeProviderType(CubeProviderType::createFromParamTree(paramTree));
-    return cubeProvider;
+    typedef Dune::Stuff::Grid::Provider::Cube<GridType> ProviderType;
+    return Dune::shared_ptr<ProviderType>(new ProviderType(ProviderType::createFromDescription(description)));
 #if HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
 #if defined ALUGRID_CONFORM || defined ALUGRID_CUBE || defined ALUGRID_SIMPLEX || defined ALBERTAGRID || defined UGGRID
   } else if (type == "stuff.grid.provider.gmsh") {
-    typedef Dune::Stuff::Grid::Provider::Gmsh<GridType> GmshProviderType;
-    GmshProviderType* gmshProvider = new GmshProviderType(GmshProviderType::createFromParamTree(paramTree));
-    return gmshProvider;
+    typedef Dune::Stuff::Grid::Provider::Gmsh<GridType> ProviderType;
+    return Dune::shared_ptr<ProviderType>(new ProviderType(ProviderType::createFromDescription(description)));
 #endif // defined ALUGRID_CONFORM || defined ALUGRID_CUBE || defined ALUGRID_SIMPLEX || defined ALBERTAGRID || defined
 // UGGRID
 #endif // HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
   } else if (type == "stuff.grid.provider.starcd") {
-    typedef Dune::Stuff::Grid::Provider::StarCD<GridType> StarCDProviderType;
-    StarCDProviderType* starCDProvider = new StarCDProviderType(StarCDProviderType::createFromParamTree(paramTree));
-    return starCDProvider;
+    typedef Dune::Stuff::Grid::Provider::StarCD<GridType> ProviderType;
+    return Dune::shared_ptr<ProviderType>(new ProviderType(ProviderType::createFromDescription(description)));
   } else
-    DUNE_THROW(Dune::RangeError, "\nERROR: unknown grid provider '" << type << "' requested!");
-} // Interface< GridImp >* create(const std::string& type, const Dune::ParameterTree paramTree = Dune::ParameterTree())
+    DUNE_THROW(Dune::RangeError,
+               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " unknown grid provider '" << type
+                    << "' requested!");
+} // ... create(...)
+
 
 } // namespace Provider
 } // namespace Grid
