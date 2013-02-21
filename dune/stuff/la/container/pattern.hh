cac56437a0d7f1dbf55c8f57ce409e4aed7b0d49
Felix Albrecht <mail@felixalbrecht.de>
2013-02-21 18:01:00 +0100

    [la.container.pattern] added createCompressedSparsityPattern()

--- dune/stuff/la/container/pattern.hh
+++ dune/stuff/la/container/pattern.hh
@@ -10,6 +10,7 @@ namespace Stuff {
 namespace LA {
 namespace Container {
 
+
 class SparsityPatternDefault
 {
 private:
@@ -45,6 +46,40 @@ public:
 private:
   BaseType vectorOfSets_;
 }; // class SparsityPatternDefault
+
+
+// forward of the Interface
+template <class T>
+class MatrixInterface;
+
+
+template <class T>
+SparsityPatternDefault*
+createCompressedSparsityPattern(const SparsityPatternDefault& uncompressedPattern, const MatrixInterface<T>& matrix,
+                                const typename T::ElementType threshhold = typename T::ElementType(0))
+{
+  assert(!(threshhold < typename T::ElementType(0)) && "Please provide a nonnegative threshhold!");
+  // we create a new pattern of only nonzero entries
+  SparsityPatternDefault* compressedPattern = new SparsityPatternDefault(uncompressedPattern.size());
+  typename T::ElementType absValue(0);
+  // * therefore we loop over the uncompressed pattern
+  for (size_t row = 0; row < uncompressedPattern.size(); ++row) {
+    // * get the uncompressed row,
+    const auto& uncompressedRowSet = uncompressedPattern.set(row);
+    // * get the new one
+    auto& compressedRowSet = compressedPattern->set(row);
+    // * and loop over the uncompressed row
+    for (size_t col : uncompressedRowSet) {
+      // * get the value in the matric
+      absValue = std::abs(matrix.get(row, col));
+      // * and add the column to the new pattern, if the value is large enough
+      if (absValue > threshhold)
+        compressedRowSet.insert(col);
+    }
+  }
+  return compressedPattern;
+} // ... compressSparsityPattern(...)
+
 
 } // namespace Container
 } // namespace LA
