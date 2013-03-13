0dc80cfcdbe5533fae0b4d70fb8468d0f206e9c1
Felix Albrecht <mail@felixalbrecht.de>
2013-03-13 11:36:56 +0100

    [grid.provider] major change, to allow for grid specific specialization i.e. avialableGridProviders() -> GridProviders< ... >::available()

--- dune/stuff/grid/provider.hh
+++ dune/stuff/grid/provider.hh
@@ -9,95 +9,73 @@
 
 #if HAVE_DUNE_GRID
 
-#include <functional>
-#include <dune/common/shared_ptr.hh>
 #include <dune/common/parametertree.hh>
+
+#include <dune/grid/sgrid.hh>
 
 #include <dune/stuff/common/color.hh>
 
 #include "provider/interface.hh"
 #include "provider/cube.hh"
+#ifdef HAVE_UNSTRUCTURED_GRIDFACTORY
 #include "provider/gmsh.hh"
+#endif
 #include "provider/starcd.hh"
 
 namespace Dune {
 namespace Stuff {
 
 
-std::vector<std::string> availableGridProviders()
-{
-  return {"gridprovider.cube"
-#ifdef HAVE_UNSTRUCTURED_GRIDFACTORY
-          ,
-          "gridprovider.gmsh"
-#endif
-          ,
-          "gridprovider.starcd"};
-} // ... availableGridProviders()
-
-
 #if defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
 template <class GridType = Dune::GridSelector::GridType>
 #else
 template <class GridType = Dune::SGrid<2, 2>>
 #endif
-Dune::ParameterTree createSampleGridProviderDescription(const std::string type)
+class GridProviders
 {
-  if (type == "gridprovider.cube") {
-    return GridProviderCube<GridType>::createSampleDescription();
+public:
+  static std::vector<std::string> available()
+  {
+    return {"gridprovider.cube"
 #ifdef HAVE_UNSTRUCTURED_GRIDFACTORY
-  } else if (type == "gridprovider.gmsh") {
-    return GridProviderGmsh<GridType>::createSampleDescription();
+            ,
+            "gridprovider.gmsh"
 #endif
-  } else if (type == "gridprovider.starcd") {
-    return GridProviderStarCD<GridType>::createSampleDescription();
-  } else
-    DUNE_THROW(Dune::RangeError,
-               "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " unknown grid provider '" << type
-                    << "' requested!");
-} // ... createSampleGridProviderDescription(...)
+            ,
+            "gridprovider.starcd"};
+  } // ... available()
 
-
-template <class GridType>
-struct DSGP_FunctionObject
-{
-  typedef std::function<GridProviderInterface<GridType>*(const std::string)> Type;
-};
-
-template <class ProviderType>
-std::pair<std::string, typename DSGP_FunctionObject<typename ProviderType::GridType>::Type>
-make(const Dune::ParameterTree paramTree)
-{
-  return std::make_pair(ProviderType::id(), std::bind(&ProviderType::create, paramTree, std::placeholders::_1));
-}
-
-#define DSGP_MAKE(type) make<type<GridType>>(description)
-
-
-#if defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
-template <class GridType = Dune::GridSelector::GridType>
-#else
-template <class GridType = Dune::SGrid<2, 2>>
-#endif
-GridProviderInterface<GridType>* createGridProvider(const std::string& type = "grid.provider.cube",
-                                                    const Dune::ParameterTree description = Dune::ParameterTree())
-{
-  typedef std::map<std::string, std::pair<std::string, typename DSGP_FunctionObject<GridType>::Type>> MapType;
-
-  MapType ptr_map = {{"gridprovider.cube", DSGP_MAKE(GridProviderCube)}
+  static Dune::ParameterTree createSampleDescription(const std::string type)
+  {
+    if (type == "gridprovider.cube") {
+      return GridProviderCube<GridType>::createSampleDescription();
 #ifdef HAVE_UNSTRUCTURED_GRIDFACTORY
-                     ,
-                     {"gridprovider.gmsh", DSGP_MAKE(GridProviderGmsh)}
+    } else if (type == "gridprovider.gmsh") {
+      return GridProviderGmsh<GridType>::createSampleDescription();
 #endif
-                     ,
-                     {"gridprovider.starcd", DSGP_MAKE(GridProviderStarCD)}};
-  auto id_func_it = ptr_map.find(type);
-  if (id_func_it == ptr_map.end())
-    DUNE_THROW(Dune::RangeError,
-               "\n" << Common::colorStringRed("ERROR:") << " unknown grid provider '" << type << "' requested!");
-  auto id_func = id_func_it->second;
-  return id_func.second(id_func.first);
-} // ... createGridProvider(...)
+    } else if (type == "gridprovider.starcd") {
+      return GridProviderStarCD<GridType>::createSampleDescription();
+    } else
+      DUNE_THROW(Dune::RangeError,
+                 "\n" << Common::colorStringRed("ERROR:") << " unknown gridprovider '" << type << "' requested!");
+  } // ... createSampleDescription(...)
+
+  static GridProviderInterface<GridType>* create(const std::string& type = "gridprovider.cube",
+                                                 const Dune::ParameterTree description = Dune::ParameterTree())
+  {
+    if (type == "gridprovider.cube")
+      return GridProviderCube<GridType>::create(description);
+#ifdef HAVE_UNSTRUCTURED_GRIDFACTORY
+    else if (type == "gridprovider.gmsh")
+      return GridProviderGmsh<GridType>::create(description);
+#endif
+    else if (type == "gridprovider.starcd")
+      return GridProviderStarCD<GridType>::create(description);
+    else
+      DUNE_THROW(Dune::RangeError,
+                 "\n" << Common::colorStringRed("ERROR:") << " unknown gridprovider '" << type << "' requested!");
+  } // ... create(...)
+}; // class GridProviders
 
 
 } // namespace Stuff
