6d82907a171bcf405353446d1bd3b4caaf979e4c
Kirsten Weber <k_webe11@pingpong.(none)>
2013-03-27 17:13:22 +0100

    added unitcube-example files for gmsh and starcd

--- dune/stuff/grid/provider.hh
+++ dune/stuff/grid/provider.hh
@@ -17,8 +17,10 @@
 
 #include "provider/interface.hh"
 #include "provider/cube.hh"
-#ifdef HAVE_UNSTRUCTURED_GRIDFACTORY
+#if HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
+#if defined ALUGRID_CONFORM || defined ALUGRID_CUBE || defined ALUGRID_SIMPLEX || defined ALBERTAGRID || defined UGGRID
 #include "provider/gmsh.hh"
+#endif
 #endif
 #include "provider/starcd.hh"
 
@@ -36,22 +38,29 @@ class GridProviders
 public:
   static std::vector<std::string> available()
   {
-    return {"gridprovider.cube"
-#ifdef HAVE_UNSTRUCTURED_GRIDFACTORY
-            ,
-            "gridprovider.gmsh"
+    return
+    {
+      "gridprovider.cube"
+#if HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
+#if defined ALUGRID_CONFORM || defined ALUGRID_CUBE || defined ALUGRID_SIMPLEX || defined ALBERTAGRID || defined UGGRID
+          ,
+          "gridprovider.gmsh"
 #endif
-            ,
-            "gridprovider.starcd"};
+#endif
+          ,
+          "gridprovider.starcd"
+    };
   } // ... available()
 
   static Dune::ParameterTree createSampleDescription(const std::string type, const std::string subname = "")
   {
     if (type == "gridprovider.cube") {
       return GridProviderCube<GridType>::createSampleDescription(subname);
-#ifdef HAVE_UNSTRUCTURED_GRIDFACTORY
+#if HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
+#if defined ALUGRID_CONFORM || defined ALUGRID_CUBE || defined ALUGRID_SIMPLEX || defined ALBERTAGRID || defined UGGRID
     } else if (type == "gridprovider.gmsh") {
       return GridProviderGmsh<GridType>::createSampleDescription(subname);
+#endif
 #endif
     } else if (type == "gridprovider.starcd") {
       return GridProviderStarCD<GridType>::createSampleDescription(subname);
@@ -65,9 +74,11 @@ public:
   {
     if (type == "gridprovider.cube")
       return GridProviderCube<GridType>::create(description);
-#ifdef HAVE_UNSTRUCTURED_GRIDFACTORY
+#if HAVE_ALUGRID || HAVE_ALBERTA || HAVE_UG
+#if defined ALUGRID_CONFORM || defined ALUGRID_CUBE || defined ALUGRID_SIMPLEX || defined ALBERTAGRID || defined UGGRID
     else if (type == "gridprovider.gmsh")
       return GridProviderGmsh<GridType>::create(description);
+#endif
 #endif
     else if (type == "gridprovider.starcd")
       return GridProviderStarCD<GridType>::create(description);
--- dune/stuff/grid/provider/starcd.hh
+++ dune/stuff/grid/provider/starcd.hh
@@ -41,6 +41,9 @@ namespace Stuff {
 
 /**
  * \brief   StarCD grid provider
+ *
+ *          Implemented for dimensions 1, 2, and 3.
+ *
  */
 #if defined HAVE_CONFIG_H || defined HAVE_CMAKE_CONFIG
 template <class GridImp = Dune::GridSelector::GridType>
@@ -157,10 +160,12 @@ public:
         for (unsigned int k = 0; k < numberOfVerticesCube; k++) {
           cubeVertices[k] = items2[k + 1] - 1;
         }
-        std::swap(cubeVertices[2], cubeVertices[3]);
-        std::swap(cubeVertices[6], cubeVertices[7]);
+        if (dim > 1)
+          std::swap(cubeVertices[2], cubeVertices[3]);
+        if (dim == 3)
+          std::swap(cubeVertices[6], cubeVertices[7]);
         factory.insertElement(Dune::GeometryType(Dune::GeometryType::cube, dim), cubeVertices);
-      } else if (items2.size() == numberOfVerticesPrism + 1) // prism
+      } else if ((items2.size() == numberOfVerticesPrism + 1) && (dim == 3)) // prism
       {
         numberOfPrisms++;
         for (unsigned int k = 0; k < numberOfVerticesPrism; k++) {
