fa016b73bf3343d63958cea44197ed756b586e6c
Felix Albrecht <mail@felixalbrecht.de>
2013-06-24 23:23:56 +0200

    [test.grid_provider] make compile again

--- dune/stuff/test/grid_provider.cc
+++ dune/stuff/test/grid_provider.cc
@@ -48,8 +48,8 @@ struct CubeTest : public testing::Test
   void test_cube()
   {
     const std::vector<std::string> types                = Dune::Stuff::GridProviders<GridType>::available();
-    const ParameterTree description                     = Dune::Stuff::GridProviders<GridType>::createSampleDescription(types[0]);
-    const GridProviderInterface<GridType>* gridProvider = GridProviders<GridType>::create(types[0], description);
+    const ParameterTree settings                        = Dune::Stuff::GridProviders<GridType>::defaultSettings(types[0]);
+    const GridProviderInterface<GridType>* gridProvider = GridProviders<GridType>::create(types[0], settings);
     EXPECT_GT(gridProvider->grid()->size(0), 0);
     EXPECT_GT(gridProvider->grid()->size(1), 0);
   }
