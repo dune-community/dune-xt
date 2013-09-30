ff82e950a1c022ad358572757bd5dc2783adc5c2
Felix Albrecht <mail@felixalbrecht.de>
2013-09-30 22:58:53 +0200

    [functions.interfaces] added visualize()

--- dune/stuff/functions/interfaces.hh
+++ dune/stuff/functions/interfaces.hh
@@ -18,6 +18,12 @@
 
 namespace Dune {
 namespace Stuff {
+namespace Function {
+
+// forward, include is below
+template <class GridViewType, int dimRange>
+class VisualizationAdapter;
+}
 
 
 /**
@@ -411,6 +417,17 @@ public:
     return static_id();
   }
   /* @} */
+
+  template <class GridViewType>
+  void visualize(const GridViewType& grid_view, const std::string filename) const
+  {
+    if (filename.empty())
+      DUNE_THROW(IOError, "Empty filename given!");
+    auto adapter = std::make_shared<Function::VisualizationAdapter<GridViewType, dimRange>>(*this);
+    VTKWriter<GridViewType> vtk_writer(grid_view, VTK::nonconforming);
+    vtk_writer.addVertexData(adapter);
+    vtk_writer.write(filename);
+  } // ... visualize(...)
 }; // class LocalizableFunctionInterface
 
 
@@ -569,5 +586,7 @@ TimeFunctionAdapter<DomainFieldImp, domainDim, RangeFieldImp, rangeDimRows> time
 
 } // namespace Stuff
 } // namespace Dune
+
+#include "visualization.hh"
 
 #endif // DUNE_STUFF_FUNCTION_INTERFACE_HH
