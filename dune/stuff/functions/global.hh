4b9c271e1336fae25b0be0d02857e66b57fa82c7
Rene Milk <rene.milk@uni-muenster.de>
2013-10-11 13:19:09 +0200

    [functions] introduces GlobalFunction, moves Timedependent*

---
+++ dune/stuff/functions/global.hh
@@ -0,0 +1,156 @@
+#ifndef DUNE_STUFF_FUNCTION_GLOBAL_HH
+#define DUNE_STUFF_FUNCTION_GLOBAL_HH
+
+#include <memory>
+#include <string>
+
+#if HAVE_DUNE_FEM
+#include <dune/fem/function/common/function.hh>
+#include <dune/fem/space/common/functionspace.hh>
+#endif
+
+#include <dune/stuff/functions/interfaces.hh>
+
+namespace Dune {
+namespace Stuff {
+
+/**
+ * base class for global valued functions that provides automatic local functions via LocalizableFunctionInterface
+ */
+template <class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim>
+class GlobalFunction
+    : public LocalizableFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>
+#if HAVE_DUNE_FEM
+      ,
+      public Dune::Fem::Function<Dune::Fem::FunctionSpace<DomainFieldImp, RangeFieldImp, domainDim, rangeDim>,
+                                 GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim>>
+#endif // HAVE_DUNE_FEM
+{
+  typedef GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim> ThisType;
+  typedef LocalfunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>
+      LocalfunctionBaseType;
+
+public:
+  typedef DomainFieldImp DomainFieldType;
+  static const unsigned int dimDomain = domainDim;
+  typedef Dune::FieldVector<DomainFieldType, dimDomain> DomainType;
+
+  typedef RangeFieldImp RangeFieldType;
+  static const unsigned int dimRange     = rangeDim;
+  static const unsigned int dimRangeRows = dimRange;
+  static const unsigned int dimRangeCols = 1;
+  typedef Dune::FieldVector<RangeFieldType, dimRange> RangeType;
+#if HAVE_DUNE_FEM
+  typedef typename Dune::Fem::Function<Dune::Fem::FunctionSpace<DomainFieldImp, RangeFieldImp, domainDim, rangeDim>,
+                                       GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp,
+                                                      rangeDim>>::JacobianRangeType JacobianRangeType;
+#else
+  typedef typename LocalfunctionBaseType::JacobianRangeType JacobianRangeType;
+#endif
+
+  virtual ~GlobalFunction()
+  {
+  }
+
+  virtual std::string name() const
+  {
+    return "";
+  }
+
+  virtual ThisType* copy() const
+  {
+    return new ThisType(*this);
+  }
+
+  virtual size_t order() const
+  {
+    return std::numeric_limits<size_t>::max();
+  }
+
+  class Localfunction : public LocalfunctionBaseType
+  {
+  public:
+    Localfunction(const EntityImp& entity, const ThisType& global_function)
+      : geometry_(entity.geometry())
+      , entity_(entity)
+      , global_function_(global_function)
+    {
+    }
+
+    virtual void evaluate(const DomainType& xx, RangeType& ret) const
+    {
+      const auto xx_global = geometry_.global(xx);
+      global_function_.evaluate(xx_global, ret);
+    }
+
+    virtual void jacobian(const DomainType& xx, JacobianRangeType& ret) const
+    {
+      const auto xx_global = geometry_.global(xx);
+      global_function_.jacobian(xx_global, ret);
+    }
+
+    virtual size_t order() const
+    {
+      return global_function_.order();
+    }
+
+    const EntityImp& entity() const
+    {
+      return entity_;
+    }
+
+  private:
+    const typename EntityImp::Geometry& geometry_;
+    const EntityImp& entity_;
+    const ThisType& global_function_;
+  };
+
+  virtual std::unique_ptr<LocalfunctionBaseType> local_function(const EntityImp& entity) const
+  {
+    return Common::make_unique<Localfunction>(entity, *this);
+  }
+}; // class GlobalFunction
+
+
+template <class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim>
+class GlobalConstantFunction : public GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim>
+{
+  typedef GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim> BaseType;
+  typedef typename BaseType::RangeType RangeType;
+  typedef typename BaseType::DomainType DomainType;
+  typedef typename BaseType::JacobianRangeType JacobianRangeType;
+
+public:
+  explicit GlobalConstantFunction(const RangeType& constant)
+    : constant_(constant)
+  {
+  }
+
+  explicit GlobalConstantFunction(const RangeFieldImp& value)
+    : constant_(value)
+  {
+  }
+
+  virtual size_t order() const final override
+  {
+    return 0;
+  }
+
+  virtual void evaluate(const DomainType& /*x*/, RangeType& ret) const final
+  {
+    ret = constant_;
+  }
+
+  virtual void jacobian(const DomainType& /*x*/, JacobianRangeType& ret) const final
+  {
+    ret = JacobianRangeType(0);
+  }
+
+private:
+  const RangeType constant_;
+};
+
+} // namespace Stuff
+} // namespace Dune
+
+#endif // DUNE_STUFF_FUNCTION_GLOBAL_HH
--- dune/stuff/functions/interfaces.hh
+++ dune/stuff/functions/interfaces.hh
@@ -22,6 +22,8 @@
 #include <dune/fem/space/common/functionspace.hh>
 #endif
 
+#include <dune/stuff/common/memory.hh>
+
 namespace Dune {
 namespace Stuff {
 #if HAVE_DUNE_GRID
@@ -424,84 +426,6 @@ public:
     return ret;
   }
 }; // class FunctionInterface
-
-
-/**
- * \brief Interface for scalar and vector valued timedependent functions.
- */
-template <class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim>
-class TimedependentFunctionInterface
-{
-public:
-  typedef DomainFieldImp DomainFieldType;
-  static const unsigned int dimDomain = domainDim;
-  typedef Dune::FieldVector<DomainFieldType, dimDomain> DomainType;
-
-  typedef RangeFieldImp RangeFieldType;
-  static const unsigned int dimRange = rangeDim;
-  typedef Dune::FieldVector<RangeFieldType, dimRange> RangeType;
-
-  virtual ~TimedependentFunctionInterface()
-  {
-  }
-
-  static std::string static_id()
-  {
-    return "dune.stuff.timedependentfunction";
-  }
-
-  /** \defgroup info ´´These methods should be implemented in order to identify the function.'' */
-  /* @{ */
-  virtual std::string name() const
-  {
-    return static_id();
-  }
-
-  virtual int order() const
-  {
-    return -1;
-  }
-  /* @} */
-
-  /** \defgroup must ´´This method has to be implemented.'' */
-  /* @{ */
-  virtual void evaluate(const DomainType& /*xx*/, const double& /*tt*/, RangeType& /*ret*/) const = 0;
-  /* @} */
-}; // class TimedependentFunctionInterface
-
-
-//! use this to throw a stationary function into an algorithm that expects an instationary one
-template <class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDimRows>
-struct TimeFunctionAdapter
-    : public Dune::Stuff::TimedependentFunctionInterface<DomainFieldImp, domainDim, RangeFieldImp, rangeDimRows>
-{
-  typedef Dune::Stuff::FunctionInterface<DomainFieldImp, domainDim, RangeFieldImp, rangeDimRows> WrappedType;
-
-  TimeFunctionAdapter(const WrappedType& wr)
-    : wrapped_(wr)
-  {
-  }
-
-  virtual void evaluate(const typename WrappedType::DomainType& x, typename WrappedType::RangeType& ret) const
-  {
-    wrapped_(x, ret);
-  }
-
-  virtual void evaluate(const typename WrappedType::DomainType& x, const double& /*t*/,
-                        typename WrappedType::RangeType& ret) const
-  {
-    wrapped_(x, ret);
-  }
-
-  const WrappedType& wrapped_;
-};
-
-template <class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDimRows>
-TimeFunctionAdapter<DomainFieldImp, domainDim, RangeFieldImp, rangeDimRows> timefunctionAdapted(
-    const Dune::Stuff::FunctionInterface<DomainFieldImp, domainDim, RangeFieldImp, rangeDimRows>& wrapped)
-{
-  return TimeFunctionAdapter<DomainFieldImp, domainDim, RangeFieldImp, rangeDimRows>(wrapped);
-}
 
 
 } // namespace Stuff
