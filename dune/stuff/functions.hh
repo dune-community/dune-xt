11a9cb03223575f9533a1f6542a99ac95fbae262
Felix Albrecht <mail@felixalbrecht.de>
2013-10-22 10:31:39 +0200

    [functions] moved to lib * Now this one is a little tricky. The problem is that we   basically need a specialization for every combination   of dimensions (since some functions may only be   implemented for some dimensions). * We now generate specialized declarations for all   dimensions in the header. * We also generate specialized definitions for all   dimensions in the source and we require the   implementer of a new functions to add the function in   the source file. * Adding a new function is a sub optimal task, since it   requires some copying. This solution may not be the   most elegant but it seemed to be *fastest*, since we   can just copy/paste find/replace. * Ideas and patches are welcome!

--- dune/stuff/functions.hh
+++ dune/stuff/functions.hh
@@ -1,3 +1,8 @@
+// This file is part of the dune-stuff project:
+//   http://users.dune-project.org/projects/dune-stuff/
+// Copyright Holders: Felix Albrecht
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
 #ifndef DUNE_STUFF_FUNCTIONS_HH
 #define DUNE_STUFF_FUNCTIONS_HH
 
@@ -20,230 +25,67 @@ namespace Stuff {
 
 
 /**
- *  \brief      This is a way to create new functions fulfilling the FunctionInterface using their string identifier.
+ *  \brief  This is a way to create new functions fulfilling the LocalizableFunctionInterface using their string
+ *          identifier.
  *
- *  \attention  This class will not compile for all dimensions. The errors should give you a hint which specializations
- *              are needed below.
+ *  \note If you implement a new function, you should add it to the libdunestuff by editing the functions.cc file as
+ *        follows. Inside the DUNE_STUFF_FUNCTIONS_CC_GENERATE_SPECIALIZATION macro, for each function, you will find
+ *        three lines like
+ *          FUNCTION_Expression_ ## ddim ## _ ## rdim ## _ ## rCdim ## _STATIC_ID(E, D, ddim, R, rdim, rCdim) \
+ *        Yust copy each of those and replace Expression by the class name of your Function. In addition, below the
+ *        DUNE_STUFF_FUNCTIONS_CC_GENERATE_SPECIALIZATION macro you will find a very long list of macro definitions like
+ *          FUNCTION_Expression_1_1_1_ ... (E, D, ddim, R, rdim, rCdim)
+ *        for each function with ... replaced by STATIC_ID, SETTINGS and CREATE. You will have to copy those 81 lines of
+ *        code for your function and insert which combination of dimensions you did implement and which not. This might
+ *        seem totally ridiculous but it can be acchieved quite fast by a few find and replace commands of your favorite
+ *        text editor. Patches welcome if you have a better idea!
  */
-template <class E, class D, int d, class R, int rR, int rC = 1>
+template <class E, class D, int d, class R, int r, int rC = 1>
 class Functions
 {
 public:
-  static std::vector<std::string> available()
-  {
-    return {Function::Constant<E, D, d, R, rR, rC>::static_id(),
-            Function::Expression<E, D, d, R, rR, rC>::static_id(),
-            Function::Checkerboard<E, D, d, R, rR, rC>::static_id(),
-            Function::Spe10Model1<E, D, d, R, rR, rC>::static_id()};
-  } // ... available(...)
+  static std::vector<std::string> available() = delete;
 
-  static Dune::ParameterTree defaultSettings(const std::string type = available()[0])
-  {
-    if (type == Function::Constant<E, D, d, R, rR, rC>::static_id())
-      return Function::Constant<E, D, d, R, rR, rC>::defaultSettings();
-    else if (type == Function::Expression<E, D, d, R, rR, rC>::static_id())
-      return Function::Expression<E, D, d, R, rR, rC>::defaultSettings();
-    else if (type == Function::Checkerboard<E, D, d, R, rR, rC>::static_id())
-      return Function::Checkerboard<E, D, d, R, rR, rC>::defaultSettings();
-    else if (type == Function::Spe10Model1<E, D, d, R, rR, rC>::static_id())
-      return Function::Spe10Model1<E, D, d, R, rR, rC>::defaultSettings();
-    else
-      DUNE_THROW(Dune::RangeError,
-                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " unknown function '" << type
-                      << "' requested!");
-  } // ... defaultSettings(...)
+  static Dune::ParameterTree defaultSettings(const std::string /*type = available()[0]*/) = delete;
 
-  static LocalizableFunctionInterface<E, D, d, R, rR, rC>*
-  create(const std::string type = available()[0], const Dune::ParameterTree settings = defaultSettings())
-  {
-    if (type == Function::Constant<E, D, d, R, rR, rC>::static_id())
-      return Function::Constant<E, D, d, R, rR, rC>::create(settings);
-    else if (type == Function::Expression<E, D, d, R, rR, rC>::static_id())
-      return Function::Expression<E, D, d, R, rR, rC>::create(settings);
-    else if (type == Function::Checkerboard<E, D, d, R, rR, rC>::static_id())
-      return Function::Checkerboard<E, D, d, R, rR, rC>::create(settings);
-    else if (type == Function::Spe10Model1<E, D, d, R, rR, rC>::static_id())
-      return Function::Spe10Model1<E, D, d, R, rR, rC>::create(settings);
-    else
-      DUNE_THROW(Dune::RangeError,
-                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " unknown function '" << type
-                      << "' requested!");
-  } // ... create(...)
+  static LocalizableFunctionInterface<E, D, d, R, r, rC>*
+  create(const std::string /*type = available()[0]*/, const ParameterTree /*settings = defaultSettings()*/) = delete;
 }; // class Functions
 
+#define DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE(ddim)                                                                  \
+  DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE_COLS(ddim, 1)                                                                \
+  DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE_COLS(ddim, 2)                                                                \
+  DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE_COLS(ddim, 3)
 
-template <class E, class D, class R>
-class Functions<E, D, 1, R, 1, 1>
-{
-  static const unsigned int d  = 1;
-  static const unsigned int rR = 1;
-  static const unsigned int rC = 1;
+#define DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE_COLS(ddim, rdim)                                                       \
+  DUNE_STUFF_FUNCTIONS_GENERATE_CLASS(ddim, rdim, 1)                                                                   \
+  DUNE_STUFF_FUNCTIONS_GENERATE_CLASS(ddim, rdim, 2)                                                                   \
+  DUNE_STUFF_FUNCTIONS_GENERATE_CLASS(ddim, rdim, 3)
 
-public:
-  static std::vector<std::string> available()
-  {
-    return {Function::Constant<E, D, d, R, rR, rC>::static_id(),
-            Function::Expression<E, D, d, R, rR, rC>::static_id(),
-            Function::Checkerboard<E, D, d, R, rR, rC>::static_id(),
-            Function::Spe10Model1<E, D, d, R, rR, rC>::static_id()};
-  } // ... available(...)
+#define DUNE_STUFF_FUNCTIONS_GENERATE_CLASS(ddim, rdim, rCdim)                                                         \
+  template <class E, class D, class R>                                                                                 \
+  class Functions<E, D, ddim, R, rdim, rCdim>                                                                          \
+  {                                                                                                                    \
+    static const int d  = ddim;                                                                                        \
+    static const int r  = rdim;                                                                                        \
+    static const int rC = rCdim;                                                                                       \
+                                                                                                                       \
+  public:                                                                                                              \
+    static std::vector<std::string> available();                                                                       \
+                                                                                                                       \
+    static Dune::ParameterTree defaultSettings(const std::string type = available()[0]);                               \
+                                                                                                                       \
+    static LocalizableFunctionInterface<E, D, ddim, R, rdim, rCdim>*                                                   \
+    create(const std::string type = available()[0], const ParameterTree settings = defaultSettings());                 \
+  }; // class Functions< ..., ddim, ..., rdim, rCdim >
 
-  static Dune::ParameterTree defaultSettings(const std::string type = available()[0])
-  {
-    if (type == Function::Constant<E, D, d, R, rR, rC>::static_id())
-      return Function::Constant<E, D, d, R, rR, rC>::defaultSettings();
-    else if (type == Function::Expression<E, D, d, R, rR, rC>::static_id())
-      return Function::Expression<E, D, d, R, rR, rC>::defaultSettings();
-    else if (type == Function::Checkerboard<E, D, d, R, rR, rC>::static_id())
-      return Function::Checkerboard<E, D, d, R, rR, rC>::defaultSettings();
-    else if (type == Function::Spe10Model1<E, D, d, R, rR, rC>::static_id())
-      return Function::Spe10Model1<E, D, d, R, rR, rC>::defaultSettings();
-    else
-      DUNE_THROW(Dune::RangeError,
-                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " unknown function '" << type
-                      << "' requested!");
-  } // ... defaultSettings(...)
+DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE(1)
+DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE(2)
+DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE(3)
 
-  static LocalizableFunctionInterface<E, D, d, R, rR, rC>*
-  create(const std::string type = available()[0], const Dune::ParameterTree settings = defaultSettings())
-  {
-    if (type == Function::Constant<E, D, d, R, rR, rC>::static_id())
-      return Function::Constant<E, D, d, R, rR, rC>::create(settings);
-    else if (type == Function::Expression<E, D, d, R, rR, rC>::static_id())
-      return Function::Expression<E, D, d, R, rR, rC>::create(settings);
-    else if (type == Function::Checkerboard<E, D, d, R, rR, rC>::static_id())
-      return Function::Checkerboard<E, D, d, R, rR, rC>::create(settings);
-    else if (type == Function::Spe10Model1<E, D, d, R, rR, rC>::static_id())
-      return Function::Spe10Model1<E, D, d, R, rR, rC>::create(settings);
-    else
-      DUNE_THROW(Dune::RangeError,
-                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:") << " unknown function '" << type
-                      << "' requested!");
-  } // ... create(...)
-}; // class Functions< ..., 1, ..., 1, 1 >
-
-
-// template< class D, class R >
-// class Functions< D, 2, R, 1, 1 >
-//{
-// public:
-//  static std::vector< std::string > available()
-//  {
-//    return {
-//        FunctionExpression< D, 2, R, 1, 1 >::static_id()
-//      , FunctionCheckerboard< D, 2, R, 1, 1 >::static_id()
-//      , FunctionConstant< D, 2, R, 1, 1 >::static_id()
-//    };
-//  } // ... available(...)
-
-//  static Dune::ParameterTree defaultSettings(const std::string type = available()[0])
-//  {
-//    if (type == FunctionCheckerboard< D, 2, R, 1, 1 >::static_id())
-//      return FunctionCheckerboard< D, 2, R, 1, 1 >::defaultSettings();
-//    else if (type == FunctionExpression< D, 2, R, 1, 1 >::static_id())
-//      return FunctionExpression< D, 2, R, 1, 1 >::defaultSettings();
-//    else if (type == FunctionConstant< D, 2, R, 1, 1 >::static_id())
-//      return FunctionConstant< D, 2, R, 1, 1 >::defaultSettings();
-//    else
-//      DUNE_THROW(Dune::RangeError,
-//                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
-//                 << " unknown function '" << type << "' requested!");
-//  } // ... defaultSettings(...)
-
-//  static FunctionInterface< D, 2, R, 1, 1 >* create(const std::string type = available()[0],
-//                                                      const Dune::ParameterTree settings = defaultSettings())
-//  {
-//    if (type == FunctionCheckerboard< D, 2, R, 1, 1 >::static_id())
-//      return FunctionCheckerboard< D, 2, R, 1, 1 >::create(settings);
-//    else if (type == FunctionExpression< D, 2, R, 1, 1 >::static_id())
-//      return FunctionExpression< D, 2, R, 1, 1 >::create(settings);
-//    else if (type == FunctionConstant< D, 2, R, 1, 1 >::static_id())
-//      return FunctionConstant< D, 2, R, 1, 1 >::create(settings);
-//    else
-//      DUNE_THROW(Dune::RangeError,
-//                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
-//                 << " unknown function '" << type << "' requested!");
-//  } // ... create(...)
-//}; // class Functions< D, 2, R, 1, 1 >
-
-
-// template< class D, class R >
-// class Functions< D, 3, R, 1, 1 >
-//{
-// public:
-//  static std::vector< std::string > available()
-//  {
-//    return {
-//        FunctionExpression< D, 3, R, 1, 1 >::static_id()
-//      , FunctionCheckerboard< D, 3, R, 1, 1 >::static_id()
-//      , FunctionConstant< D, 3, R, 1, 1 >::static_id()
-//    };
-//  } // ... available(...)
-
-//  static Dune::ParameterTree defaultSettings(const std::string type = available()[0])
-//  {
-//    if (type == FunctionCheckerboard< D, 3, R, 1, 1 >::static_id())
-//      return FunctionCheckerboard< D, 3, R, 1, 1 >::defaultSettings();
-//    else if (type == FunctionExpression< D, 3, R, 1, 1 >::static_id())
-//      return FunctionExpression< D, 3, R, 1, 1 >::defaultSettings();
-//    else if (type == FunctionConstant< D, 3, R, 1, 1 >::static_id())
-//      return FunctionConstant< D, 3, R, 1, 1 >::defaultSettings();
-//    else
-//      DUNE_THROW(Dune::RangeError,
-//                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
-//                 << " unknown function '" << type << "' requested!");
-//  } // ... defaultSettings(...)
-
-//  static FunctionInterface< D, 3, R, 1, 1 >* create(const std::string type = available()[0],
-//                                                      const Dune::ParameterTree settings = defaultSettings())
-//  {
-//    if (type == FunctionCheckerboard< D, 3, R, 1, 1 >::static_id())
-//      return FunctionCheckerboard< D, 3, R, 1, 1 >::create(settings);
-//    else if (type == FunctionExpression< D, 3, R, 1, 1 >::static_id())
-//      return FunctionExpression< D, 3, R, 1, 1 >::create(settings);
-//    else if (type == FunctionConstant< D, 3, R, 1, 1 >::static_id())
-//      return FunctionConstant< D, 3, R, 1, 1 >::create(settings);
-//    else
-//      DUNE_THROW(Dune::RangeError,
-//                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
-//                 << " unknown function '" << type << "' requested!");
-//  } // ... create(...)
-//}; // class Functions< D, 3, R, 1, 1 >
-
-
-// template< class D, int d, class R, int r >
-// class Functions< D, d, R, r, 1 >
-//{
-// public:
-//  static std::vector< std::string > available()
-//  {
-//    return {
-//        FunctionExpression< D, d, R, r, 1 >::static_id()
-//    };
-//  } // ... available(...)
-
-//  static Dune::ParameterTree defaultSettings(const std::string type = available()[0])
-//  {
-//    if (type == FunctionExpression< D, d, R, r, 1 >::static_id())
-//      return FunctionExpression< D, d, R, r, 1 >::defaultSettings();
-//    else
-//      DUNE_THROW(Dune::RangeError,
-//                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
-//                 << " unknown function '" << type << "' requested!");
-//  } // ... defaultSettings(...)
-
-//  static FunctionInterface< D, d, R, r, 1 >* create(const std::string type = available()[0],
-//                                                      const Dune::ParameterTree settings = defaultSettings())
-//  {
-//    if (type == FunctionExpression< D, d, R, r, 1 >::static_id())
-//      return FunctionExpression< D, d, R, r, 1 >::create(settings);
-//    else
-//      DUNE_THROW(Dune::RangeError,
-//                 "\n" << Dune::Stuff::Common::colorStringRed("ERROR:")
-//                 << " unknown function '" << type << "' requested!");
-//  } // ... create(...)
-//}; // class Functions< D, d, R, r, 1 >
-
+#undef DUNE_STUFF_FUNCTIONS_GENERATE_CLASS
+#undef DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE_COLS
+#undef DUNE_STUFF_FUNCTIONS_GENERATE_DIM_RANGE
 
 } // namespace Stuff
 } // namespace Dune
