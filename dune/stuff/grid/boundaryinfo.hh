a65a59498382e31506738f4d4b8414f86c2791ec
Felix Albrecht <mail@felixalbrecht.de>
2013-12-02 14:56:48 +0100

    [grid.boundaryinfo] remove config.h include

--- dune/stuff/grid/boundaryinfo.hh
+++ dune/stuff/grid/boundaryinfo.hh
@@ -6,12 +6,6 @@
 #ifndef DUNE_STUFF_GRID_BOUNDARYINFO_HH
 #define DUNE_STUFF_GRID_BOUNDARYINFO_HH
 
-#ifdef HAVE_CMAKE_CONFIG
-#include "cmake_config.h"
-#elif defined(HAVE_CONFIG_H)
-#include "config.h"
-#endif // ifdef HAVE_CMAKE_CONFIG
-
 #include <map>
 #include <set>
 #include <string>
