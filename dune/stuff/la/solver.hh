a17441b427070267f55767e8b352f6ec1954f60a
Felix Albrecht <mail@felixalbrecht.de>
2014-01-07 12:09:38 +0100

    [la.solver] major update * there is now a class Solver< MatrixType > that is   specialized for each matrix type * the solver is selected by passing an option to the   apply method

--- dune/stuff/la/solver.hh
+++ dune/stuff/la/solver.hh
@@ -1,78 +1,126 @@
+// This file is part of the dune-stuff project:
+//   https://users.dune-project.org/projects/dune-stuff/
+// Copyright Holders: Felix Albrecht, Rene Milk
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
 #ifndef DUNE_STUFF_LA_SOLVER_HH
 #define DUNE_STUFF_LA_SOLVER_HH
 
 #include <string>
 #include <vector>
 
-#include <dune/stuff/common/color.hh>
-#include <dune/stuff/common/parameter/tree.hh>
+#include <dune/stuff/common/type_utils.hh>
 
-#include "solver/interface.hh"
-#include "solver/eigen.hh"
-#include "solver/fasp.hh"
+#include <dune/stuff/common/exceptions.hh>
+#include <dune/stuff/common/configtree.hh>
+
+#include "container/interfaces.hh"
+#include "container/dunedynamic.hh"
+#include "container/eigen.hh"
 
 namespace Dune {
 namespace Stuff {
 namespace LA {
 
 
-static inline std::vector<std::string> solverTypes()
+class SolverUtils
 {
-  return
+protected:
+  static void check_given(const std::string& type, const std::vector<std::string>& opts)
   {
-    "bicgstab.ilut", "bicgstab.diagonal", "bicgstab", "cg", "cg.diagonal", "direct.superlu"
-#if HAVE_FASP
-        ,
-        "amg.fasp"
-#endif // FAVE_FASP
-  };
-} // std::vector< std::string > types()
-
-
-template <class MatrixType, class VectorType>
-Dune::ParameterTree solverDefaultSettings(const std::string type = solverTypes()[0])
-{
-  if (type == "bicgstab.ilut") {
-    return Dune::Stuff::LA::BicgstabILUTSolver<MatrixType, VectorType>::defaultSettings();
-#if HAVE_FASP
-  } else if (type == "amg.fasp") {
-    return Dune::Stuff::LA::AmgFaspSolver<MatrixType, VectorType>::defaultSettings();
-#endif // HAVE_FASP
+    if (std::find(opts.begin(), opts.end(), type) == opts.end()) {
+      std::stringstream ss;
+      for (auto opt : opts)
+        ss << opt << " ";
+      DUNE_THROW_COLORFULLY(Exception::configuration_error,
+                            "Given type '" << type << "' not supported (see below for a list of supported ones). "
+                                           << "Call options() first!\n"
+                                           << ss.str());
+    }
   }
-  return SolverInterface<MatrixType, VectorType>::defaultIterativeSettings();
-} // Dune::ParameterTree defaultSettings(const std::string type)
+};
 
 
-template <class MatrixType, class VectorType>
-SolverInterface<MatrixType, VectorType>* createSolver(const std::string type = solverTypes()[0])
+template <class MatrixImp>
+class Solver
 {
-  if (type == "cg") {
-    return new Dune::Stuff::LA::CgSolver<MatrixType, VectorType>();
-  } else if (type == "cg.diagonal") {
-    return new Dune::Stuff::LA::CgDiagonalSolver<MatrixType, VectorType>();
-  } else if (type == "bicgstab") {
-    return new Dune::Stuff::LA::BicgstabSolver<MatrixType, VectorType>();
-  } else if (type == "bicgstab.diagonal") {
-    return new Dune::Stuff::LA::BicgstabDiagonalSolver<MatrixType, VectorType>();
-  } else if (type == "bicgstab.ilut") {
-    return new Dune::Stuff::LA::BicgstabILUTSolver<MatrixType, VectorType>();
-  } else if (type == "direct.superlu") {
-    return new Dune::Stuff::LA::DirectSuperLuSolver<MatrixType, VectorType>();
-#if HAVE_FASP
-  } else if (type == "amg.fasp") {
-    return new Dune::Stuff::LA::AmgFaspSolver<MatrixType, VectorType>();
-#endif // HAVE_FASP
-  } else
-    DUNE_THROW(Dune::RangeError,
-               "\n" << Dune::Stuff::Common::colorString("ERROR:", Dune::Stuff::Common::Colors::red)
-                    << " unknown linear solver '"
-                    << type
-                    << "' requested!");
-} // SolverInterface< ElementType >* create(const std::string type = "eigen.bicgstab.incompletelut")
+  static_assert(AlwaysFalse<MatrixImp>::value, "This is the unspecialized version of LA::Solver< ... >. Please include "
+                                               "the correct header for your matrix implementation!");
+
+public:
+  typedef MatrixImp MatrixType;
+
+  Solver(const MatrixType& /*matrix*/)
+  {
+    DUNE_THROW_COLORFULLY(NotImplemented,
+                          "This is the unspecialized version of LA::Solver< ... >. "
+                          "Please include the correct header for your matrix implementation '"
+                              << Common::Typename<MatrixType>::value()
+                              << "'!");
+  }
+
+  static std::vector<std::string> options()
+  {
+    DUNE_THROW_COLORFULLY(NotImplemented,
+                          "This is the unspecialized version of LA::Solver< ... >. "
+                          "Please include the correct header for your matrix implementation '"
+                              << Common::Typename<MatrixType>::value()
+                              << "'!");
+  }
+
+  static Common::ConfigTree options(const std::string& /*type*/)
+  {
+    DUNE_THROW_COLORFULLY(NotImplemented,
+                          "This is the unspecialized version of LA::Solver< ... >. "
+                          "Please include the correct header for your matrix implementation '"
+                              << Common::Typename<MatrixType>::value()
+                              << "'!");
+  }
+
+  /**
+   *  \return Returns 0 if the solve was successfull. Otherwise
+   *          * 1 if the given matrix did not fulfill the requirements
+   *          * 2 if an iterative solver did not converge
+   *          * 3 if the solver was not set up correctly
+   *          * 4 if the computed solution does not solve the system (only if NDEBUG is not defined)
+   */
+  template <class RhsType, class SolutionType>
+  size_t apply(const RhsType& /*rhs*/, SolutionType& /*solution*/) const
+  {
+    DUNE_THROW_COLORFULLY(NotImplemented,
+                          "This is the unspecialized version of LA::Solver< ... >. "
+                          "Please include the correct header for your matrix implementation '"
+                              << Common::Typename<MatrixType>::value()
+                              << "'!");
+  }
+
+  template <class RhsType, class SolutionType>
+  size_t apply(const RhsType& /*rhs*/, SolutionType& /*solution*/, const std::string& /*type*/) const
+  {
+    DUNE_THROW_COLORFULLY(NotImplemented,
+                          "This is the unspecialized version of LA::Solver< ... >. "
+                          "Please include the correct header for your matrix implementation '"
+                              << Common::Typename<MatrixType>::value()
+                              << "'!");
+  }
+
+  template <class RhsType, class SolutionType>
+  size_t apply(const RhsType& /*rhs*/, SolutionType& /*solution*/, const Common::ConfigTree& /*options*/) const
+  {
+    DUNE_THROW_COLORFULLY(NotImplemented,
+                          "This is the unspecialized version of LA::Solver< ... >. "
+                          "Please include the correct header for your matrix implementation '"
+                              << Common::Typename<MatrixType>::value()
+                              << "'!");
+  }
+}; // class Solver
 
 
 } // namespace LA
 } // namespace Stuff
 } // namespace Dune
+
+#include "solver/dunedynamic.hh"
+#include "solver/eigen.hh"
 
 #endif // DUNE_STUFF_LA_SOLVER_HH
