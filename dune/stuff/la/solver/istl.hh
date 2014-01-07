50c5511716a5c723f534de1eb2ba3bc089ff64a3
Felix Albrecht <mail@felixalbrecht.de>
2014-01-07 14:39:01 +0100

    [la.solver.istl] updated to new layout

--- dune/stuff/la/solver/istl.hh
+++ dune/stuff/la/solver/istl.hh
@@ -15,235 +15,155 @@
 #include <dune/istl/solvers.hh>
 #include <dune/istl/paamg/amg.hh>
 
+#include <dune/stuff/common/exceptions.hh>
+#include <dune/stuff/common/configtree.hh>
 #include <dune/stuff/la/container/istl.hh>
 
-#include "interface.hh"
+#include "../solver.hh"
 
 namespace Dune {
 namespace Stuff {
 namespace LA {
 
-template <class M, class V>
-struct IsIstlMV
+
+template <class S>
+class Solver<IstlRowMajorSparseMatrix<S>> : protected SolverUtils
 {
-  typedef typename std::is_same<IstlRowMajorSparseMatrix<typename M::ScalarType>, M>::type Mtype;
-  typedef typename std::is_same<IstlDenseVector<typename V::ScalarType>, V>::type Vtype;
-  static constexpr bool value = Mtype::value && Vtype::value;
-};
-
-
-// template< class MatrixImp, class VectorImp >
-// class CgSolver< MatrixImp, VectorImp, typename std::enable_if< IsIstlMV< MatrixImp, VectorImp >::value >::type >
-//  : public SolverInterface< MatrixImp, VectorImp >
-//{
-//  typedef SolverInterface< MatrixImp, VectorImp > BaseType;
-// public:
-//  typedef typename BaseType::MatrixType MatrixType;
-//  typedef typename BaseType::VectorType VectorType;
-//  typedef typename BaseType::ScalarType ScalarType;
-
-//  virtual size_t apply(const MatrixType& lhs,
-//                       const VectorType& rhs,
-//                       VectorType& solution,
-//                       const Dune::ParameterTree description = BaseType::defaultIterativeSettings()) const
-//  {
-//    assert(false);
-//  } // ... apply(...)
-//}; // class CgSolver
-
-
-// template< class MatrixImp, class VectorImp >
-// class CgDiagonalSolver< MatrixImp, VectorImp, typename std::enable_if< IsIstlMV< MatrixImp, VectorImp >::value
-// >::type >
-//  : public SolverInterface< MatrixImp, VectorImp >
-//{
-//  typedef SolverInterface< MatrixImp, VectorImp > BaseType;
-// public:
-//  typedef typename BaseType::MatrixType MatrixType;
-//  typedef typename BaseType::VectorType VectorType;
-//  typedef typename BaseType::ScalarType ScalarType;
-
-//  virtual size_t apply(const MatrixType& lhs,
-//                       const VectorType& rhs,
-//                       VectorType& solution,
-//                       const Dune::ParameterTree description = BaseType::defaultIterativeSettings()) const
-//  {
-//    assert(false);
-//  } // ... apply(...)
-//}; // class CgDiagonalSolver
-
-
-// template< class MatrixImp, class VectorImp >
-// class BicgstabSolver< MatrixImp, VectorImp, typename std::enable_if< IsIstlMV< MatrixImp, VectorImp >::value >::type
-// >
-//  : public SolverInterface< MatrixImp, VectorImp >
-//{
-//  typedef SolverInterface< MatrixImp, VectorImp > BaseType;
-// public:
-//  typedef typename BaseType::MatrixType MatrixType;
-//  typedef typename BaseType::VectorType VectorType;
-//  typedef typename BaseType::ScalarType ScalarType;
-
-//  virtual size_t apply(const MatrixType& lhs,
-//                       const VectorType& rhs,
-//                       VectorType& solution,
-//                       const Dune::ParameterTree description = BaseType::defaultIterativeSettings()) const
-//  {
-//    assert(false);
-//  } // ... apply(...)
-//}; // class BicgstabSolver
-
-
-// template< class MatrixImp, class VectorImp >
-// class BicgstabDiagonalSolver< MatrixImp, VectorImp,
-//                              typename std::enable_if< IsIstlMV< MatrixImp, VectorImp >::value >::type >
-//  : public SolverInterface< MatrixImp, VectorImp >
-//{
-//  typedef SolverInterface< MatrixImp, VectorImp > BaseType;
-// public:
-//  typedef typename BaseType::MatrixType MatrixType;
-//  typedef typename BaseType::VectorType VectorType;
-//  typedef typename BaseType::ScalarType ScalarType;
-
-//  virtual size_t apply(const MatrixType& lhs,
-//                       const VectorType& rhs,
-//                       VectorType& solution,
-//                       const Dune::ParameterTree description = BaseType::defaultIterativeSettings()) const
-//  {
-//    assert(false);
-//  } // ... apply(...)
-//}; // class BicgstabDiagonalSolver
-
-
-template <class MatrixImp, class VectorImp>
-class BicgstabILUTSolver<MatrixImp, VectorImp, typename std::enable_if<IsIstlMV<MatrixImp, VectorImp>::value>::type>
-    : public SolverInterface<MatrixImp, VectorImp>
-{
-  typedef SolverInterface<MatrixImp, VectorImp> BaseType;
-
 public:
-  typedef typename BaseType::MatrixType MatrixType;
-  typedef typename BaseType::VectorType VectorType;
-  typedef typename BaseType::ScalarType ScalarType;
+  typedef IstlRowMajorSparseMatrix<S> MatrixType;
 
-  static Dune::ParameterTree defaultSettings()
+  Solver(const MatrixType& matrix)
+    : matrix_(matrix)
   {
-    Dune::ParameterTree description                 = BaseType::defaultIterativeSettings();
-    description["precision"]                        = "1.0e-8";
-    description["preconditioner.iterations"]        = "10";
-    description["preconditioner.relaxation_factor"] = "1.0";
-    return description;
-  } // Dune::ParameterTree defaultSettings()
+  }
 
-  virtual size_t apply(const MatrixType& lhs, const VectorType& rhs, VectorType& solution,
-                       const Dune::ParameterTree description = BaseType::defaultIterativeSettings()) const
+  static std::vector<std::string> options()
   {
-    typedef MatrixAdapter<typename MatrixType::BackendType,
-                          typename VectorType::BackendType,
-                          typename VectorType::BackendType> MatrixOperatorType;
-    MatrixOperatorType matrix_operator(*(lhs.backend_));
-    typedef SeqILUn<typename MatrixType::BackendType,
-                    typename VectorType::BackendType,
-                    typename VectorType::BackendType> PreconditionerType;
-    PreconditionerType preconditioner(
-        *(lhs.backend_),
-        description.get("preconditioner.iterations",
-                        defaultSettings().template get<size_t>("preconditioner.iterations")),
-        description.get("preconditioner.relaxation_factor",
-                        defaultSettings().template get<double>("preconditioner.relaxation_factor")));
-    typedef BiCGSTABSolver<typename VectorType::BackendType> SolverType;
-    SolverType solver(matrix_operator,
-                      preconditioner,
-                      description.get("precision", defaultSettings().template get<double>("precision")),
-                      description.get("maxIter", defaultSettings().template get<size_t>("maxIter")),
-                      description.get("verbose", defaultSettings().template get<int>("verbose")));
-    Dune::InverseOperatorResult stat;
-    solver.apply(*(solution.backend_), *(rhs.backend_), stat);
-    if (stat.converged)
-      return 0;
-    else
-      return 3;
+    return {"bicgstab.ilut", "bicgstab.amg.ilu0"};
+  }
+
+  static Common::ConfigTree options(const std::string& type)
+  {
+    SolverUtils::check_given(type, options());
+    Common::ConfigTree iterative_options({"max_iter", "precision", "verbose"}, {"10000", "1e-10", "0"});
+    if (type == "bicgstab.ilut") {
+      iterative_options.add("preconditioner.iterations", "10");
+      iterative_options.add("preconditioner.relaxation_factor", "1.0");
+    } else if (type == "bicgstab.amg.ilu0") {
+      iterative_options.add("smoother.iterations", "1");
+      iterative_options.add("smoother.relaxation_factor", "1");
+      iterative_options.add("smoother.max_level", "15");
+      iterative_options.add("smoother.coarse_target", "2000");
+      iterative_options.add("smoother.min_coarse_rate", "1.2");
+      iterative_options.add("smoother.prolong_damp", "1.6");
+      iterative_options.add("smoother.anisotropy_dim", "2");
+      iterative_options.add("smoother.verbose", "0");
+    } else
+      DUNE_THROW_COLORFULLY(Exception::internal_error,
+                            "Given type '" << type << "' is not supported, although it was reported by options()!");
+    iterative_options.add("type", type);
+    return iterative_options;
+  } // ... options(...)
+
+  size_t apply(const IstlDenseVector<S>& rhs, IstlDenseVector<S>& solution) const
+  {
+    return apply(rhs, solution, options()[0]);
+  }
+
+  size_t apply(const IstlDenseVector<S>& rhs, IstlDenseVector<S>& solution, const std::string& type) const
+  {
+    return apply(rhs, solution, options(type));
   } // ... apply(...)
-}; // class BicgstabILUTSolver
 
-
-// template< class MatrixImp, class VectorImp >
-// class DirectSuperLuSolver< MatrixImp, VectorImp,
-//                           typename std::enable_if< IsIstlMV< MatrixImp, VectorImp >::value >::type >
-//  : public SolverInterface< MatrixImp, VectorImp >
-//{
-//  typedef SolverInterface< MatrixImp, VectorImp > BaseType;
-// public:
-//  typedef typename BaseType::MatrixType MatrixType;
-//  typedef typename BaseType::VectorType VectorType;
-//  typedef typename BaseType::ScalarType ScalarType;
-
-//  DirectSuperLuSolver()
-//  {}
-
-//  virtual size_t apply(const MatrixType& lhs,
-//                       const VectorType& rhs,
-//                       VectorType& solution,
-//                       const Dune::ParameterTree description = BaseType::defaultIterativeSettings()) const
-//  {
-//    assert(false);
-//  } // ... apply(...)
-//}; // class DirectSuperLuSolver
-
-
-template <class MatrixImp, class VectorImp>
-class AmgSolver<MatrixImp, VectorImp, typename std::enable_if<IsIstlMV<MatrixImp, VectorImp>::value>::type>
-    : public SolverInterface<MatrixImp, VectorImp>
-{
-  typedef SolverInterface<MatrixImp, VectorImp> BaseType;
-
-public:
-  typedef typename BaseType::MatrixType MatrixType;
-  typedef typename BaseType::VectorType VectorType;
-  typedef typename BaseType::ScalarType ScalarType;
-
-  static Dune::ParameterTree defaultSettings()
+  /**
+   *  \note does a copy of the rhs
+   */
+  size_t apply(const IstlDenseVector<S>& rhs, IstlDenseVector<S>& solution, const Common::ConfigTree& opts) const
   {
-    return BaseType::defaultIterativeSettings();
-  } // Dune::ParameterTree defaultSettings()
-
-  virtual size_t apply(const MatrixType& lhs, const VectorType& rhs, VectorType& solution,
-                       const Dune::ParameterTree description = BaseType::defaultIterativeSettings()) const
-  {
-    typedef MatrixAdapter<typename MatrixType::BackendType,
-                          typename VectorType::BackendType,
-                          typename VectorType::BackendType> MatrixOperatorType;
-    MatrixOperatorType matrix_operator(*(lhs.backend_));
-    typedef SeqILU0<typename MatrixType::BackendType,
-                    typename VectorType::BackendType,
-                    typename VectorType::BackendType> PreconditionerType;
-    typedef Dune::Amg::AMG<MatrixOperatorType, typename VectorType::BackendType, PreconditionerType> SmootherType;
-    Dune::SeqScalarProduct<typename VectorType::BackendType> scalar_product;
-    typedef typename Dune::Amg::SmootherTraits<PreconditionerType>::Arguments SmootherArgs;
-    SmootherArgs smootherArgs;
-    smootherArgs.iterations       = 1;
-    smootherArgs.relaxationFactor = 1;
-    Dune::Amg::Parameters params(15, 2000);
-    params.setDefaultValuesAnisotropic(2); // <- dim
-    typedef Dune::Amg::CoarsenCriterion<Dune::Amg::SymmetricCriterion<typename MatrixType::BackendType,
-                                                                      Dune::Amg::FirstDiagonal>> AmgCriterion;
-    AmgCriterion amg_criterion(params);
-    SmootherType smoother(matrix_operator, amg_criterion, smootherArgs);
-    typedef BiCGSTABSolver<typename VectorType::BackendType> SolverType;
-    SolverType solver(matrix_operator,
-                      scalar_product,
-                      smoother,
-                      description.get("precision", defaultSettings().template get<double>("precision")),
-                      description.get("maxIter", defaultSettings().template get<size_t>("maxIter")),
-                      description.get("verbose", defaultSettings().template get<int>("verbose")));
-    Dune::InverseOperatorResult stat;
-    solver.apply(*(solution.backend_), *(rhs.backend_), stat);
-    if (stat.converged)
-      return 0;
-    else
-      return 3;
+    if (!opts.has_key("type"))
+      DUNE_THROW_COLORFULLY(Exception::configuration_error,
+                            "Given options (see below) need to have at least the key 'type' set!\n\n" << opts);
+    const auto type = opts.get<std::string>("type");
+    this->check_given(type, options());
+    const Common::ConfigTree default_opts = options(type);
+    IstlDenseVector<S> writable_rhs       = rhs.copy();
+    // solve
+    if (type == "bicgstab.ilut") {
+      typedef MatrixAdapter<typename MatrixType::BackendType,
+                            typename IstlDenseVector<S>::BackendType,
+                            typename IstlDenseVector<S>::BackendType> MatrixOperatorType;
+      MatrixOperatorType matrix_operator(matrix_.backend());
+      typedef SeqILUn<typename MatrixType::BackendType,
+                      typename IstlDenseVector<S>::BackendType,
+                      typename IstlDenseVector<S>::BackendType> PreconditionerType;
+      PreconditionerType preconditioner(
+          matrix_.backend(),
+          opts.get("preconditioner.iterations", default_opts.get<size_t>("preconditioner.iterations")),
+          opts.get("preconditioner.relaxation_factor", default_opts.get<S>("preconditioner.relaxation_factor")));
+      typedef BiCGSTABSolver<typename IstlDenseVector<S>::BackendType> SolverType;
+      SolverType solver(matrix_operator,
+                        preconditioner,
+                        opts.get("precision", default_opts.get<S>("precision")),
+                        opts.get("max_iter", default_opts.get<size_t>("max_iter")),
+                        opts.get("verbose", default_opts.get<int>("verbose")));
+      InverseOperatorResult stat;
+      solver.apply(solution.backend(), writable_rhs.backend(), stat);
+      if (!stat.converged)
+        return 2;
+    } else if (type == "bicgstab.amg.ilu0") {
+      typedef MatrixAdapter<typename MatrixType::BackendType,
+                            typename IstlDenseVector<S>::BackendType,
+                            typename IstlDenseVector<S>::BackendType> MatrixOperatorType;
+      MatrixOperatorType matrix_operator(matrix_.backend());
+      typedef SeqILU0<typename MatrixType::BackendType,
+                      typename IstlDenseVector<S>::BackendType,
+                      typename IstlDenseVector<S>::BackendType> PreconditionerType;
+      typedef Dune::Amg::AMG<MatrixOperatorType, typename IstlDenseVector<S>::BackendType, PreconditionerType>
+          SmootherType;
+      Dune::SeqScalarProduct<typename IstlDenseVector<S>::BackendType> scalar_product;
+      typedef typename Dune::Amg::SmootherTraits<PreconditionerType>::Arguments SmootherArgs;
+      SmootherArgs smootherArgs;
+      smootherArgs.iterations = opts.get("smoother.iterations", default_opts.get<size_t>("smoother.iterations"));
+      smootherArgs.relaxationFactor =
+          opts.get("smoother.relaxation_factor", default_opts.get<S>("smoother.relaxation_factor"));
+      Dune::Amg::Parameters params(
+          opts.get("smoother.max_level", default_opts.get<size_t>("smoother.max_level")),
+          opts.get("smoother.coarse_target", default_opts.get<size_t>("smoother.coarse_target")),
+          opts.get("smoother.min_coarse_rate", default_opts.get<S>("smoother.min_coarse_rate")),
+          opts.get("smoother.prolong_damp", default_opts.get<S>("smoother.prolong_damp")));
+      params.setDefaultValuesAnisotropic(opts.get("smoother.anisotropy_dim",
+                                                  default_opts.get<size_t>("smoother.anisotropy_dim"))); // <- dim
+      typedef Dune::Amg::CoarsenCriterion<Dune::Amg::SymmetricCriterion<typename MatrixType::BackendType,
+                                                                        Dune::Amg::FirstDiagonal>> AmgCriterion;
+      AmgCriterion amg_criterion(params);
+      amg_criterion.setDebugLevel(opts.get("smoother.verbose", default_opts.get<size_t>("smoother.verbose")));
+      SmootherType smoother(matrix_operator, amg_criterion, smootherArgs);
+      typedef BiCGSTABSolver<typename IstlDenseVector<S>::BackendType> SolverType;
+      SolverType solver(matrix_operator,
+                        scalar_product,
+                        smoother,
+                        opts.get("precision", default_opts.get<S>("precision")),
+                        opts.get("max_iter", default_opts.get<size_t>("max_iter")),
+                        opts.get("verbose", default_opts.get<size_t>("verbose")));
+      InverseOperatorResult stat;
+      solver.apply(solution.backend(), writable_rhs.backend(), stat);
+      if (!stat.converged)
+        return 2;
+    } else
+      DUNE_THROW_COLORFULLY(Exception::internal_error,
+                            "Given type '" << type << "' is not supported, although it was reported by options()!");
+// check
+#ifndef NDEBUG
+    matrix_.mv(solution, writable_rhs);
+    if (!rhs.almost_equal(writable_rhs))
+      return 4;
+#endif // NDEBUG
+    return 0;
   } // ... apply(...)
-}; // class AmgSolver
+
+private:
+  const MatrixType& matrix_;
+}; // class Solver
 
 
 } // namespace LA
