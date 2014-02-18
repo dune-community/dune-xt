cfc82b65ff3186d57054a83810fccb2714cbb685
Felix Schindler <felix@schindlerfamily.de>
2014-02-18 11:39:38 +0100

    [la.solver] changed api * apply() does not return an error code any more * each solver is required to throw an appropriate exception in case of any   errors

--- dune/stuff/la/solver.hh
+++ dune/stuff/la/solver.hh
@@ -10,7 +10,6 @@
 #include <vector>
 
 #include <dune/stuff/common/type_utils.hh>
-
 #include <dune/stuff/common/exceptions.hh>
 #include <dune/stuff/common/configtree.hh>
 
@@ -20,6 +19,24 @@
 
 namespace Dune {
 namespace Stuff {
+namespace Exceptions {
+
+
+class linear_solver_failed_bc_matrix_did_not_fulfill_requirements : public linear_solver_failed
+{
+};
+class linear_solver_failed_bc_it_did_not_converge : public linear_solver_failed
+{
+};
+class linear_solver_failed_bc_it_was_not_set_up_correctly : public linear_solver_failed
+{
+};
+class linear_solver_failed_bc_the_solution_does_not_solve_the_system : public linear_solver_failed
+{
+};
+
+
+} // namespace Exceptions
 namespace LA {
 
 
@@ -78,14 +95,11 @@ public:
   }
 
   /**
-   *  \return Returns 0 if the solve was successfull. Otherwise
-   *          * 1 if the given matrix did not fulfill the requirements
-   *          * 2 if an iterative solver did not converge
-   *          * 3 if the solver was not set up correctly
-   *          * 4 if the computed solution does not solve the system (only if NDEBUG is not defined)
+   *  Throws any of the above exceptions, if there was a problem. If none was thrown we beleive that a suitable solution
+   *  was found (given the current options).
    */
   template <class RhsType, class SolutionType>
-  size_t apply(const RhsType& /*rhs*/, SolutionType& /*solution*/) const
+  void apply(const RhsType& /*rhs*/, SolutionType& /*solution*/) const
   {
     DUNE_THROW_COLORFULLY(NotImplemented,
                           "This is the unspecialized version of LA::Solver< ... >. "
@@ -95,7 +109,7 @@ public:
   }
 
   template <class RhsType, class SolutionType>
-  size_t apply(const RhsType& /*rhs*/, SolutionType& /*solution*/, const std::string& /*type*/) const
+  void apply(const RhsType& /*rhs*/, SolutionType& /*solution*/, const std::string& /*type*/) const
   {
     DUNE_THROW_COLORFULLY(NotImplemented,
                           "This is the unspecialized version of LA::Solver< ... >. "
@@ -105,7 +119,7 @@ public:
   }
 
   template <class RhsType, class SolutionType>
-  size_t apply(const RhsType& /*rhs*/, SolutionType& /*solution*/, const Common::ConfigTree& /*options*/) const
+  void apply(const RhsType& /*rhs*/, SolutionType& /*solution*/, const Common::ConfigTree& /*options*/) const
   {
     DUNE_THROW_COLORFULLY(NotImplemented,
                           "This is the unspecialized version of LA::Solver< ... >. "
