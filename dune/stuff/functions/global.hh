1e9e4f1d0e3688bee4e715bf1f177a80376ff614
Felix Schindler <felix@schindlerfamily.de>
2014-03-24 16:30:20 +0100

    [functions.global] added matrix-valued variant

--- dune/stuff/functions/global.hh
+++ dune/stuff/functions/global.hh
@@ -20,20 +20,106 @@
 namespace Dune {
 namespace Stuff {
 
-#if HAVE_DUNE_PDELAB
 
-#endif
+/**
+ * base class for global matrix-valued valued functions that provides automatic local functions via
+ * LocalizableFunctionInterface
+ */
+template <class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim, int rangeDimCols>
+class GlobalFunction
+    : public LocalizableFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols>
+{
+  typedef GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols> ThisType;
+  typedef LocalfunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols>
+      LocalfunctionBaseType;
+
+public:
+  typedef typename LocalfunctionBaseType::DomainFieldType DomainFieldType;
+  static const unsigned int dimDomain = LocalfunctionBaseType::dimDomain;
+  typedef typename LocalfunctionBaseType::DomainType DomainType;
+
+  typedef typename LocalfunctionBaseType::RangeFieldType RangeFieldType;
+  static const unsigned int dimRange     = LocalfunctionBaseType::dimRange;
+  static const unsigned int dimRangeCols = LocalfunctionBaseType::dimRangeCols;
+  typedef typename LocalfunctionBaseType::RangeType RangeType;
+
+  typedef typename LocalfunctionBaseType::JacobianRangeType JacobianRangeType;
+
+  virtual ~GlobalFunction()
+  {
+  }
+
+  virtual std::string name() const
+  {
+    return "dune.stuff.functions.global";
+  }
+
+  virtual ThisType* copy() const
+  {
+    DUNE_THROW(NotImplemented, "not needed, no meaningful default implementation possible -> exception");
+  }
+
+  virtual size_t order() const
+  {
+    return std::numeric_limits<size_t>::max();
+  }
+
+  virtual void evaluate(const DomainType& xx, RangeType& ret) const = 0;
+
+  virtual void jacobian(const DomainType& /*x*/, JacobianRangeType& /*ret*/) const
+  {
+    DUNE_THROW(NotImplemented, "This does not make sense yet for matrix-valued functions!");
+  }
+
+  class Localfunction : public LocalfunctionBaseType
+  {
+  public:
+    Localfunction(const EntityImp& entity, const ThisType& global_function)
+      : LocalfunctionBaseType(entity)
+      , geometry_(entity.geometry())
+      , global_function_(global_function)
+    {
+    }
+
+    virtual void evaluate(const DomainType& xx, RangeType& ret) const
+    {
+      const auto xx_global = geometry_.global(xx);
+      global_function_.evaluate(xx_global, ret);
+    }
+
+    virtual void jacobian(const DomainType& xx, JacobianRangeType& ret) const
+    {
+      const auto xx_global = geometry_.global(xx);
+      global_function_.jacobian(xx_global, ret);
+    }
+
+    virtual size_t order() const
+    {
+      return global_function_.order();
+    }
+
+  private:
+    const typename EntityImp::Geometry& geometry_;
+    const ThisType& global_function_;
+  };
+
+  virtual std::unique_ptr<LocalfunctionBaseType> local_function(const EntityImp& entity) const
+  {
+    return Common::make_unique<Localfunction>(entity, *this);
+  }
+}; // class GlobalFunction
+
 
 /**
  * base class for global valued functions that provides automatic local functions via LocalizableFunctionInterface
  */
 template <class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim>
-class GlobalFunction
+class GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>
     : public LocalizableFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>
 #if HAVE_DUNE_FEM
       ,
       public Dune::Fem::Function<Dune::Fem::FunctionSpace<DomainFieldImp, RangeFieldImp, domainDim, rangeDim>,
-                                 GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim>>
+                                 GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>>
 #endif // HAVE_DUNE_FEM
 #if HAVE_DUNE_PDELAB
       ,
@@ -41,10 +127,10 @@ class GlobalFunction
       public PDELab::FunctionInterface<PDELab::FunctionTraits<DomainFieldImp, domainDim,
                                                               FieldVector<DomainFieldImp, domainDim>, RangeFieldImp,
                                                               rangeDim, FieldVector<RangeFieldImp, rangeDim>>,
-                                       GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim>>
+                                       GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>>
 #endif
 {
-  typedef GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim> ThisType;
+  typedef GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1> ThisType;
   typedef LocalfunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>
       LocalfunctionBaseType;
 
@@ -60,8 +146,8 @@ public:
   typedef Dune::FieldVector<RangeFieldType, dimRange> RangeType;
 #if HAVE_DUNE_FEM
   typedef typename Dune::Fem::Function<Dune::Fem::FunctionSpace<DomainFieldImp, RangeFieldImp, domainDim, rangeDim>,
-                                       GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp,
-                                                      rangeDim>>::JacobianRangeType JacobianRangeType;
+                                       GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim,
+                                                      1>>::JacobianRangeType JacobianRangeType;
 #else
   typedef typename LocalfunctionBaseType::JacobianRangeType JacobianRangeType;
 #endif
@@ -127,13 +213,14 @@ public:
   {
     return Common::make_unique<Localfunction>(entity, *this);
   }
-}; // class GlobalFunction
+}; // class GlobalFunction< ..., 1 >
 
 
-template <class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim>
-class GlobalConstantFunction : public GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim>
+template <class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim, int rangeDimCols>
+class GlobalConstantFunction
+    : public GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols>
 {
-  typedef GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim> BaseType;
+  typedef GlobalFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols> BaseType;
   typedef typename BaseType::RangeType RangeType;
   typedef typename BaseType::DomainType DomainType;
   typedef typename BaseType::JacobianRangeType JacobianRangeType;
@@ -161,7 +248,7 @@ public:
 
   virtual void jacobian(const DomainType& /*x*/, JacobianRangeType& ret) const DS_FINAL
   {
-    ret = JacobianRangeType(0);
+    ret *= 0.0;
   }
 
 private:
