4a43456e77022243748889159499eef5b9cdfee2
Tobias Leibner <tobias.leibner@googlemail.com>
2014-05-04 14:42:54 +0200

    [functions.spe10] fix variable names

--- dune/stuff/functions/spe10.hh
+++ dune/stuff/functions/spe10.hh
@@ -132,17 +132,13 @@ public:
                                           const std::string sub_name = static_id())
   {
     // get correct config
-    Common::ConfigTree cfg;
-    if (config.has_sub(sub_name))
-      cfg = config.sub(sub_name);
-    else
-      cfg = config;
+    const Common::ConfigTree cfg = config.has_sub(sub_name) ? config.sub(sub_name) : config;
     // extract needed data
     const auto filename = cfg.get<std::string>("filename");
     auto lower_left     = cfg.get<std::vector<DomainFieldType>>("lower_left", dimDomain);
     auto upper_right    = cfg.get<std::vector<DomainFieldType>>("upper_right", dimDomain);
-    const auto min_val  = cfg.get<RangeFieldType>("minValue", minValue);
-    const auto max_val  = cfg.get<RangeFieldType>("maxValue", maxValue);
+    const auto min_val  = cfg.get<RangeFieldType>("min_value", minValue);
+    const auto max_val  = cfg.get<RangeFieldType>("max_value", maxValue);
     const auto nm       = cfg.get<std::string>("name", static_id());
     // create and return, leave the checks to the constructor
     return Common::make_unique<ThisType>(filename, std::move(lower_left), std::move(upper_right), min_val, max_val, nm);
