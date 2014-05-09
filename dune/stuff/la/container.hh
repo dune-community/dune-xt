caae58c5e6f30d408f93ca4e5272db95c7f03130
Felix Schindler <felix@schindlerfamily.de>
2014-05-09 09:45:41 +0200

    [la.container] rewrote Container * provides {Vector,Matrix}Type for a given backend

--- dune/stuff/la/container.hh
+++ dune/stuff/la/container.hh
@@ -18,120 +18,40 @@ namespace Stuff {
 namespace LA {
 
 
-template <class ContainerImp>
-class Container
+template <class ScalarType, ChooseBackend backend>
+struct Container;
+
+
+template <class ScalarType>
+struct Container<ScalarType, ChooseBackend::common_dense>
 {
-public:
-  static ContainerImp create(const size_t /*size*/)
-  {
-    static_assert(Dune::AlwaysFalse<ContainerImp>::value, "Please specialize this class for this ContainerImp!");
-  }
-};
+  typedef CommonDenseVector<ScalarType> VectorType;
+  typedef CommonDenseMatrix<ScalarType> MatrixType;
+}; // struct Container< ..., common_dense >
 
 
-template <class S>
-class Container<CommonDenseVector<S>>
+template <class ScalarType>
+struct Container<ScalarType, ChooseBackend::eigen_dense>
 {
-public:
-  static CommonDenseVector<S> create(const size_t size)
-  {
-    return CommonDenseVector<S>(size, S(1));
-  }
-};
+  typedef EigenDenseVector<ScalarType> VectorType;
+  typedef EigenDenseMatrix<ScalarType> MatrixType;
+}; // struct Container< ..., eigen_dense >
 
-template <class S>
-class Container<CommonDenseMatrix<S>>
+
+template <class ScalarType>
+struct Container<ScalarType, ChooseBackend::eigen_sparse>
 {
-public:
-  static CommonDenseMatrix<S> create(const size_t size)
-  {
-    CommonDenseMatrix<S> matrix(size, size);
-    for (size_t ii = 0; ii < size; ++ii)
-      matrix.unit_row(ii);
-    return matrix;
-  }
-};
+  typedef EigenDenseVector<ScalarType> VectorType;
+  typedef EigenRowMajorSparseMatrix<ScalarType> MatrixType;
+}; // struct Container< ..., eigen_sparse >
 
 
-#if HAVE_DUNE_ISTL
-template <class S>
-class Container<IstlDenseVector<S>>
+template <class ScalarType>
+struct Container<ScalarType, ChooseBackend::istl_sparse>
 {
-public:
-  static IstlDenseVector<S> create(const size_t size)
-  {
-    return IstlDenseVector<S>(size, S(1));
-  }
-};
-
-template <class S>
-class Container<IstlRowMajorSparseMatrix<S>>
-{
-public:
-  static IstlRowMajorSparseMatrix<S> create(const size_t size)
-  {
-    Dune::Stuff::LA::SparsityPatternDefault pattern(size);
-    for (size_t ii = 0; ii < size; ++ii)
-      pattern.inner(ii).insert(ii);
-    Dune::Stuff::LA::IstlRowMajorSparseMatrix<S> matrix(size, size, pattern);
-    for (size_t ii = 0; ii < size; ++ii)
-      matrix.unit_row(ii);
-    return matrix;
-  }
-};
-#endif // HAVE_DUNE_ISTL
-
-
-#if HAVE_EIGEN
-template <class S>
-class Container<EigenDenseVector<S>>
-{
-public:
-  static EigenDenseVector<S> create(const size_t size)
-  {
-    return EigenDenseVector<S>(size, S(1));
-  }
-};
-
-template <class S>
-class Container<EigenMappedDenseVector<S>>
-{
-public:
-  static EigenMappedDenseVector<S> create(const size_t size)
-  {
-    return EigenMappedDenseVector<S>(size, S(1));
-  }
-};
-
-template <class S>
-class Container<EigenDenseMatrix<S>>
-{
-public:
-  static EigenDenseMatrix<S> create(const size_t size)
-  {
-    Dune::Stuff::LA::EigenDenseMatrix<S> matrix(size, size);
-    for (size_t ii = 0; ii < size; ++ii)
-      matrix.unit_row(ii);
-    return matrix;
-  }
-};
-
-template <class S>
-class Container<EigenRowMajorSparseMatrix<S>>
-{
-public:
-  static EigenRowMajorSparseMatrix<S> create(const size_t size)
-  {
-    Dune::Stuff::LA::SparsityPatternDefault pattern(size);
-    for (size_t ii = 0; ii < size; ++ii)
-      pattern.inner(ii).insert(ii);
-    Dune::Stuff::LA::EigenRowMajorSparseMatrix<S> matrix(size, size, pattern);
-    for (size_t ii = 0; ii < size; ++ii)
-      matrix.unit_row(ii);
-    return matrix;
-  }
-};
-#endif // HAVE_EIGEN
+  typedef IstlDenseVector<ScalarType> VectorType;
+  typedef IstlRowMajorSparseMatrix<ScalarType> MatrixType;
+}; // struct Container< ..., istl_sparse >
 
 
 } // namespace LA
