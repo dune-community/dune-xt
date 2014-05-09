f1d3f1609c26787121a8b760e539e88da6f4fd88
Felix Schindler <felix@schindlerfamily.de>
2014-05-09 09:40:07 +0200

    [grid.partview] * added ChooseLayer to choose Level/Leaf * added Layer< ... > to select correct Level/Leaf{View,Part}

--- dune/stuff/grid/partview.hh
+++ dune/stuff/grid/partview.hh
@@ -21,24 +21,40 @@ namespace Grid {
 
 enum class ChoosePartView
 {
-  part,
-  view
+  view,
+  part
 }; // enum class ChoosePartView
+
+
+enum class ChooseLayer
+{
+  level,
+  leaf
+}; // enum class ChooseLayer
 
 
 #if HAVE_DUNE_GRID
 
 
 // forwards
+template <class GridType, ChooseLayer layer, ChoosePartView part_view>
+struct Layer;
+
 template <class GridType, ChoosePartView type>
 struct LevelPartView;
 
 template <class GridType, ChoosePartView type>
 struct LeafPartView;
 
+template <class GridType, ChooseLayer type>
+struct LayerView;
+
+template <class GridType, ChooseLayer type>
+struct LayerPart;
+
 
 template <class GridType>
-struct LevelPartView<GridType, ChoosePartView::view>
+struct Layer<GridType, ChooseLayer::level, ChoosePartView::view>
 {
   typedef typename GridType::LevelGridView Type;
 
@@ -48,26 +64,74 @@ struct LevelPartView<GridType, ChoosePartView::view>
     assert(level <= grid.maxLevel());
     return std::make_shared<Type>(grid.levelGridView(level));
   } // ... create(...)
+}; // struct Layer< ..., level, view >
+
+
+template <class GridType>
+struct Layer<GridType, ChooseLayer::leaf, ChoosePartView::view>
+{
+  typedef typename GridType::LeafGridView Type;
+
+  static std::shared_ptr<Type> create(GridType& grid, const int /*level*/ = 0)
+  {
+    return std::make_shared<Type>(grid.leafGridView());
+  }
+}; // struct Layer< ..., leaf, view >
+
+
+template <class GridType>
+struct LevelPartView<GridType, ChoosePartView::view>
+{
+  typedef typename Layer<GridType, ChooseLayer::level, ChoosePartView::view>::Type Type;
+
+  static std::shared_ptr<Type> create(GridType& grid, const int level)
+  {
+    return Layer<GridType, ChooseLayer::level, ChoosePartView::view>::create(grid, level);
+  }
 }; // struct LevelPartView< ..., view >
 
 
 template <class GridType>
 struct LeafPartView<GridType, ChoosePartView::view>
 {
-  typedef typename GridType::LeafGridView Type;
+  typedef typename Layer<GridType, ChooseLayer::leaf, ChoosePartView::view>::Type Type;
 
-  static std::shared_ptr<Type> create(GridType& grid)
+  static std::shared_ptr<Type> create(GridType& grid, const int /*level*/ = 0)
   {
-    return std::make_shared<Type>(grid.leafGridView());
+    return Layer<GridType, ChooseLayer::leaf, ChoosePartView::view>::create(grid);
   }
 }; // struct LeafPartView< ..., view >
+
+
+template <class GridType>
+struct LayerView<GridType, ChooseLayer::level>
+{
+  typedef typename Layer<GridType, ChooseLayer::level, ChoosePartView::view>::Type Type;
+
+  static std::shared_ptr<Type> create(GridType& grid, const int level)
+  {
+    return Layer<GridType, ChooseLayer::level, ChoosePartView::view>::create(grid, level);
+  }
+}; // struct LayerView< ..., level >
+
+
+template <class GridType>
+struct LayerView<GridType, ChooseLayer::leaf>
+{
+  typedef typename Layer<GridType, ChooseLayer::leaf, ChoosePartView::view>::Type Type;
+
+  static std::shared_ptr<Type> create(GridType& grid, const int /*level*/ = 0)
+  {
+    return Layer<GridType, ChooseLayer::level, ChoosePartView::view>::create(grid);
+  }
+}; // struct LayerView< ..., leaf >
 
 
 #if HAVE_DUNE_FEM
 
 
 template <class GridType>
-struct LevelPartView<GridType, ChoosePartView::part>
+struct Layer<GridType, ChooseLayer::level, ChoosePartView::part>
 {
   typedef Dune::Fem::LevelGridPart<GridType> Type;
 
@@ -77,23 +141,78 @@ struct LevelPartView<GridType, ChoosePartView::part>
     assert(level <= grid.maxLevel());
     return std::make_shared<Type>(grid, level);
   } // ... create(...)
+}; // struct Layer< ..., level, part >
+
+
+template <class GridType>
+struct Layer<GridType, ChooseLayer::leaf, ChoosePartView::part>
+{
+  typedef Dune::Fem::LeafGridPart<GridType> Type;
+
+  static std::shared_ptr<Type> create(GridType& grid, const int /*level*/ = 0)
+  {
+    return std::make_shared<Type>(grid);
+  }
+}; // struct Layer< ..., leaf, part >
+
+
+template <class GridType>
+struct LevelPartView<GridType, ChoosePartView::part>
+{
+  typedef typename Layer<GridType, ChooseLayer::level, ChoosePartView::part>::Type Type;
+
+  static std::shared_ptr<Type> create(GridType& grid, const int level)
+  {
+    return Layer<GridType, ChooseLayer::level, ChoosePartView::part>::create(grid, level);
+  }
 }; // struct LevelPartView< ..., part >
 
 
 template <class GridType>
 struct LeafPartView<GridType, ChoosePartView::part>
 {
-  typedef Dune::Fem::LeafGridPart<GridType> Type;
+  typedef typename Layer<GridType, ChooseLayer::leaf, ChoosePartView::part>::Type Type;
 
-  static std::shared_ptr<Type> create(GridType& grid)
+  static std::shared_ptr<Type> create(GridType& grid, const int /*level*/ = 0)
   {
-    return std::make_shared<Type>(grid);
+    return Layer<GridType, ChooseLayer::leaf, ChoosePartView::part>::create(grid);
   }
 }; // struct LeafPartView< ..., part >
 
 
+template <class GridType>
+struct LayerPart<GridType, ChooseLayer::level>
+{
+  typedef typename Layer<GridType, ChooseLayer::level, ChoosePartView::part>::Type Type;
+
+  static std::shared_ptr<Type> create(GridType& grid, const int level)
+  {
+    return Layer<GridType, ChooseLayer::level, ChoosePartView::part>::create(grid, level);
+  }
+}; // struct LayerPart< ..., level >
+
+
+template <class GridType>
+struct LayerPart<GridType, ChooseLayer::leaf>
+{
+  typedef typename Layer<GridType, ChooseLayer::leaf, ChoosePartView::part>::Type Type;
+
+  static std::shared_ptr<Type> create(GridType& grid, const int /*level*/ = 0)
+  {
+    return Layer<GridType, ChooseLayer::leaf, ChoosePartView::part>::create(grid);
+  }
+}; // struct LayerPart< ..., leaf >
+
+
 #endif // HAVE_DUNE_FEM
 #else // HAVE_DUNE_GRID
+
+
+template <class GridType, ChooseLayer layer, ChoosePartView type>
+struct Layer
+{
+  static_assert(AlwaysFalse<GridType>::value, "You are missing dune-grid!");
+};
 
 
 template <class GridType, ChoosePartView type>
@@ -101,6 +220,7 @@ struct LevelPartView
 {
   static_assert(AlwaysFalse<GridType>::value, "You are missing dune-grid!");
 };
+
 
 template <class GridType, ChoosePartView type>
 struct LeafPartView
