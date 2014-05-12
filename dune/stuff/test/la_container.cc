198f819ac9fc8c4637f3dc1e26916367ea676f02
Rene Milk <rene.milk@uni-muenster.de>
2014-05-12 14:05:37 +0200

    [tests] fixes a few renaming fubars

--- dune/stuff/test/la_container.cc
+++ dune/stuff/test/la_container.cc
@@ -19,6 +19,131 @@
 #include <dune/stuff/la/container/istl.hh>
 #include <dune/stuff/la/container.hh>
 
+namespace Dune {
+namespace Stuff {
+namespace LA {
+
+
+template <class ContainerImp>
+class ContainerFactory
+{
+public:
+  static ContainerImp create(const size_t /*size*/)
+  {
+    static_assert(Dune::AlwaysFalse<ContainerImp>::value, "Please specialize this class for this ContainerImp!");
+  }
+};
+
+
+template <class S>
+class ContainerFactory<CommonDenseVector<S>>
+{
+public:
+  static CommonDenseVector<S> create(const size_t size)
+  {
+    return CommonDenseVector<S>(size, S(1));
+  }
+};
+
+template <class S>
+class ContainerFactory<CommonDenseMatrix<S>>
+{
+public:
+  static CommonDenseMatrix<S> create(const size_t size)
+  {
+    CommonDenseMatrix<S> matrix(size, size);
+    for (size_t ii = 0; ii < size; ++ii)
+      matrix.unit_row(ii);
+    return matrix;
+  }
+};
+
+
+#if HAVE_DUNE_ISTL
+template <class S>
+class ContainerFactory<IstlDenseVector<S>>
+{
+public:
+  static IstlDenseVector<S> create(const size_t size)
+  {
+    return IstlDenseVector<S>(size, S(1));
+  }
+};
+
+template <class S>
+class ContainerFactory<IstlRowMajorSparseMatrix<S>>
+{
+public:
+  static IstlRowMajorSparseMatrix<S> create(const size_t size)
+  {
+    Dune::Stuff::LA::SparsityPatternDefault pattern(size);
+    for (size_t ii = 0; ii < size; ++ii)
+      pattern.inner(ii).insert(ii);
+    Dune::Stuff::LA::IstlRowMajorSparseMatrix<S> matrix(size, size, pattern);
+    for (size_t ii = 0; ii < size; ++ii)
+      matrix.unit_row(ii);
+    return matrix;
+  }
+};
+#endif // HAVE_DUNE_ISTL
+
+
+#if HAVE_EIGEN
+template <class S>
+class ContainerFactory<EigenDenseVector<S>>
+{
+public:
+  static EigenDenseVector<S> create(const size_t size)
+  {
+    return EigenDenseVector<S>(size, S(1));
+  }
+};
+
+template <class S>
+class ContainerFactory<EigenMappedDenseVector<S>>
+{
+public:
+  static EigenMappedDenseVector<S> create(const size_t size)
+  {
+    return EigenMappedDenseVector<S>(size, S(1));
+  }
+};
+
+template <class S>
+class ContainerFactory<EigenDenseMatrix<S>>
+{
+public:
+  static EigenDenseMatrix<S> create(const size_t size)
+  {
+    Dune::Stuff::LA::EigenDenseMatrix<S> matrix(size, size);
+    for (size_t ii = 0; ii < size; ++ii)
+      matrix.unit_row(ii);
+    return matrix;
+  }
+};
+
+template <class S>
+class ContainerFactory<EigenRowMajorSparseMatrix<S>>
+{
+public:
+  static EigenRowMajorSparseMatrix<S> create(const size_t size)
+  {
+    Dune::Stuff::LA::SparsityPatternDefault pattern(size);
+    for (size_t ii = 0; ii < size; ++ii)
+      pattern.inner(ii).insert(ii);
+    Dune::Stuff::LA::EigenRowMajorSparseMatrix<S> matrix(size, size, pattern);
+    for (size_t ii = 0; ii < size; ++ii)
+      matrix.unit_row(ii);
+    return matrix;
+  }
+};
+#endif // HAVE_EIGEN
+
+
+} // namespace LA
+} // namespace Stuff
+} // namespace Dune
+
 
 using namespace Dune;
 
@@ -90,7 +215,7 @@ struct ContainerTest : public ::testing::Test
     // dynamic tests
     // * of the container as itself (aka the derived type)
     ContainerImp DUNE_UNUSED(d_empty);
-    ContainerImp d_by_size = Stuff::LA::Container<ContainerImp>::create(dim);
+    ContainerImp d_by_size = Stuff::LA::ContainerFactory<ContainerImp>::create(dim);
     ContainerImp d_copy_constructor(d_by_size);
     ContainerImp DUNE_UNUSED(d_copy_assignment) = d_by_size;
     ContainerImp d_deep_copy = d_by_size.copy();
