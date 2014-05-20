210d8019c30104e16349490fe9afc6cbdec5192c
Felix Schindler <felix@schindlerfamily.de>
2014-05-20 20:50:27 +0200

    [la.container.eigen] remove dune-pymor remnants

--- dune/stuff/la/container/eigen.hh
+++ dune/stuff/la/container/eigen.hh
@@ -27,18 +27,6 @@
 #include "pattern.hh"
 
 namespace Dune {
-namespace Pymor {
-namespace Operators {
-
-// forwards, needed for friendlyness
-template <class ScalarImp>
-class EigenRowMajorSparseInverse;
-
-template <class ScalarImp>
-class EigenRowMajorSparse;
-
-} // namespace Operators
-} // namespace Pymor
 namespace Stuff {
 namespace LA {
 
@@ -873,9 +861,6 @@ private:
       backend_ = std::make_shared<BackendType>(*backend_);
   } // ... ensure_uniqueness(...)
 
-  friend class Dune::Pymor::Operators::EigenRowMajorSparseInverse<ScalarType>;
-  friend class Dune::Pymor::Operators::EigenRowMajorSparse<ScalarType>;
-
   mutable std::shared_ptr<BackendType> backend_;
 }; // class EigenDenseMatrix
 
@@ -1173,9 +1158,6 @@ private:
     if (!backend_.unique())
       backend_ = std::make_shared<BackendType>(*backend_);
   } // ... ensure_uniqueness(...)
-
-  friend class Dune::Pymor::Operators::EigenRowMajorSparseInverse<ScalarType>;
-  friend class Dune::Pymor::Operators::EigenRowMajorSparse<ScalarType>;
 
   mutable std::shared_ptr<BackendType> backend_;
 }; // class EigenRowMajorSparseMatrix
