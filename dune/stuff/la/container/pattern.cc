758b8eb7708ea2d2f515904cae8632d859e7817f
Felix Schindler <felix@schindlerfamily.de>
2014-07-11 14:57:07 +0200

    [pattern] cleaned up a bit and added some convenient methods
    This commit adds the convenient method `insert(ii, jj)`, which searches for
    `jj` in the `ii`th vector and adds it, if necessarry (it mimics the old
    `inner(ii).insert(jj)`, where `inner(ii) `was a set). In addition there are
    two new methods for sorting rows, namely `sort(ii)` and `sort()`, to ensure
    that the column indices are sorted in each row because there are (/may be)
    backends which need this. Use of them is optional (of coarse) if you know
    what you are doing.
    
    If there is too much of a performance penalty we could think about
    providing two separate implementations of the sparsity pattern. A
    (possibly) memory intensive one based on a `std::set` (as it used to be)
    and a (possibly) runtime intensive one based on a `std::vector` (as it is
    now)...

--- dune/stuff/la/container/pattern.cc
+++ dune/stuff/la/container/pattern.cc
@@ -3,9 +3,12 @@
 // Copyright holders: Rene Milk, Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-#include "pattern.hh"
+#include "config.h"
 
-#include <assert.h>
+#include <cassert>
+#include <algorithm>
+
+#include "pattern.hh"
 
 namespace Dune {
 namespace Stuff {
@@ -16,45 +19,65 @@ namespace LA {
 // ==== SparsityPatternDefault ====
 // ================================
 SparsityPatternDefault::SparsityPatternDefault(const size_t _size)
-  : vectorOfSets_(_size)
+  : vector_of_vectors_(_size)
 {
 }
 
 size_t SparsityPatternDefault::size() const
 {
-  return vectorOfSets_.size();
+  return vector_of_vectors_.size();
 }
 
 typename SparsityPatternDefault::InnerType& SparsityPatternDefault::inner(const size_t ii)
 {
   assert(ii < size() && "Wrong index requested!");
-  return vectorOfSets_[ii];
+  return vector_of_vectors_[ii];
 }
 
 const typename SparsityPatternDefault::InnerType& SparsityPatternDefault::inner(const size_t ii) const
 {
   assert(ii < size() && "Wrong index requested!");
-  return vectorOfSets_[ii];
+  return vector_of_vectors_[ii];
 }
 
 typename SparsityPatternDefault::ConstOuterIteratorType SparsityPatternDefault::begin() const
 {
-  return vectorOfSets_.begin();
+  return vector_of_vectors_.begin();
 }
 
 typename SparsityPatternDefault::ConstOuterIteratorType SparsityPatternDefault::end() const
 {
-  return vectorOfSets_.end();
+  return vector_of_vectors_.end();
 }
 
 bool SparsityPatternDefault::operator==(const SparsityPatternDefault& other) const
 {
-  return vectorOfSets_ == other.vectorOfSets_;
+  return vector_of_vectors_ == other.vector_of_vectors_;
 }
 
 bool SparsityPatternDefault::operator!=(const SparsityPatternDefault& other) const
 {
-  return vectorOfSets_ != other.vectorOfSets_;
+  return vector_of_vectors_ != other.vector_of_vectors_;
+}
+
+void SparsityPatternDefault::insert(const size_t outer_index, const size_t inner_index)
+{
+  assert(ii < size() && "Wrong index requested!");
+  if (std::find(vector_of_vectors_[outer_index].begin(), vector_of_vectors_[outer_index].end(), inner_index)
+      == vector_of_vectors_[outer_index].end())
+    vector_of_vectors_[outer_index].push_back(inner_index);
+}
+
+void SparsityPatternDefault::sort(const size_t outer_index)
+{
+  assert(ii < size() && "Wrong index requested!");
+  std::sort(vector_of_vectors_[outer_index].begin(), vector_of_vectors_[outer_index].end());
+}
+
+void SparsityPatternDefault::sort()
+{
+  for (auto& inner_vector : vector_of_vectors_)
+    std::sort(inner_vector.begin(), inner_vector.end());
 }
 
 
