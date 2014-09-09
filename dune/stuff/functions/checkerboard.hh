963c4f34aac74a169a34052eec9e33244851ff3c
Felix Schindler <felix.schindler@wwu.de>
2014-09-09 18:22:18 +0200

    [functions.checkerboard]
    * use Common::FieldVector in ctor (is convertible from Dune::FieldVector
      and std::vector)
    * forbid assignment

--- dune/stuff/functions/checkerboard.hh
+++ dune/stuff/functions/checkerboard.hh
@@ -14,6 +14,7 @@
 
 #include <dune/stuff/common/configuration.hh>
 #include <dune/stuff/common/debug.hh>
+#include <dune/stuff/common/fvector.hh>
 
 #include "interfaces.hh"
 
@@ -125,7 +126,8 @@ public:
     const Common::Configuration cfg         = config.has_sub(sub_name) ? config.sub(sub_name) : config;
     const Common::Configuration default_cfg = default_config();
     // calculate number of values and get values
-    auto num_elements = cfg.get("num_elements", default_cfg.get<std::vector<size_t>>("num_elements"), dimDomain);
+    auto num_elements =
+        cfg.get("num_elements", default_cfg.get<Common::FieldVector<size_t, dimDomain>>("num_elements"), dimDomain);
     size_t num_values = 1;
     for (size_t ii = 0; ii < num_elements.size(); ++ii)
       num_values *= num_elements[ii];
@@ -135,31 +137,26 @@ public:
       values[ii] = RangeType(values_rf[ii]);
     // create
     return Common::make_unique<ThisType>(
-        cfg.get("lower_left", default_cfg.get<std::vector<DomainFieldType>>("lower_left"), dimDomain),
-        cfg.get("upper_right", default_cfg.get<std::vector<DomainFieldType>>("upper_right"), dimDomain),
+        cfg.get(
+            "lower_left", default_cfg.get<Common::FieldVector<DomainFieldType, dimDomain>>("lower_left"), dimDomain),
+        cfg.get(
+            "upper_right", default_cfg.get<Common::FieldVector<DomainFieldType, dimDomain>>("upper_right"), dimDomain),
         std::move(num_elements),
         std::move(values),
         cfg.get("name", default_cfg.get<std::string>("name")));
   } // ... create(...)
 
-  Checkerboard(std::vector<DomainFieldType>&& lowerLeft, std::vector<DomainFieldType>&& upperRight,
-               std::vector<size_t>&& numElements, std::vector<RangeType>&& values, std::string nm = static_id())
-    : lowerLeft_(new std::vector<DomainFieldType>(std::move(lowerLeft)))
-    , upperRight_(new std::vector<DomainFieldType>(std::move(upperRight)))
-    , numElements_(new std::vector<size_t>(std::move(numElements)))
-    , values_(new std::vector<RangeType>(std::move(values)))
+  Checkerboard(const Common::FieldVector<DomainFieldType, dimDomain>& lowerLeft,
+               const Common::FieldVector<DomainFieldType, dimDomain>& upperRight,
+               const Common::FieldVector<size_t, dimDomain>& numElements, const std::vector<RangeType>& values,
+               const std::string nm = static_id())
+    : lowerLeft_(new Common::FieldVector<DomainFieldType, dimDomain>(lowerLeft))
+    , upperRight_(new Common::FieldVector<DomainFieldType, dimDomain>(upperRight))
+    , numElements_(new Common::FieldVector<size_t, dimDomain>(numElements))
+    , values_(new std::vector<RangeType>(values))
     , name_(nm)
   {
     // checks
-    if (lowerLeft_->size() < dimDomain)
-      DUNE_THROW(Dune::RangeError,
-                 "lowerLeft too small (is " << lowerLeft_->size() << ", should be " << dimDomain << ")");
-    if (upperRight_->size() < dimDomain)
-      DUNE_THROW(Dune::RangeError,
-                 "upperRight too small (is " << upperRight_->size() << ", should be " << dimDomain << ")");
-    if (numElements_->size() < dimDomain)
-      DUNE_THROW(Dune::RangeError,
-                 "numElements too small (is " << numElements_->size() << ", should be " << dimDomain << ")");
     size_t totalSubdomains = 1;
     for (size_t dd = 0; dd < dimDomain; ++dd) {
       const auto& ll = (*lowerLeft_)[dd];
@@ -183,17 +180,9 @@ public:
   {
   }
 
-  ThisType& operator=(const ThisType& other)
-  {
-    if (this != &other) {
-      lowerLeft_   = other.lowerLeft_;
-      upperRight_  = other.upperRight_;
-      numElements_ = other.numElements_;
-      values_      = other.values_;
-      name_        = other.name_;
-    }
-    return *this;
-  }
+  ThisType& operator=(const ThisType& other) = delete;
+
+  ThisType& operator=(ThisType&& source) = delete;
 
   virtual ThisType* copy() const DS_OVERRIDE
   {
@@ -236,9 +225,9 @@ public:
   } // ... local_function(...)
 
 private:
-  std::shared_ptr<const std::vector<DomainFieldType>> lowerLeft_;
-  std::shared_ptr<const std::vector<DomainFieldType>> upperRight_;
-  std::shared_ptr<const std::vector<size_t>> numElements_;
+  std::shared_ptr<const Common::FieldVector<DomainFieldType, dimDomain>> lowerLeft_;
+  std::shared_ptr<const Common::FieldVector<DomainFieldType, dimDomain>> upperRight_;
+  std::shared_ptr<const Common::FieldVector<size_t, dimDomain>> numElements_;
   std::shared_ptr<const std::vector<RangeType>> values_;
   std::string name_;
 }; // class Checkerboard
