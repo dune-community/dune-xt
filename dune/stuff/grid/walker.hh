3e89002a48a6fb53d532096c0582b6903c1110b7
Rene Milk <rene.milk@uni-muenster.de>
2014-10-30 18:57:50 +0100

    [common.parallel] rewire ThreadManager singleton usage

--- dune/stuff/grid/walker.hh
+++ dune/stuff/grid/walker.hh
@@ -175,7 +175,7 @@ public:
   {
 #if DUNE_VERSION_NEWER(DUNE_COMMON, 3, 9) // EXADUNE
     if (use_tbb) {
-      const auto num_partitions = ThreadManager::current_threads();
+      const auto num_partitions = threadManager().current_threads();
       RangedPartitioning<GridViewType, 0> partitioning(grid_view_, num_partitions);
       this->walk(partitioning);
       return;
