115a730c35bb09ef0a33a61bfe51217e327bf8b9
Felix Schindler <felix.schindler@wwu.de>
2015-02-03 14:34:53 +0100

    [functions.femadapter] remove unused local typedefs

--- dune/stuff/functions/femadapter.hh
+++ dune/stuff/functions/femadapter.hh
@@ -44,15 +44,8 @@ class FemAdapter
   public:
     typedef typename BaseType::EntityType EntityType;
 
-    typedef typename BaseType::DomainFieldType DomainFieldType;
-    static const unsigned int dimDomain = BaseType::dimDomain;
     typedef typename BaseType::DomainType DomainType;
-
-    typedef typename BaseType::RangeFieldType RangeFieldType;
-    static const unsigned int dimRange     = BaseType::dimRange;
-    static const unsigned int dimRangeCols = BaseType::dimRangeCols;
     typedef typename BaseType::RangeType RangeType;
-
     typedef typename BaseType::JacobianRangeType JacobianRangeType;
 
     Localfunction(const DiscreteFunctionType& df, const EntityType& ent)
@@ -88,17 +81,6 @@ class FemAdapter
 public:
   typedef typename BaseType::EntityType EntityType;
   typedef typename BaseType::LocalfunctionType LocalfunctionType;
-
-  typedef typename BaseType::DomainFieldType DomainFieldType;
-  static const int dimDomain = BaseType::dimDomain;
-  typedef typename BaseType::DomainType DomainType;
-
-  typedef typename BaseType::RangeFieldType RangeFieldType;
-  static const int dimRange     = BaseType::dimRange;
-  static const int dimRangeCols = BaseType::dimRangeCols;
-  typedef typename BaseType::RangeType RangeType;
-
-  typedef typename BaseType::JacobianRangeType JacobianRangeType;
 
   FemAdapter(const DiscreteFunctionType& df)
     : df_(df)
