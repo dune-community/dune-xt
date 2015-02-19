9bd4dfd0cdd3bc6b00510fcdbe81ea7bd14f406c
Tobias Leibner <tobias.leibner@googlemail.com>
2015-02-19 10:34:05 +0100

    [test.grid_periodicview] add tests

--- dune/stuff/test/grid_periodicview.cc
+++ dune/stuff/test/grid_periodicview.cc
@@ -5,6 +5,9 @@
 //
 // Contributors: Tobias Leibner
 
+//# define DUNE_STUFF_TEST_MAIN_ENABLE_INFO_LOGGING 1
+//# define DUNE_STUFF_TEST_MAIN_ENABLE_TIMED_LOGGING 1
+
 #include "main.hxx"
 
 #if HAVE_DUNE_GRID
@@ -21,31 +24,36 @@
 using namespace Dune;
 using namespace Stuff;
 
-#define YASPGRIDS YaspGrid<1>, YaspGrid<2>, YaspGrid<3>, YaspGrid<4>
+#define YASPGRIDS YaspGrid<1>, YaspGrid<2>, YaspGrid<3>
 
 #if HAVE_ALUGRID
-#define ALUGRIDS                                                                                                       \
-  ALUGrid<2, 2, cube, nonconforming>, ALUGrid<3, 3, cube, nonconforming>, ALUGrid<2, 2, simplex, conforming>,          \
-      ALUGrid<3, 3, simplex, conforming>
+#define ALUCUBEGRIDS ALUGrid<2, 2, cube, nonconforming>, ALUGrid<3, 3, cube, nonconforming>
+
+#define ALUSIMPLEXGRIDS ALUGrid<2, 2, simplex, conforming>, ALUGrid<3, 3, simplex, conforming>
 #endif // HAVE_ALUGRID
 
 
 template <class GridImp>
-struct PeriodicViewTestBase : public testing::Test
+struct PeriodicViewTestCube : public testing::Test
 {
-  typedef typename GridImp GridType;
-  typedef Grid::Providers::Cube<GridType> GridProviderType;
+  typedef GridImp GridType;
+  typedef typename GridType::ctype ctype;
+  typedef typename GridType::template Codim<0>::Geometry GeometryType;
+  typedef Dune::Stuff::Grid::Providers::template Cube<GridType> GridProviderType;
   typedef typename GridType::LeafGridView GridViewType;
-  typedef typename Grid::PeriodicGridView<GridViewType> PeriodicGridViewType;
+  typedef typename GridViewType::IndexSet IndexSet;
+  typedef typename Dune::Stuff::Grid::template PeriodicGridView<GridViewType> PeriodicGridViewType;
   typedef typename PeriodicGridViewType::template Codim<0>::Entity EntityType;
   typedef typename PeriodicGridViewType::template Codim<0>::Iterator EntityIteratorType;
-  typedef typename Grid::PeriodicIntersectionIterator<GridViewType> PeriodicIntersectionIteratorType;
-  typedef typename Grid::PeriodicIntersection<GridViewType> PeriodicIntersectionType;
+  typedef
+      typename Dune::Stuff::Grid::template PeriodicIntersectionIterator<GridViewType> PeriodicIntersectionIteratorType;
+  typedef typename Dune::Stuff::Grid::template PeriodicIntersection<GridViewType> PeriodicIntersectionType;
+  typedef typename GridViewType::CollectiveCommunication CollectiveCommunication;
   static const size_t dimDomain = GridViewType::dimension;
 
-  void check()
+  void check(const bool is_simplex)
   {
-
+    const bool is_cube                         = !is_simplex;
     GridProviderType grid_provider             = *(GridProviderType::create());
     const std::shared_ptr<const GridType> grid = grid_provider.grid_ptr();
     const GridViewType grid_view               = grid->leafGridView();
@@ -59,111 +67,222 @@ struct PeriodicViewTestBase : public testing::Test
     const PeriodicGridViewType partially_periodic_grid_view(grid_view_ref, periodic_directions);
     // create PeriodicGridviewType that is periodic only in all directions
     periodic_directions.set();
-    const PeriodicGridViewType periodic_grid_view(grid_view_ref, periodic_directions);
+    const PeriodicGridViewType fully_periodic_grid_view(grid_view_ref, periodic_directions);
 
-    size_t neighbor_count           = 0;
-    size_t boundary_count           = 0;
-    const EntityIteratorType it_end = periodic_grid_view.end<0>();
-    for (EntityIteratorType it = periodic_grid_view.begin<0>(); it != it_end; ++it) {
-      const EntityType& entity                        = *it;
-      const PeriodicIntersectionIteratorType i_it_end = periodic_grid_view.iend(entity);
-      for (PeriodicIntersectionIteratorType i_it = periodic_grid_view.ibegin(entity); i_it != i_it_end; ++i_it) {
-        const PeriodicIntersectionType& intersection = *i_it;
-        if (intersection.neighbor())
-          ++neighbor_count;
-        const auto outside_ptr         = intersection.outside();
-        const auto coords_in_outside   = intersection.geometryInOutside().center();
-        const auto coords_in_outside_2 = outside_ptr->geometryInInside().center();
-        EXPECT_TRUE(Dune::Stuff::Common::FloatCmp::eq(coords_in_outside, coords_in_outside_2));
-        const auto index_in_outside           = intersection_indexInOutside();
-        const auto global_intersection_coords = intersection.geometry().center();
-        const auto global_outside_coords      = outside_ptr->geometry().center();
-        const size_t coord_difference         = 0;
-        const size_t differing_coordinate;
-        for (size_t ii = 0; ii < dimDomain; ++ii) {
-          if (global_outside_coords[ii] != global_intersection_coords) {
-            ++coord_difference;
-            differing_coordinate = ii;
+    std::vector<const PeriodicGridViewType*> periodic_grid_view_vector(3);
+    periodic_grid_view_vector[0] = &non_periodic_grid_view;
+    periodic_grid_view_vector[1] = &partially_periodic_grid_view;
+    periodic_grid_view_vector[2] = &fully_periodic_grid_view;
+
+    for (size_t variant = 0; variant <= 2; ++variant) {
+      const PeriodicGridViewType periodic_grid_view = *(periodic_grid_view_vector[variant]);
+
+      // check interface
+      const GridType& DUNE_UNUSED(test_grid) = periodic_grid_view.grid();
+      const IndexSet& DUNE_UNUSED(test_indexSet) = periodic_grid_view.indexSet();
+      const int codim0_size = periodic_grid_view.size(0);
+      EXPECT_EQ(codim0_size, grid_view.size(0));
+      if (dimDomain == 1)
+        EXPECT_EQ(codim0_size, int(8));
+      if (dimDomain == 2 && is_cube)
+        EXPECT_EQ(codim0_size, int(8 * 8));
+      if (dimDomain == 2 && is_simplex)
+        EXPECT_EQ(codim0_size, int(2 * 8 * 8));
+      if (dimDomain == 3 && is_cube)
+        EXPECT_EQ(codim0_size, int(8 * 8 * 8));
+      if (dimDomain == 3 && is_simplex)
+        EXPECT_EQ(codim0_size, int(6 * 8 * 8 * 8));
+      EXPECT_EQ(periodic_grid_view.size(Dune::cube), grid_view.size(Dune::cube));
+      EXPECT_EQ(periodic_grid_view.size(Dune::simplex), grid_view.size(Dune::simplex));
+      EXPECT_EQ(periodic_grid_view.overlapSize(0), grid_view.overlapSize(0));
+      EXPECT_EQ(periodic_grid_view.overlapSize(1), grid_view.overlapSize(1));
+      EXPECT_EQ(periodic_grid_view.ghostSize(0), grid_view.ghostSize(0));
+      EXPECT_EQ(periodic_grid_view.ghostSize(1), grid_view.ghostSize(1));
+      const CollectiveCommunication& DUNE_UNUSED(test_comm) = periodic_grid_view.comm();
+
+      size_t neighbor_count = 0;
+      size_t boundary_count = 0;
+      size_t periodic_count = 0;
+      // iterate over all entities
+      const EntityIteratorType it_end = periodic_grid_view.template end<0>();
+      for (EntityIteratorType it = periodic_grid_view.template begin<0>(); it != it_end; ++it) {
+        const EntityType& entity = *it;
+        EXPECT_TRUE(periodic_grid_view.contains(entity));
+        // iterate over all intersections on current entity
+        const PeriodicIntersectionIteratorType i_it_end = periodic_grid_view.iend(entity);
+        for (PeriodicIntersectionIteratorType i_it = periodic_grid_view.ibegin(entity); i_it != i_it_end; ++i_it) {
+          const PeriodicIntersectionType& intersection = *i_it;
+          if (intersection.neighbor()) {
+            ++neighbor_count;
+            const auto outside = *(intersection.outside());
+            // find corresponding intersection in outside
+            const auto index_in_outside                             = intersection.indexInOutside();
+            const PeriodicIntersectionType* intersection_in_outside = periodic_grid_view.ibegin(outside).operator->();
+            const PeriodicIntersectionIteratorType i_it_outside_end = periodic_grid_view.iend(outside);
+            for (PeriodicIntersectionIteratorType i_it_outside = periodic_grid_view.ibegin(outside);
+                 i_it_outside != i_it_outside_end;
+                 ++i_it_outside) {
+              const PeriodicIntersectionType* outside_intersection = i_it_outside.operator->();
+              if (outside_intersection->indexInInside() == index_in_outside) {
+                intersection_in_outside = outside_intersection;
+              }
+            }
+            // check outside_intersection coords
+            const auto coords_in_outside   = intersection.geometryInOutside().center();
+            const auto coords_in_outside_2 = intersection_in_outside->geometryInInside().center();
+            EXPECT_TRUE(Dune::Stuff::Common::FloatCmp::eq(coords_in_outside, coords_in_outside_2));
+            // check global intersection coords in periodic case
+            const auto global_intersection_coords         = intersection.geometry().center();
+            const auto global_outside_intersection_coords = (*intersection_in_outside).geometry().center();
+            size_t coord_difference_count                 = 0;
+            size_t differing_coordinate;
+            for (size_t ii = 0; ii < dimDomain; ++ii) {
+              if (Dune::Stuff::Common::FloatCmp::ne(global_outside_intersection_coords[ii],
+                                                    global_intersection_coords[ii])) {
+                ++coord_difference_count;
+                differing_coordinate = ii;
+              }
+            }
+            if (intersection.is_periodic()) {
+              EXPECT_EQ(size_t(1), coord_difference_count);
+              EXPECT_TRUE(
+                  (Dune::Stuff::Common::FloatCmp::eq(global_outside_intersection_coords[differing_coordinate], ctype(1))
+                   && Dune::Stuff::Common::FloatCmp::eq(global_intersection_coords[differing_coordinate], ctype(0)))
+                  || (Dune::Stuff::Common::FloatCmp::eq(global_outside_intersection_coords[differing_coordinate],
+                                                        ctype(0))
+                      && Dune::Stuff::Common::FloatCmp::eq(global_intersection_coords[differing_coordinate],
+                                                           ctype(1))));
+              ++periodic_count;
+              if (variant == 1)
+                EXPECT_EQ(size_t(0), differing_coordinate);
+            }
+          }
+          if (intersection.boundary()) {
+            ++boundary_count;
+            EXPECT_FALSE(intersection.is_periodic());
+            EXPECT_FALSE(intersection.neighbor());
           }
         }
-        if (intersection.is_periodic()) {
-          EXPECT_EQ(size_t(1), coord_difference);
-          EXPECT_TRUE((Dune::Stuff::Common::FloatCmp::eq(global_outside_coords[differing_coordinate], 1)
-                       && Dune::Stuff::Common::FloatCmp::eq(global_intersection_coords[differing_coordinate], 0))
-                      || (Dune::Stuff::Common::FloatCmp::eq(global_outside_coords[differing_coordinate], 0)
-                          && Dune::Stuff::Common::FloatCmp::eq(global_intersection_coords[differing_coordinate], 1)));
-        }
+      }
 
-        if (intersection.boundary())
-          ++boundary_count;
+      if (dimDomain == 1) {
+        if (variant == 0) {
+          EXPECT_EQ(size_t(2 * 7), neighbor_count);
+          EXPECT_EQ(size_t(2), boundary_count);
+          EXPECT_EQ(size_t(0), periodic_count);
+        }
+        if (variant == 1) {
+          EXPECT_EQ(size_t(2 * 7 + 2), neighbor_count);
+          EXPECT_EQ(size_t(0), boundary_count);
+          EXPECT_EQ(size_t(2), periodic_count);
+        }
+        if (variant == 2) {
+          EXPECT_EQ(size_t(2 * 7 + 2), neighbor_count);
+          EXPECT_EQ(size_t(0), boundary_count);
+          EXPECT_EQ(size_t(2), periodic_count);
+        }
+      }
+      if (dimDomain == 2 && is_cube) {
+        if (variant == 0) {
+          EXPECT_EQ(size_t(2 * 2 * 8 * 7), neighbor_count);
+          EXPECT_EQ(size_t(2 * 8 * 2), boundary_count);
+          EXPECT_EQ(size_t(0), periodic_count);
+        }
+        if (variant == 1) {
+          EXPECT_EQ(size_t(2 * 2 * 8 * 7 + 2 * 8), neighbor_count);
+          EXPECT_EQ(size_t(2 * 8 * 2 - 2 * 8), boundary_count);
+          EXPECT_EQ(size_t(2 * 8), periodic_count);
+        }
+        if (variant == 2) {
+          EXPECT_EQ(size_t(2 * 2 * 8 * 7 + 2 * 8 * 2), neighbor_count);
+          EXPECT_EQ(size_t(0), boundary_count);
+          EXPECT_EQ(size_t(2 * 8 * 2), periodic_count);
+        }
+      }
+      if (dimDomain == 2 && is_simplex) {
+        if (variant == 0) {
+          EXPECT_EQ(size_t(2 * 2 * 8 * 7 + 2 * 64), neighbor_count);
+          EXPECT_EQ(size_t(2 * 8 * 2), boundary_count);
+          EXPECT_EQ(size_t(0), periodic_count);
+        }
+        if (variant == 1) {
+          EXPECT_EQ(size_t(2 * 2 * 8 * 7 + 2 * 8 + 2 * 64), neighbor_count);
+          EXPECT_EQ(size_t(2 * 8 * 2 - 2 * 8), boundary_count);
+          EXPECT_EQ(size_t(2 * 8), periodic_count);
+        }
+        if (variant == 2) {
+          EXPECT_EQ(size_t(2 * 2 * 8 * 7 + 2 * 8 * 2 + 2 * 64), neighbor_count);
+          EXPECT_EQ(size_t(0), boundary_count);
+          EXPECT_EQ(size_t(2 * 8 * 2), periodic_count);
+        }
+      }
+      if (dimDomain == 3 && is_cube) {
+        if (variant == 0) {
+          EXPECT_EQ(size_t(2 * 3 * 7 * 64), neighbor_count);
+          EXPECT_EQ(size_t(6 * 64), boundary_count);
+          EXPECT_EQ(size_t(0), periodic_count);
+        }
+        if (variant == 1) {
+          EXPECT_EQ(size_t(2 * 3 * 7 * 64 + 2 * 64), neighbor_count);
+          EXPECT_EQ(size_t(6 * 64 - 2 * 64), boundary_count);
+          EXPECT_EQ(size_t(2 * 64), periodic_count);
+        }
+        if (variant == 2) {
+          EXPECT_EQ(size_t(2 * 3 * 7 * 64 + 6 * 64), neighbor_count);
+          EXPECT_EQ(size_t(0), boundary_count);
+          EXPECT_EQ(size_t(6 * 64), periodic_count);
+        }
+      }
+      if (dimDomain == 3 && is_simplex) {
+        if (variant == 0) {
+          EXPECT_EQ(size_t(2 * 2 * 3 * 7 * 64 + 2 * 6 * 8 * 8 * 8), neighbor_count);
+          EXPECT_EQ(size_t(2 * 6 * 64), boundary_count);
+          EXPECT_EQ(size_t(0), periodic_count);
+        }
+        if (variant == 1) {
+          EXPECT_EQ(size_t(2 * 2 * 3 * 7 * 64 + 2 * 2 * 64 + 2 * 6 * 8 * 8 * 8), neighbor_count);
+          EXPECT_EQ(size_t(2 * 6 * 64 - 2 * 2 * 64), boundary_count);
+          EXPECT_EQ(size_t(2 * 2 * 64), periodic_count);
+        }
+        if (variant == 2) {
+          EXPECT_EQ(size_t(2 * 2 * 3 * 7 * 64 + 2 * 6 * 64 + 2 * 6 * 8 * 8 * 8), neighbor_count);
+          EXPECT_EQ(size_t(0), boundary_count);
+          EXPECT_EQ(size_t(2 * 6 * 64), periodic_count);
+        }
       }
     }
-
-
-    const GridType& DUNE_UNUSED(test_grid) = periodic_grid_view.grid();
-    const IndexSet& DUNE_UNUSED(test_indexSet) = periodic_grid_view.indexSet();
-    size_t num_entities = 1;
-    for (size_t i = 0; i < dimDomain; ++i) {
-      num_entities *= 8;
-    }
-    EXPECT_EQ(num_entities, periodic_grid_view.size(0));
-    EXPECT_EQ(num_entities, periodic_grid_view.size(1));
-
-      int 	size (const GeometryType &type) const
-    obtain number of entities with a given geometry type
-
-          template<class EntityType >
-  bool 	contains (const EntityType &e) const
-    Return true if the given entity is contained in this grid view.
-  template<int cd>
-  Codim< cd >::Iterator 	begin () const
-    obtain begin iterator for this view
-  template<int cd>
-  Codim< cd >::Iterator 	end () const
-    obtain end iterator for this view
-  template<int cd, PartitionIteratorType pitype>
-  Codim< cd >::template
-  Partition< pitype >::Iterator 	begin () const
-    obtain begin iterator for this view
-  template<int cd, PartitionIteratorType pitype>
-  Codim< cd >::template
-  Partition< pitype >::Iterator 	end () const
-    obtain end iterator for this view
-  IntersectionIterator 	ibegin (const typename Codim< 0 >::Entity &entity) const
-    obtain begin intersection iterator with respect to this view
-  IntersectionIterator 	iend (const typename Codim< 0 >::Entity &entity) const
-    obtain end intersection iterator with respect to this view
-  const CollectiveCommunication & 	comm () const
-    obtain collective communication object
-  int 	overlapSize (int codim) const
-    Return size of the overlap region for a given codim on the grid view.
-  int 	ghostSize (int codim) const
-    Return size of the ghost region for a given codim on the grid view.
-  template<class DataHandleImp , class DataType >
-  void 	communicate (CommDataHandleIF< DataHandleImp, DataType > &data, InterfaceType iftype, CommunicationDirection dir) const
-    Communicate data on this view.
   }
+};
+
+template <class GridImp>
+struct PeriodicViewTestSimplex : public PeriodicViewTestCube<GridImp>
+{
 };
 
 typedef testing::Types<YASPGRIDS
 #if HAVE_ALUGRID
                        ,
-                       ALUGRIDS
+                       ALUCUBEGRIDS
 #endif
-                       > GridTypes;
+                       > CubeGridTypes;
 
-TYPED_TEST_CASE(PeriodicViewTest, GridTypes);
-TYPED_TEST(CubeGridProvider, is_default_creatable)
+typedef testing::Types<
+#if HAVE_ALUGRID
+    ALUSIMPLEXGRIDS
+#endif
+    > SimplexGridTypes;
+
+
+TYPED_TEST_CASE(PeriodicViewTestCube, CubeGridTypes);
+TYPED_TEST(PeriodicViewTestCube, checkcube)
 {
-  this->is_default_creatable();
+  this->check(false);
 }
-TYPED_TEST(PeriodicViewTest, fulfills_const_interface)
+
+TYPED_TEST_CASE(PeriodicViewTestSimplex, SimplexGridTypes);
+TYPED_TEST(PeriodicViewTestSimplex, checksimplex)
 {
-  this->const_interface();
-}
-TYPED_TEST(PeriodicViewTest, is_visualizable)
-{
-  this->visualize();
+  this->check(true);
 }
 
 
