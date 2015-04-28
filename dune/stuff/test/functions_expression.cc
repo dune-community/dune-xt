dd09a6858a99a3846c96e5fae399b3d4ab629d01
Tobias Leibner <tobias.leibner@googlemail.com>
2015-04-28 14:39:43 +0200

    [test.functions_expression] add some tests

--- dune/stuff/test/functions_expression.cc
+++ dune/stuff/test/functions_expression.cc
@@ -32,8 +32,12 @@
                                                                                                                        \
     void check() const                                                                                                 \
     {                                                                                                                  \
-      const std::unique_ptr<const LocalizableFunctionType> function(                                                   \
-          LocalizableFunctionType::create(LocalizableFunctionType::default_config()));                                 \
+      Dune::Stuff::Common::Configuration config = LocalizableFunctionType::default_config();                           \
+      const std::unique_ptr<const LocalizableFunctionType> function(LocalizableFunctionType::create(config));          \
+      config["expression"] = "[2*x[0] 2*x[1] 2*x[2]]";                                                                 \
+      config["gradient"] = "[2 0 0; 0 2 0; 0 0 2]";                                                                    \
+      const std::unique_ptr<const LocalizableFunctionType> function2(LocalizableFunctionType::create(config));         \
+      const LocalizableFunctionType function3("x", {"x[0]", "x[0]", "x[0]"});                                          \
     }                                                                                                                  \
   };
 // TEST_STRUCT_GENERATOR
