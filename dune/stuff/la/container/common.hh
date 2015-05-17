22f0cda08d791eedf49357dc1ff76cd67c057196
Felix Schindler <felix.schindler@wwu.de>
2015-05-17 18:08:44 +0200

    [la.container.common] add data()

--- dune/stuff/la/container/common.hh
+++ dune/stuff/la/container/common.hh
@@ -69,7 +69,8 @@ public:
  */
 template <class ScalarImp = double>
 class CommonDenseVector : public VectorInterface<internal::CommonDenseVectorTraits<ScalarImp>, ScalarImp>,
-                          public ProvidesBackend<internal::CommonDenseVectorTraits<ScalarImp>>
+                          public ProvidesBackend<internal::CommonDenseVectorTraits<ScalarImp>>,
+                          public ProvidesDataAccess<internal::CommonDenseVectorTraits<ScalarImp>>
 {
   typedef CommonDenseVector<ScalarImp> ThisType;
   typedef VectorInterface<internal::CommonDenseVectorTraits<ScalarImp>, ScalarImp> VectorInterfaceType;
@@ -172,6 +173,15 @@ public:
     ensure_uniqueness();
     return *backend_;
   } // ... backend(...)
+
+  /// \}
+  /// \name Required by ProvidesDataAccess.
+  /// \{
+
+  ScalarType* data()
+  {
+    return &(backend()[0]);
+  }
 
   /// \}
   /// \name Required by ContainerInterface.
