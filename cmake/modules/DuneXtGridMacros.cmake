1ef6b37203d2597c8d392db8d81b46731c97c5ce
Felix Schindler <felix.schindler@wwu.de>
2016-01-20 18:36:23 +0100

    update header

--- cmake/modules/DuneXtGridMacros.cmake
+++ cmake/modules/DuneXtGridMacros.cmake
@@ -1,1 +1,8 @@
+# This file is part of the dune-xt-functions project:
+#   https://github.com/dune-community/dune-xt-functions
+# The copyright lies with the authors of this file (see below).
+# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+# Authors:
+#   Felix Schindler (2016)
+#
 # File for module specific CMake tests.
