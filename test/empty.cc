ace9d54fd43ed5d254b70f6c9e52290427fa80b4
Felix Schindler <felix.schindler@wwu.de>
2016-01-21 17:17:52 +0100

    [test] add empty test

---
+++ test/empty.cc
@@ -0,0 +1,16 @@
+// This file is part of the dune-xt-grid project:
+//   https://github.com/dune-community/dune-xt-grid
+// The copyright lies with the authors of this file (see below).
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+// Authors:
+
+/**
+  * This file is intended as a starting point for quick testing.
+  */
+
+#include <dune/xt/common/test/main.hxx>
+
+
+TEST(empty, main)
+{
+}
