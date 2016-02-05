64bb62d752996bae189edd4ebe317604c1873191
Felix Schindler <felix.schindler@wwu.de>
2016-02-05 17:55:11 +0100

    [gridprovider] start to refactor dgf

--- dune/xt/grid/gridprovider/dgf.hh
+++ dune/xt/grid/gridprovider/dgf.hh
@@ -30,88 +30,50 @@
 #include <dune/xt/common/configuration.hh>
 #include <dune/xt/common/string.hh>
 
-#include <dune/xt/grid/provider/interface.hh>
+#include "provider.hh"
 
 namespace Dune {
 namespace XT {
 namespace Grid {
-namespace Providers {
+namespace internal {
 
 
-template <class GridImp>
-class DGF : public Grid::ProviderInterface<GridImp>
+template <class GridType>
+static GridProvider<GridType> create_dgf_grid(const std::string& filename)
 {
-  typedef Grid::ProviderInterface<GridImp> BaseType;
-  typedef DGF<GridImp> ThisType;
-
-public:
-  using typename BaseType::GridType;
-  using BaseType::dimDomain;
-
-  static const std::string static_id()
-  {
-    return BaseType::static_id() + ".dgf";
-  }
-
-  static Common::Configuration default_config(const std::string sub_name = "")
-  { // size_t(...) required, else linker error with clang
-    Common::Configuration config("filename", "dgf_" + Common::to_string(size_t(dimDomain)) + "d_interval.dgf");
-    if (sub_name.empty())
-      return config;
-    else {
-      Common::Configuration tmp;
-      tmp.add(config, sub_name);
-      return tmp;
-    }
-  } // ... default_config(...)
-
-  static std::unique_ptr<ThisType> create(const Common::Configuration config = default_config(),
-                                          const std::string sub_name = static_id())
-  {
-    const Common::Configuration cfg         = config.has_sub(sub_name) ? config.sub(sub_name) : config;
-    const Common::Configuration default_cfg = default_config();
-    return Common::make_unique<ThisType>(cfg.get("filename", default_cfg.get<std::string>("filename")));
-  }
-
-  DGF(const std::string filename)
-  {
-    grid_ = std::shared_ptr<GridType>(GridPtr<GridType>(filename).release());
-  }
-
-  DGF(ThisType&& source) = default;
-  DGF(const ThisType& other) = default;
-
-  virtual ~DGF() = default;
-
-  ThisType& operator=(const ThisType& other) = default;
-  ThisType& operator=(ThisType&& source) = default;
-
-  virtual const GridType& grid() const override final
-  {
-    return *grid_;
-  }
-
-  virtual GridType& grid() override final
-  {
-    return *grid_;
-  }
-
-  const std::shared_ptr<const GridType> grid_ptr() const
-  {
-    return grid_;
-  }
-
-  std::shared_ptr<GridType> grid_ptr()
-  {
-    return grid_;
-  }
-
-private:
-  std::shared_ptr<GridType> grid_;
-}; // class DGF
+  return GridProvider<GridType>(GridPtr<GridType>(filename).release());
+}
 
 
-} // namespace Providers
+} // namespace internal
+
+
+static inline Common::Configuration dgf_gridprovider_default_config()
+{
+  Common::Configuration config;
+  config["type"]     = "xt.grid.gridprovider.dgf";
+  config["filename"] = "dgf_1d_interval.dgf";
+  return config;
+}
+
+
+template <class GridType>
+typename std::enable_if<is_grid<GridType>::value, GridProvider<GridType>>::type
+make_dgf_grid(const std::string& filename)
+{
+  return internal::create_dgf_grid<GridType>(filename);
+}
+
+
+template <class GridType>
+typename std::enable_if<is_grid<GridType>::value, GridProvider<GridType>>::type
+make_dgf_grid(const Common::Configuration& cfg = dgf_gridprovider_default_config())
+{
+  auto filename = cfg.get("filename", dgf_gridprovider_default_config().get<std::string>("filename"));
+  return internal::create_dgf_grid<GridType>(filename);
+}
+
+
 } // namespace Grid
 } // namespace XT
 } // namespace Dune
