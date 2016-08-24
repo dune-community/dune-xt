08721d9738f89fd0bfd9ccf2c6bc883eddc96d81
Rene Milk <rene.milk@wwu.de>
2016-08-24 14:30:55 +0200

    [stuff-leftovers] adds EOC gridprovider

---
+++ dune/xt/grid/gridprovider/eoc.hh
@@ -0,0 +1,99 @@
+// This file is part of the dune-stuff project:
+//   https://github.com/wwu-numerik/dune-stuff
+// Copyright holders: Rene Milk, Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_XT_GRID_PROVIDER_EOC_HH
+#define DUNE_XT_GRID_PROVIDER_EOC_HH
+
+#if HAVE_DUNE_GRID
+#include <dune/grid/io/file/dgfparser.hh>
+#endif
+
+#include <dune/xt/grid/gridprovider/provider.hh>
+
+namespace Dune {
+namespace XT {
+namespace Grid {
+
+/**
+ *  The purpose of this class is to behave like a XT::Grid::GridProvider and at the same time to provide a
+ *  means to obtain the real grid level corresponding to a refinement level.
+ */
+template <class GridImp>
+class EOCGridProvider : public GridProvider<GridImp>
+{
+  typedef GridProvider<GridImp> BaseType;
+
+public:
+  using typename BaseType::GridType;
+
+  explicit EOCGridProvider(GridType& grd, const size_t num_refs)
+    : BaseType(grd)
+  {
+    setup(num_refs);
+  }
+
+  explicit EOCGridProvider(GridType* grid_ptr, const size_t num_refs)
+    : BaseType(grid_ptr)
+  {
+    setup(num_refs);
+  }
+
+  explicit EOCGridProvider(std::shared_ptr<GridType> grid_ptr, const size_t num_refs)
+    : BaseType(grid_ptr)
+  {
+    setup(num_refs);
+  }
+
+  explicit EOCGridProvider(std::unique_ptr<GridType>&& grid_ptr, const size_t num_refs)
+    : BaseType(grid_ptr)
+  {
+    setup(num_refs);
+  }
+
+  size_t num_refinements() const
+  {
+    assert(levels_.size() > 0);
+    return levels_.size() - 1;
+  }
+
+  int level_of(const size_t refinement) const
+  {
+    assert(refinement <= num_refinements());
+    return levels_[refinement];
+  }
+
+  int reference_level() const
+  {
+    return reference_level_;
+  }
+
+  typename BaseType::LevelGridViewType reference_grid_view() const
+  {
+    return this->level_view(reference_level_);
+  }
+
+private:
+  void setup(const size_t num_refinements)
+  {
+    levels_.push_back(this->grid().maxLevel());
+    static const int refine_steps_for_half = DGFGridInfo<GridType>::refineStepsForHalf();
+    for (size_t rr = 0; rr < num_refinements; ++rr) {
+      this->grid().globalRefine(refine_steps_for_half);
+      levels_.push_back(this->grid().maxLevel());
+    }
+    this->grid().globalRefine(refine_steps_for_half);
+    reference_level_ = this->grid().maxLevel();
+  } // ... setup(...)
+
+  std::vector<int> levels_;
+  int reference_level_;
+}; // class EOCGridProvider
+
+
+} // namespace Grid
+} // namespace Stuff
+} // namespace Dune
+
+#endif // DUNE_XT_GRID_PROVIDER_EOC_HH
