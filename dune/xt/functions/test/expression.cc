bf5045c06c51ce88ca5855e81ab145e92a1c6616
Tobias Leibner <tobias.leibner@googlemail.com>
2016-12-12 12:43:41 +0100

    [test.expression] fix "gradient is both key and subtree" error

--- dune/xt/functions/test/expression.cc
+++ dune/xt/functions/test/expression.cc
@@ -35,9 +35,13 @@ struct ExpressionFunctionTest : public FunctionTest<TESTFUNCTIONTYPE>
     Common::Configuration config = FunctionType::default_config();
     const std::unique_ptr<const FunctionType> function(FunctionType::create(config));
     config["expression"] = "[2*x[0] 3*x[0] 4*x[0]; 1 sin(x[0]) 0; cos(x[0]) x[0] 0]";
-    config["gradient"] = "[2 0 0; 0 0 0; -sin(x[0]) 0 0]";
-    config["gradient.1"] = "[3 0 0; cos(x[0]) 0 0; 1 0 0]";
-    config["gradient.2"] = "[4 0 0; 0 0 0; 0 0 0]";
+    if (dimRangeCols == 1)
+      config["gradient"] = "[2 0 0; 0 0 0; -sin(x[0]) 0 0]";
+    else {
+      config["gradient.0"] = "[2 0 0; 0 0 0; -sin(x[0]) 0 0]";
+      config["gradient.1"] = "[3 0 0; cos(x[0]) 0 0; 1 0 0]";
+      config["gradient.2"] = "[4 0 0; 0 0 0; 0 0 0]";
+    }
     const std::unique_ptr<const FunctionType> function2(FunctionType::create(config));
     const std::unique_ptr<const FunctionType> function3(
         new FunctionType("x", "sin(x[0])", 3, FunctionType::static_id(), {"cos(x[0])", "0", "0"}));
