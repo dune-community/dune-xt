715999b1f932fd37c0773fb8edfc836e2314ff54
Rene Milk <rene.milk@wwu.de>
2017-01-26 17:28:31 +0100

    [ranged] fix headercheck

--- dune/xt/grid/parallel/partitioning/ranged.hh
+++ dune/xt/grid/parallel/partitioning/ranged.hh
@@ -1,6 +1,7 @@
 #ifndef DUNE_XT_GRID_PARALLEL_PARTITIONING_RANGED_HH
 #define DUNE_XT_GRID_PARALLEL_PARTITIONING_RANGED_HH
 
+#include <dune/common/version.hh>
 #if DUNE_VERSION_NEWER(DUNE_GRID, 3, 9) // EXADUNE
 #include <dune/grid/utility/partitioning/ranged.hh>
 #else
