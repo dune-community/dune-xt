7ffd3eed7f87a7774606d21e5b40a9ea7fa963c1
Felix Schindler <felix.schindler@wwu.de>
2017-02-22 08:13:56 +0100

    [license] update cfg and license statement everywhere

--- dune/xt/la/solver/fasp.hh
+++ dune/xt/la/solver/fasp.hh
@@ -1,12 +1,12 @@
 // This file is part of the dune-xt-la project:
 //   https://github.com/dune-community/dune-xt-la
-// The copyright lies with the authors of this file (see below).
-// License: Dual licensed as  BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+// Copyright 2009-2017 dune-xt-la developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 //      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 //          with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
 //   Felix Schindler (2013 - 2014, 2016)
-//   Rene Milk       (2013, 2015)
+//   Rene Milk       (2013, 2015 - 2016)
 
 #ifndef DUNE_XT_LA_SOLVER_FASP_HH
 #define DUNE_XT_LA_SOLVER_FASP_HH
--- python/dune/xt/__init__.py
+++ python/dune/xt/__init__.py
@@ -1,10 +1,10 @@
 # This file is part of the dune-xt-la project:
 #   https://github.com/dune-community/dune-xt-la
-# The copyright lies with the authors of this file (see below).
+# Copyright 2009-2017 dune-xt-la developers and contributors. All rights reserved.
 # License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 #      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 #          with "runtime exception" (http://www.dune-project.org/license.html)
 # Authors:
-#   Felix Schindler (2016 - 2017)
+#   Felix Schindler (2017)
 
 __import__('pkg_resources').declare_namespace(__name__)
