33fe644596506736e608ed7ff1aa1f0609db4f48
Felix Schindler <felix.schindler@wwu.de>
2017-02-23 10:15:07 +0100

    [license] update cfg and apply license statement everywhere

--- dune/xt/functions/composition.hh
+++ dune/xt/functions/composition.hh
@@ -1,7 +1,13 @@
-// This file is part of the dune-stuff project:
-//   https://github.com/wwu-numerik/dune-stuff/
-// Copyright holders: Rene Milk, Felix Schindler
-// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+// This file is part of the dune-xt-functions project:
+//   https://github.com/dune-community/dune-xt-functions
+// Copyright 2009-2017 dune-xt-functions developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler (2017)
+//   Rene Milk       (2016 - 2017)
+//   Tobias Leibner  (2016)
 
 #ifndef DUNE_XT_FUNCTIONS_COMPOSITION_HH
 #define DUNE_XT_FUNCTIONS_COMPOSITION_HH
--- dune/xt/functions/reinterpret.hh
+++ dune/xt/functions/reinterpret.hh
@@ -1,7 +1,12 @@
-// This file is part of the dune-xt project:
-//   https://github.com/wwu-numerik/dune-stuff/
-// Copyright holders: Rene Milk, Felix Schindler
-// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+// This file is part of the dune-xt-functions project:
+//   https://github.com/dune-community/dune-xt-functions
+// Copyright 2009-2017 dune-xt-functions developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Rene Milk      (2016)
+//   Tobias Leibner (2016)
 
 #ifndef DUNE_XT_FUNCTION_REINTERPRET_HH
 #define DUNE_XT_FUNCTION_REINTERPRET_HH
