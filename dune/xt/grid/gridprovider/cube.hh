4eee27366991c248887f6ac3e8dc49d0a5a75e3b
Felix Schindler <felix.schindler@wwu.de>
2017-03-08 14:32:01 +0100

    [gridprovider.cube] add make_cube_dd_subdomains_grid()

--- dune/xt/grid/gridprovider/cube.hh
+++ dune/xt/grid/gridprovider/cube.hh
@@ -27,11 +27,11 @@
 #include <dune/xt/common/fvector.hh>
 #include <dune/xt/common/memory.hh>
 #include <dune/xt/common/misc.hh>
-
 #include <dune/xt/grid/structuredgridfactory.hh>
 #include <dune/xt/grid/type_traits.hh>
-
 #include <dune/xt/grid/grids.hh>
+#include <dune/xt/grid/dd/subdomains/factory.hh>
+#include <dune/xt/grid/dd/subdomains/grid.hh>
 
 #include "provider.hh"
 
@@ -55,6 +55,22 @@ static inline Common::Configuration cube_gridprovider_default_config()
   config["num_elements"] = "[8 8 8 8]";
   config["num_refinements"] = "0";
   config["overlap_size"] = "[1 1 1 1]";
+  return config;
+}
+
+
+static inline std::string cube_dd_subdomains_gridprovider_id()
+{
+  return "xt.grid.gridprovider.dd.subdomains.cube";
+}
+
+
+static inline Common::Configuration cube_dd_subdomains_gridprovider_default_config()
+{
+  Common::Configuration config = cube_gridprovider_default_config();
+  config["num_partitions"] = "[2 2 2 2]";
+  config["oversampling_layers"] = "0";
+  config["inner_boundary_segment_index"] = Common::to_string(std::numeric_limits<size_t>::max() - 42);
   return config;
 }
 
@@ -257,6 +273,151 @@ make_cube_grid(const Common::Configuration& cfg = cube_gridprovider_default_conf
   return CubeGridProviderFactory<GridType>::create(cfg);
 }
 
+
+#if HAVE_DUNE_FEM
+
+
+template <class GridType>
+class CubeDdSubdomainsGridProviderFactory
+{
+  static_assert(XT::Grid::is_grid<GridType>::value, "");
+
+public:
+  typedef grid::Multiscale::Default<GridType> DdGridType;
+
+  static std::string static_id()
+  {
+    return cube_dd_subdomains_gridprovider_id();
+  }
+
+  static XT::Common::Configuration default_config()
+  {
+    return cube_dd_subdomains_gridprovider_default_config();
+  }
+
+  static GridProvider<GridType, DdGridType>
+  create(const FieldVector<typename GridType::ctype, GridType::dimension>& lower_left,
+         const FieldVector<typename GridType::ctype, GridType::dimension>& upper_right,
+         const std::array<unsigned int, GridType::dimension>& num_elements,
+         const unsigned int num_refinements,
+         const std::array<unsigned int, GridType::dimension>& overlap_size,
+         const std::array<unsigned int, GridType::dimension>& num_partitions,
+         const size_t num_oversampling_layers,
+         const size_t inner_boundary_segment_index)
+  {
+    auto grid =
+        make_cube_grid<GridType>(lower_left, upper_right, num_elements, num_refinements, overlap_size).grid_ptr();
+
+    typedef grid::Multiscale::Factory::Default<GridType> MsGridFactoryType;
+    const size_t neighbor_recursion_level = grid::Multiscale::Factory::NeighborRecursionLevel<GridType>::compute();
+    // prepare
+    MsGridFactoryType factory(*grid, inner_boundary_segment_index);
+    factory.prepare();
+    // global grid part
+    const auto global_grid_part = factory.globalGridPart();
+    // walk the grid
+    const auto entity_it_end = global_grid_part->template end<0>();
+    for (auto entity_it = global_grid_part->template begin<0>(); entity_it != entity_it_end; ++entity_it) {
+      // get center of entity
+      const auto& entity = *entity_it;
+      const auto center = entity.geometry().center();
+      // decide on the subdomain this entity shall belong to
+      std::vector<size_t> whichPartition(GridType::dimension, 0);
+      for (size_t dd = 0; dd < GridType::dimension; ++dd)
+        whichPartition[dd] =
+            (std::min((unsigned int)(std::floor(
+                          num_partitions[dd] * ((center[dd] - lower_left[dd]) / (upper_right[dd] - lower_left[dd])))),
+                      num_partitions[dd] - 1));
+      size_t subdomain = 0;
+      if (GridType::dimension == 1)
+        subdomain = whichPartition[0];
+      else if (GridType::dimension == 2)
+        subdomain = whichPartition[0] + whichPartition[1] * num_partitions[0];
+      else if (GridType::dimension == 3)
+        subdomain = whichPartition[0] + whichPartition[1] * num_partitions[0]
+                    + whichPartition[2] * num_partitions[1] * num_partitions[0];
+      else
+        DUNE_THROW(Dune::NotImplemented,
+                   "ERROR in " << static_id() << ": not implemented for grid dimensions other than 1, 2 or 3!");
+      // add entity to subdomain
+      factory.add(entity, subdomain /*, prefix + "  ", out*/);
+    } // walk the grid
+    // finalize
+    factory.finalize(num_oversampling_layers, neighbor_recursion_level /*, prefix + "  ", out*/);
+    // be done with it
+    return GridProvider<GridType, DdGridType>(grid, factory.createMsGrid());
+  } // ... create(...)
+}; // class CubeDdSubdomainsGridProviderFactory
+
+
+template <class GridType>
+typename std::enable_if<XT::Grid::is_grid<GridType>::value,
+                        GridProvider<GridType, grid::Multiscale::Default<GridType>>>::type
+make_cube_dd_subdomains_grid(
+    const FieldVector<typename GridType::ctype, GridType::dimension>& lower_left,
+    const FieldVector<typename GridType::ctype, GridType::dimension>& upper_right,
+    const std::array<unsigned int, GridType::dimension> num_elements =
+        cube_dd_subdomains_gridprovider_default_config().template get<std::vector<unsigned int>>("num_elements")[0],
+    const unsigned int num_refinements =
+        cube_dd_subdomains_gridprovider_default_config().template get<unsigned int>("num_refinements"),
+    const std::array<unsigned int, GridType::dimension> overlap_size =
+        XT::Common::make_array<unsigned int, GridType::dimension>(
+            cube_dd_subdomains_gridprovider_default_config().template get<std::vector<unsigned int>>("overlap_size")),
+    const std::array<unsigned int, GridType::dimension> num_partitions =
+        XT::Common::make_array<unsigned int, GridType::dimension>(
+            cube_dd_subdomains_gridprovider_default_config().template get<std::vector<unsigned int>>("num_partitions")),
+    const size_t num_oversampling_layers =
+        cube_dd_subdomains_gridprovider_default_config().template get<size_t>("num_refinements"),
+    const size_t inner_boundary_segment_index =
+        cube_dd_subdomains_gridprovider_default_config().template get<int>("inner_boundary_segment_index"))
+{
+  return CubeDdSubdomainsGridProviderFactory<GridType>::create(lower_left,
+                                                               upper_right,
+                                                               num_elements,
+                                                               num_refinements,
+                                                               overlap_size,
+                                                               num_partitions,
+                                                               num_oversampling_layers,
+                                                               inner_boundary_segment_index);
+}
+
+
+#else // HAVE_DUNE_FEM
+
+
+template <class GridType>
+class CubeDdSubdomainsGridProviderFactory
+{
+  static_assert(AlwaysFalse<GridType>::value, "You are missing dune-fem!");
+};
+
+
+template <class GridType>
+typename std::enable_if<XT::Grid::is_grid<GridType>::value,
+                        GridProvider<GridType, grid::Multiscale::Default<GridType>>>::type
+make_cube_dd_subdomains_grid(
+    const FieldVector<typename GridType::ctype, GridType::dimension>& /*lower_left*/,
+    const FieldVector<typename GridType::ctype, GridType::dimension>& /*upper_right*/,
+    const std::array<unsigned int, GridType::dimension> /*num_elements*/ =
+        cube_dd_subdomains_gridprovider_default_config().template get<std::vector<unsigned int>>("num_elements")[0],
+    const unsigned int /*num_refinements*/ =
+        cube_dd_subdomains_gridprovider_default_config().template get<unsigned int>("num_refinements"),
+    const std::array<unsigned int, GridType::dimension> /*overlap_size*/ =
+        XT::Common::make_array<unsigned int, GridType::dimension>(
+            cube_dd_subdomains_gridprovider_default_config().template get<std::vector<unsigned int>>("overlap_size")),
+    const std::array<unsigned int, GridType::dimension> /*num_partitions*/ =
+        XT::Common::make_array<unsigned int, GridType::dimension>(
+            cube_dd_subdomains_gridprovider_default_config().template get<std::vector<unsigned int>>("num_partitions")),
+    const size_t /*num_oversampling_layers*/ =
+        cube_dd_subdomains_gridprovider_default_config().template get<size_t>("num_refinements"),
+    const size_t /*inner_boundary_segment_index*/ =
+        cube_dd_subdomains_gridprovider_default_config().template get<int>("inner_boundary_segment_index"))
+{
+  static_assert(AlwaysFalse<GridType>::value, "You are missing dune-fem!");
+}
+
+
+#endif // HAVE_DUNE_FEM
 
 } // namespace Grid
 } // namespace XT
