614df4fa26c5f31ec4a628845d6ba3538fc3b1d4
Felix Schindler <felix.schindler@wwu.de>
2017-03-21 16:25:57 +0100

    [grids.bindings] add grid_name, move defines to top namespace

--- dune/xt/grid/grids.bindings.hh
+++ dune/xt/grid/grids.bindings.hh
@@ -10,18 +10,65 @@
 #ifndef DUNE_XT_GRID_GRIDS_BINDINGS_HH
 #define DUNE_XT_GRID_GRIDS_BINDINGS_HH
 
-#include "grids.hh"
+#include <dune/xt/common/string.hh>
 
-namespace Dune {
+#include "grids.hh"
 
 
 // this is used by other headers
-typedef YaspGrid<2, EquidistantOffsetCoordinates<double, 2>> YASP_2D_EQUIDISTANT_OFFSET;
+typedef Dune::YaspGrid<2, Dune::EquidistantOffsetCoordinates<double, 2>> YASP_2D_EQUIDISTANT_OFFSET;
 #if HAVE_DUNE_ALUGRID
-typedef ALUGrid<2, 2, simplex, conforming> ALU_2D_SIMPLEX_CONFORMING;
+typedef Dune::ALUGrid<2, 2, Dune::simplex, Dune::conforming> ALU_2D_SIMPLEX_CONFORMING;
 #endif
 
 
+namespace Dune {
+namespace XT {
+namespace Grid {
+namespace bindings {
+
+
+template <class G>
+struct grid_name
+{
+  static_assert(AlwaysFalse<G>::value, "Please add a specialization for this grid!");
+
+  static std::string value()
+  {
+    return "";
+  }
+};
+
+
+template <int dim>
+struct grid_name<YaspGrid<dim, EquidistantOffsetCoordinates<double, dim>>>
+{
+  static std::string value()
+  {
+    return Common::to_string(dim) + "d_cube_yaspgrid";
+  }
+};
+
+
+#if HAVE_DUNE_ALUGRID
+
+
+template <int dim, class Comm>
+struct grid_name<ALUGrid<dim, dim, simplex, conforming, Comm>>
+{
+  static std::string value()
+  {
+    return Common::to_string(dim) + "d_simplex_aluconformgrid";
+  }
+};
+
+
+#endif // HAVE_DUNE_ALUGRID
+
+
+} // namespace bindings
+} // namespace Grid
+} // namespace XT
 } // namespace Dune
 
 #endif // DUNE_XT_GRID_GRIDS_BINDINGS_HH
