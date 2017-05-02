08a4ecf93345f7b5718039364c8ce01bfa81aa61
Felix Schindler <felix.schindler@wwu.de>
2017-05-02 16:57:43 +0200

    [bindings] add test_logger(), enable logging after init_logger()

--- python/dune/xt/functions/__init__.py
+++ python/dune/xt/functions/__init__.py
@@ -12,6 +12,7 @@ from importlib import import_module
 from dune.xt.common import DEBUG # inits MPI via mpi4py
 
 _init_logger_methods = list()
+_test_logger_methods = list()
 _init_mpi_methods = list()
 _other_modules = ('xt.common', 'xt.la', 'xt.grid', 'gdt')
 
@@ -19,13 +20,14 @@ from ._functions import __dict__ as module
 to_import = [name for name in module if not name.startswith('_')]
 globals().update({name: module[name] for name in to_import})
 _init_logger_methods.append(module['_init_logger'])
+_test_logger_methods.append(module['_test_logger'])
 _init_mpi_methods.append(module['_init_mpi'])
 del to_import
 del module
 
 
-def init_logger(max_info_level=-1,
-                max_debug_level=-1,
+def init_logger(max_info_level=999,
+                max_debug_level=999,
                 enable_warnings=True,
                 enable_colors=True,
                 info_color='blue',
@@ -42,6 +44,18 @@ def init_logger(max_info_level=-1,
     for init_logger_method in init_logger_methods:
         init_logger_method(max_info_level, max_debug_level, enable_warnings, enable_colors, info_color, debug_color,
                            warning_color)
+
+def test_logger(info=True, debug=True, warning=True):
+    test_logger_methods = _test_logger_methods.copy()
+    for module_name in _other_modules:
+        try:
+            mm = import_module('dune.{}'.format(module_name))
+            for test_logger_method in mm._test_logger_methods:
+                test_logger_methods.append(test_logger_method)
+        except ModuleNotFoundError:
+            pass
+    for test_logger_method in test_logger_methods:
+        test_logger_method(info, debug, warning)
 
 def init_mpi(args=list()):
     if DEBUG:
