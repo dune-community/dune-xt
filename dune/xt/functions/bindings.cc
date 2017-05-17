e8782a73f930e636fa1ac6342e284aabf84d1fbf
Felix Schindler <felix.schindler@wwu.de>
2017-05-17 13:49:00 +0200

    [bindings] update numeric_cast and include type casters

--- dune/xt/functions/bindings.cc
+++ dune/xt/functions/bindings.cc
@@ -14,8 +14,6 @@
 #include <string>
 #include <vector>
 
-#include <dune/xt/common/exceptions.bindings.hh>
-
 #include <dune/common/parallel/mpihelper.hh>
 
 #if HAVE_DUNE_FEM
@@ -25,10 +23,7 @@
 #include <dune/pybindxi/pybind11.h>
 #include <dune/pybindxi/stl.h>
 
-#include <dune/xt/common/timedlogging.hh>
-#include <dune/xt/common/configuration.pbh>
-#include <dune/xt/common/fvector.pbh>
-
+#include <dune/xt/common/bindings.hh>
 #include <dune/xt/grid/grids.hh>
 
 #include "interfaces.pbh"
@@ -298,7 +293,7 @@ PYBIND11_PLUGIN(_functions)
 
   m.def("_init_mpi",
         [](const std::vector<std::string>& args) {
-          int argc = boost::numeric_cast<int>(args.size());
+          int argc = Dune::XT::Common::numeric_cast<int>(args.size());
           char** argv = Dune::XT::Common::vector_to_main_args(args);
           Dune::MPIHelper::instance(argc, argv);
 #if HAVE_DUNE_FEM
