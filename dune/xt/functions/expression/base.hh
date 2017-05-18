849c4ecf7c81c225d6f88978e7890566af1f59ad
Felix Schindler <felix.schindler@wwu.de>
2017-05-18 14:20:49 +0200

    [functions.expression.base] add DynamicMathExpressionBase

--- dune/xt/functions/expression/base.hh
+++ dune/xt/functions/expression/base.hh
@@ -21,17 +21,23 @@
 #include <dune/common/exceptions.hh>
 #include <dune/common/fvector.hh>
 
-#include <dune/xt/common/color.hh>
+#include <dune/xt/common/exceptions.hh>
 #include <dune/xt/common/string.hh>
 
 #include "mathexpr.hh"
+
+#ifndef DUNE_XT_FUNCTIONS_EXPRESSION_BASE_MAX_DYNAMIC_SIZE
+#define DUNE_XT_FUNCTIONS_EXPRESSION_BASE_MAX_DYNAMIC_SIZE 64
+#endif
+
 
 namespace Dune {
 namespace XT {
 namespace Functions {
 
+
 /**
- *  \brief base class that makes a function out of the from mathexpr.hh
+ *  \brief      Base class that makes a function out of the stuff from mathexpr.hh
  *  \attention  Most surely you do not want to use this class directly, but Functions::ExpressionFunction!
  */
 template <class DomainFieldImp, size_t domainDim, class RangeFieldImp, size_t rangeDim>
@@ -220,6 +226,141 @@ private:
   ROperation* op_[dimRange];
   mutable std::mutex mutex_;
 }; // class MathExpressionBase
+
+
+template <class D, class R, size_t r, size_t max_d = DUNE_XT_FUNCTIONS_EXPRESSION_BASE_MAX_DYNAMIC_SIZE>
+class DynamicMathExpressionBase
+{
+public:
+  typedef DynamicMathExpressionBase<D, R, r, max_d> ThisType;
+
+  typedef D DomainFieldType;
+  static const size_t maxDimDomain = max_d;
+
+  typedef R RangeFieldType;
+  static const size_t dimRange = r;
+
+  DynamicMathExpressionBase(const std::vector<std::string>& vars, const std::vector<std::string>& exprs)
+  {
+    setup(vars, exprs);
+  }
+
+  DynamicMathExpressionBase(const ThisType& other)
+  {
+    setup(other.variables(), other.expressions());
+  }
+
+  ThisType& operator=(const ThisType& other)
+  {
+    if (this != &other) {
+      cleanup();
+      original_variables_ = other.original_variables_;
+      variables_ = std::vector<std::string>();
+      expressions_ = std::vector<std::string>();
+      setup(other.original_variables_, other.expressions_);
+    }
+    return this;
+  }
+
+  ~DynamicMathExpressionBase()
+  {
+    cleanup();
+  }
+
+  const std::vector<std::string>& variables() const
+  {
+    return original_variables_;
+  }
+
+  const std::vector<std::string>& expressions() const
+  {
+    return expressions_;
+  }
+
+  void evaluate(const DynamicVector<DomainFieldType>& arg, DynamicVector<RangeFieldType>& ret) const
+  {
+    std::lock_guard<std::mutex> guard(mutex_);
+    // check for sizes
+    if (arg.size() != original_variables_.size())
+      DUNE_THROW(Common::Exceptions::shapes_do_not_match,
+                 "arg.size(): " << arg.size() << "\n   "
+                                << "variables.size(): "
+                                << original_variables_.size());
+    if (ret.size() < dimRange)
+      ret = Dune::DynamicVector<RangeFieldType>(dimRange);
+    // copy arg
+    for (size_t ii = 0; ii < original_variables_.size(); ++ii)
+      *(arg_[ii]) = arg[ii];
+    // copy ret
+    for (size_t ii = 0; ii < dimRange; ++ii)
+      ret[ii] = op_[ii]->Val();
+  }
+
+private:
+  void setup(const std::vector<std::string>& vars, const std::vector<std::string>& exprs)
+  {
+    static_assert((maxDimDomain > 0), "");
+    static_assert((dimRange > 0), "");
+    // set expressions
+    if (exprs.size() != dimRange)
+      DUNE_THROW(Common::Exceptions::shapes_do_not_match,
+                 "expressions.size(): " << exprs.size() << "\n   "
+                                        << "dimRange: "
+                                        << dimRange);
+    expressions_ = exprs;
+    for (const auto& ex : expressions_)
+      if (ex.empty())
+        DUNE_THROW(Common::Exceptions::wrong_input_given, "Given expressions must not be empty!");
+    if (vars.size() > maxDimDomain)
+      DUNE_THROW(
+          Common::Exceptions::shapes_do_not_match,
+          "This expression function of dynamic size was compiled to work for up to "
+              << DUNE_XT_FUNCTIONS_EXPRESSION_BASE_MAX_DYNAMIC_SIZE
+              << " variables, but you provided "
+              << vars.size()
+              << "!\n\n"
+              << "Configure dune-xt-functions with a larger DUNE_XT_FUNCTIONS_EXPRESSION_BASE_MAX_DYNAMIC_SIZE!");
+    original_variables_ = vars;
+    for (const auto& var : original_variables_)
+      if (var.empty())
+        DUNE_THROW(Common::Exceptions::wrong_input_given, "Given variables must not be empty!");
+    variables_ = original_variables_;
+    for (size_t ii = variables_.size(); ii < maxDimDomain; ++ii)
+      variables_.push_back("this_is_a_long_dummy_name_to_make_sure_it_is_not_used_" + Common::to_string(ii));
+    assert(variables_.size() == maxDimDomain);
+    // create expressions
+    for (size_t ii = 0; ii < maxDimDomain; ++ii) {
+      arg_[ii] = new DomainFieldType(0.0);
+      var_arg_[ii] = new RVar(variables_[ii].c_str(), arg_[ii]);
+      vararray_[ii] = var_arg_[ii];
+    }
+    for (size_t ii = 0; ii < dimRange; ++ii) {
+      op_[ii] = new ROperation(expressions_[ii].c_str(), maxDimDomain, vararray_);
+    }
+  } // void setup(const std::string& _variable, const std::vector< std::string >& expressions)
+
+  void cleanup()
+  {
+    for (size_t ii = 0; ii < dimRange; ++ii) {
+      delete op_[ii];
+    }
+    for (size_t ii = 0; ii < maxDimDomain; ++ii) {
+      delete var_arg_[ii];
+      delete arg_[ii];
+    }
+  } // void cleanup()
+
+  std::vector<std::string> original_variables_;
+  std::vector<std::string> variables_;
+  std::vector<std::string> expressions_;
+  size_t actualDimRange_;
+  mutable DomainFieldType* arg_[maxDimDomain];
+  RVar* var_arg_[maxDimDomain];
+  RVar* vararray_[maxDimDomain];
+  ROperation* op_[dimRange];
+  mutable std::mutex mutex_;
+}; // class DynamicMathExpressionBase
+
 
 } // namespace Functions
 } // namespace XT
