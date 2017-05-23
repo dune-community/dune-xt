1d70e41014cbca39604e3080b158b05a64df9ac5
Felix Schindler <felix.schindler@wwu.de>
2017-05-23 19:35:09 +0200

    [gridprovider.factory] add DdSubdomainGridProviderFactory

--- dune/xt/grid/gridprovider/factory.hh
+++ dune/xt/grid/gridprovider/factory.hh
@@ -181,6 +181,147 @@ public:
 }; // class GridProviderFactory
 
 
+template <class GridType>
+class DdSubdomainGridProviderFactory
+{
+  template <class G, bool available = false>
+  struct Helper
+  {
+    static std::vector<std::string> append(std::vector<std::string> in)
+    {
+      return in;
+    }
+
+    static bool compare(const std::string& /*type*/)
+    {
+      return false;
+    }
+
+    static Common::Configuration default_config()
+    {
+      DUNE_THROW(Common::Exceptions::internal_error, "This should not happen!");
+      return Common::Configuration(0);
+    }
+
+    static GridProvider<GridType, DD::SubdomainGrid<GridType>> create(const Common::Configuration& /*cfg*/)
+    {
+      DUNE_THROW(Common::Exceptions::internal_error, "This should not happen!");
+      return GridProvider<GridType, DD::SubdomainGrid<GridType>>(nullptr);
+    }
+  }; // struct Helper
+
+  template <class G>
+  struct Helper<G, true>
+  {
+    static std::vector<std::string> append(std::vector<std::string> in)
+    {
+      in.push_back(G::static_id());
+      return in;
+    }
+
+    static bool compare(const std::string& type)
+    {
+      return type == G::static_id();
+    }
+
+    static Common::Configuration default_config()
+    {
+      return G::default_config();
+    }
+
+    static GridProvider<GridType, DD::SubdomainGrid<GridType>> create(const Common::Configuration& cfg)
+    {
+      if (cfg.empty())
+        return G::create();
+      else
+        return G::create(cfg);
+    }
+  }; // struct Helper< ..., true >
+
+  template <class G>
+  static std::vector<std::string> call_append(std::vector<std::string> in)
+  {
+    return Helper<G, G::available>::append(in);
+  }
+
+  template <class G>
+  static bool call_compare(const std::string& type)
+  {
+    return Helper<G, G::available>::compare(type);
+  }
+
+  template <class G>
+  static Common::Configuration call_default_config(const std::string sub_name)
+  {
+    return Helper<G, G::available>::default_config(sub_name);
+  }
+
+  template <class G>
+  static GridProvider<GridType, DD::SubdomainGrid<GridType>> call_create(const Common::Configuration& cfg)
+  {
+    return Helper<G, G::available>::create(cfg);
+  }
+
+  static std::string available_as_str()
+  {
+    std::string ret = "";
+    const auto vals = available();
+    if (vals.size() > 0) {
+      ret += vals[0];
+      for (size_t ii = 1; ii < vals.size(); ++ii)
+        ret += "\n   " + vals[ii];
+    }
+    return ret;
+  } // ... available_as_str(...)
+
+  typedef CubeDdSubdomainsGridProviderFactory<GridType> CubeType;
+
+public:
+  static std::vector<std::string> available()
+  {
+    std::vector<std::string> ret;
+    call_append<CubeType>(ret);
+    return ret;
+  }
+
+  static Common::Configuration default_config(const std::string type)
+  {
+    if (call_compare<CubeType>(type))
+      return call_default_config<CubeType>();
+    else if (available().empty())
+      DUNE_THROW(Common::Exceptions::wrong_input_given,
+                 "There is no grid provider available for " << Common::Typename<GridType>::value() << "!");
+    else
+      DUNE_THROW(Common::Exceptions::wrong_input_given,
+                 "Requested type '" << type << "' is not one of those avaible for "
+                                    << Common::Typename<GridType>::value()
+                                    << ":\n"
+                                    << available_as_str());
+  } // ... default_config(...)
+
+  static GridProvider<GridType, DD::SubdomainGrid<GridType>> create(const Common::Configuration& config)
+  {
+    return create(config.get<std::string>("type"), config);
+  }
+
+  static GridProvider<GridType, DD::SubdomainGrid<GridType>>
+  create(const std::string& type = available()[0], const Common::Configuration config = Common::Configuration())
+  {
+    if (call_compare<CubeType>(type))
+      return call_create<CubeType>(config);
+    else if (available().empty())
+      DUNE_THROW(Common::Exceptions::wrong_input_given,
+                 "There is no grid provider available for " << Common::Typename<GridType>::value() << "!");
+    else
+      DUNE_THROW(Common::Exceptions::wrong_input_given,
+                 "Requested type '" << type << "' is not one of those avaible for "
+                                    << Common::Typename<GridType>::value()
+                                    << ":\n"
+                                    << available_as_str());
+  } // ... create(...)
+}; // class DdSubdomainGridProviderFactory
+
+
 } // namespace Grid
 } // namespace XT
 } // namespace Dune
