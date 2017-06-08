c44e928f365b4ff72a99eea2d9624dfb0347b59a
Tobias Leibner <tobias.leibner@googlemail.com>
2017-06-08 09:27:14 +0200

    [affine] adapt to new interfaces

--- dune/xt/functions/affine.hh
+++ dune/xt/functions/affine.hh
@@ -29,41 +29,25 @@
 namespace Dune {
 namespace XT {
 namespace Functions {
+namespace internal {
 
-/**
- * \brief Simple affine function of the form f(x) = A*x + b. For dimRangeCols > 1, there has to be a matrix A_i for
- * every column.
- */
-template <class EntityImp,
-          class DomainFieldImp,
-          size_t domainDim,
-          class RangeFieldImp,
-          size_t rangeDim,
-          size_t rangeDimCols = 1>
-class AffineFunction
-    : public GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols>
+
+template <class DomainFieldImp, size_t domainDim, class RangeFieldImp, size_t rangeDim, size_t rangeDimCols = 1>
+class AffineFunctionBase
 {
-  typedef GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols> BaseType;
-  typedef AffineFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols> ThisType;
+  typedef AffineFunctionBase<DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols> ThisType;
 
 public:
-  using typename BaseType::DomainType;
-  using typename BaseType::RangeFieldType;
-  using typename BaseType::RangeType;
-  using typename BaseType::JacobianRangeType;
-  using BaseType::dimDomain;
-  using BaseType::dimRange;
-  using BaseType::dimRangeCols;
-  typedef typename LA::CommonSparseMatrix<RangeFieldType> MatrixType;
-  typedef typename Dune::FieldMatrix<RangeFieldType, dimRange, dimDomain> FieldMatrixType;
-
-  using typename BaseType::LocalfunctionType;
-
-  static const bool available = true;
+  typedef FieldVector<DomainFieldImp, domainDim> DomainType;
+  typedef typename XT::Functions::RangeTypeSelector<RangeFieldImp, rangeDim, rangeDimCols>::type RangeType;
+  typedef typename XT::Functions::JacobianRangeTypeSelector<domainDim, RangeFieldImp, rangeDim, rangeDimCols>::type
+      JacobianRangeType;
+  typedef typename LA::CommonSparseMatrix<RangeFieldImp> MatrixType;
+  typedef FieldMatrix<RangeFieldImp, rangeDim, domainDim> FieldMatrixType;
 
   static std::string static_id()
   {
-    return BaseType::static_id() + ".affine";
+    return "affine";
   }
 
   static Common::Configuration default_config(const std::string sub_name = "")
@@ -80,6 +64,149 @@ public:
       return tmp;
     }
   } // ... default_config(...)
+
+  // constructor
+  explicit AffineFunctionBase(const std::vector<MatrixType> A,
+                              const RangeType b = RangeType(0),
+                              const std::string name_in = static_id())
+    : A_(A)
+    , b_(b)
+    , name_(name_in)
+    , b_zero_(Common::FloatCmp::eq(b_, RangeType(0)))
+  {
+    assert(A.size() >= rangeDimCols);
+  }
+
+  explicit AffineFunctionBase(const FieldVector<FieldMatrixType, rangeDimCols> A,
+                              const RangeType b = RangeType(0),
+                              const bool prune = true,
+                              const std::string name_in = static_id())
+    : A_(A.size())
+    , b_(b)
+    , name_(name_in)
+    , b_zero_(Common::FloatCmp::eq(b_, RangeType(0)))
+  {
+    for (size_t cc = 0; cc < rangeDimCols; ++cc)
+      A_[cc] = MatrixType(A[cc], prune);
+  }
+
+  // constructor for dimRangeCols = 1.
+  explicit AffineFunctionBase(const MatrixType A,
+                              const RangeType b = RangeType(0),
+                              const std::string name_in = static_id())
+    : AffineFunctionBase(std::vector<MatrixType>(1, A), b, name_in)
+  {
+    static_assert(rangeDimCols == 1, "Use constructor above for dimRangeCols > 1");
+  }
+
+  AffineFunctionBase(const ThisType& other) = default;
+
+// if HAVE_DUNE_FEM is true, GlobalFunctionInterface is derived from Fem::Function which has a deleted copy assignment
+// operator
+#if HAVE_DUNE_FEM
+  ThisType& operator=(const ThisType& other)
+  {
+    A_ = other.A_;
+    b_ = other.b_;
+    b_zero_ = other.b_zero_;
+    name_ = other.name_;
+    return *this;
+  }
+#endif
+
+protected:
+  template <bool is_not_tensor = (rangeDimCols == 1), class anything = void>
+  struct helper
+  {
+    static void evaluate(
+        const std::vector<MatrixType>& A, const RangeType& b, const bool b_zero, const DomainType& x, RangeType& ret)
+    {
+      A[0].mv(x, ret);
+      if (!b_zero)
+        ret += b;
+    }
+
+    static void jacobian(const std::vector<MatrixType>& A, JacobianRangeType& ret)
+    {
+      ret = A[0].operator FieldMatrixType();
+    }
+  }; // struct helper<true, ...>
+
+  template <class anything>
+  struct helper<false, anything>
+  {
+    static void evaluate(
+        const std::vector<MatrixType>& A, const RangeType& b, const bool b_zero, const DomainType& x, RangeType& ret)
+    {
+      for (size_t cc = 0; cc < rangeDimCols; ++cc) {
+        Dune::FieldVector<RangeFieldImp, rangeDim> tmp_col;
+        A[cc].mv(x, tmp_col);
+        for (size_t rr = 0; rr < rangeDim; ++rr)
+          ret[rr][cc] = tmp_col[rr];
+      }
+      if (!b_zero)
+        ret += b;
+    }
+
+    static void jacobian(const std::vector<MatrixType>& A, JacobianRangeType& ret)
+    {
+      for (size_t cc = 0; cc < rangeDimCols; ++cc)
+        ret[cc] = A[cc].operator FieldMatrixType();
+    }
+  }; // struct helper<false, ...>
+
+protected:
+  std::vector<MatrixType> A_;
+  RangeType b_;
+  bool b_zero_;
+  std::string name_;
+}; // class AffineFunctionBase<...>
+
+
+} // namespace internal
+
+
+/**
+ * \brief Simple affine function of the form f(x) = A*x + b. For dimRangeCols > 1, there has to be a matrix A_i for
+ * every column.
+ */
+template <class EntityImp,
+          class DomainFieldImp,
+          size_t domainDim,
+          class RangeFieldImp,
+          size_t rangeDim,
+          size_t rangeDimCols = 1>
+class AffineFunction
+    : public GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols>,
+      public internal::AffineFunctionBase<DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols>
+{
+  typedef GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols>
+      InterfaceType;
+  typedef internal::AffineFunctionBase<DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols> BaseType;
+  typedef AffineFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols> ThisType;
+
+public:
+  using typename InterfaceType::DomainType;
+  using typename InterfaceType::RangeFieldType;
+  using typename InterfaceType::RangeType;
+  using typename InterfaceType::JacobianRangeType;
+  using InterfaceType::dimDomain;
+  using InterfaceType::dimRange;
+  using InterfaceType::dimRangeCols;
+  using typename BaseType::MatrixType;
+  using typename BaseType::FieldMatrixType;
+
+  using BaseType::default_config;
+
+  static std::string static_id()
+  {
+    return BaseType::static_id() + ".affine";
+  }
+
+  virtual size_t order() const override
+  {
+    return 1;
+  }
 
   static std::unique_ptr<ThisType> create(const Common::Configuration config = default_config(),
                                           const std::string sub_name = static_id())
@@ -103,130 +230,179 @@ public:
   explicit AffineFunction(const std::vector<MatrixType> A,
                           const RangeType b = RangeType(0),
                           const std::string name_in = static_id())
-    : A_(A)
-    , b_(b)
-    , name_(name_in)
-    , b_zero_(Common::FloatCmp::eq(b_, RangeType(0)))
+    : BaseType(A, b, name_in)
   {
-    assert(A.size() >= dimRangeCols);
   }
 
   explicit AffineFunction(const Dune::FieldVector<FieldMatrixType, dimRangeCols> A,
                           const RangeType b = RangeType(0),
                           const bool prune = true,
                           const std::string name_in = static_id())
-    : A_(A.size())
-    , b_(b)
-    , name_(name_in)
-    , b_zero_(Common::FloatCmp::eq(b_, RangeType(0)))
+    : BaseType(A, b, prune, name_in)
   {
-    for (size_t cc = 0; cc < dimRangeCols; ++cc)
-      A_[cc] = MatrixType(A[cc], prune);
   }
 
   // constructor for dimRangeCols = 1.
   explicit AffineFunction(const MatrixType A, const RangeType b = RangeType(0), const std::string name_in = static_id())
-    : AffineFunction(std::vector<MatrixType>(1, A), b, name_in)
+    : BaseType(A, b, name_in)
   {
-    static_assert(dimRangeCols == 1, "Use constructor above for dimRangeCols > 1");
   }
 
-  AffineFunction(const ThisType& other) = default;
-
-// if HAVE_DUNE_FEM is true, GlobalFunctionInterface is derived from Fem::Function which has a deleted copy assignment
-// operator
-#if HAVE_DUNE_FEM
-  ThisType& operator=(const ThisType& other)
-  {
-    A_ = other.A_;
-    b_ = other.b_;
-    b_zero_ = other.b_zero_;
-    name_ = other.name_;
-    return *this;
-  }
-#endif
-
-  virtual std::string type() const override final
-  {
-    return BaseType::static_id() + ".affine";
-  }
-
-  virtual size_t order() const override final
-  {
-    return 1;
-  }
-
-private:
-  template <bool is_not_tensor = (rangeDimCols == 1), bool anything = true>
-  struct helper
-  {
-    static void evaluate(
-        const std::vector<MatrixType>& A, const RangeType& b, const bool b_zero, const DomainType& x, RangeType& ret)
-    {
-      A[0].mv(x, ret);
-      if (!b_zero)
-        ret += b;
-    }
-
-    static void jacobian(const std::vector<MatrixType>& A, JacobianRangeType& ret)
-    {
-      ret = A[0].operator FieldMatrixType();
-    }
-  }; // struct helper<true, ...>
-
-  template <bool anything>
-  struct helper<false, anything>
-  {
-    static void evaluate(
-        const std::vector<MatrixType>& A, const RangeType& b, const bool b_zero, const DomainType& x, RangeType& ret)
-    {
-      for (size_t cc = 0; cc < rangeDimCols; ++cc) {
-        Dune::FieldVector<RangeFieldType, dimRange> tmp_col;
-        A[cc].mv(x, tmp_col);
-        for (size_t rr = 0; rr < dimRange; ++rr)
-          ret[rr][cc] = tmp_col[rr];
-      }
-      if (!b_zero)
-        ret += b;
-    }
-
-    static void jacobian(const std::vector<MatrixType>& A, JacobianRangeType& ret)
-    {
-      for (size_t cc = 0; cc < rangeDimCols; ++cc)
-        ret[cc] = A[cc].operator FieldMatrixType();
-    }
-  }; // struct helper<false, ...>
-
-public:
-  using BaseType::evaluate;
+  using InterfaceType::evaluate;
 
   virtual void evaluate(const DomainType& x,
                         RangeType& ret,
                         const Common::Parameter& /*mu*/ = Common::Parameter()) const override final
   {
-    helper<>::evaluate(A_, b_, b_zero_, x, ret);
+    BaseType::template helper<>::evaluate(A_, b_, b_zero_, x, ret);
   }
 
-  using BaseType::jacobian;
+  using InterfaceType::jacobian;
 
-  virtual void jacobian(const DomainType& x,
+  virtual void jacobian(const DomainType& /*x*/,
                         JacobianRangeType& ret,
                         const Common::Parameter& /*mu*/ = Common::Parameter()) const override final
   {
-    helper<>::jacobian(A_, ret);
-  }
-
-  virtual std::string name() const override final
-  {
-    return name_;
+    BaseType::template helper<>::jacobian(A_, ret);
   }
 
 private:
-  std::vector<MatrixType> A_;
-  RangeType b_;
-  bool b_zero_;
-  std::string name_;
-};
+  using BaseType::A_;
+  using BaseType::b_;
+  using BaseType::b_zero_;
+}; // class AffineFunction<...>
+
+
+/**
+ * \brief Simple affine function of the form f(x, u) = A*u + b. For dimRangeCols > 1, there has to be a matrix A_i for
+ * every column.
+ */
+template <class EntityImp,
+          class DomainFieldImp,
+          size_t domainDim,
+          class U_,
+          class RangeFieldImp,
+          size_t rangeDim,
+          size_t rangeDimCols = 1>
+class AffineFluxFunction
+    : public GlobalFluxFunctionInterface<EntityImp,
+                                         DomainFieldImp,
+                                         domainDim,
+                                         U_,
+                                         0,
+                                         RangeFieldImp,
+                                         rangeDim,
+                                         rangeDimCols>,
+      public internal::
+          AffineFunctionBase<typename U_::RangeFieldType, U_::dimRange, RangeFieldImp, rangeDim, rangeDimCols>
+{
+  typedef GlobalFluxFunctionInterface<EntityImp,
+                                      DomainFieldImp,
+                                      domainDim,
+                                      U_,
+                                      0,
+                                      RangeFieldImp,
+                                      rangeDim,
+                                      rangeDimCols>
+      InterfaceType;
+  typedef internal::AffineFunctionBase<typename U_::RangeFieldType, U_::dimRange, RangeFieldImp, rangeDim, rangeDimCols>
+      BaseType;
+  typedef AffineFluxFunction<EntityImp, DomainFieldImp, domainDim, U_, RangeFieldImp, rangeDim, rangeDimCols> ThisType;
+
+public:
+  typedef typename InterfaceType::DomainType DomainType;
+  typedef typename InterfaceType::RangeFieldType RangeFieldType;
+  typedef typename InterfaceType::RangeType RangeType;
+  typedef typename InterfaceType::JacobianWrtURangeType JacobianWrtURangeType;
+  using InterfaceType::dimDomain;
+  using InterfaceType::dimRange;
+  using InterfaceType::dimRangeCols;
+  typedef typename InterfaceType::StateType StateType;
+  typedef typename InterfaceType::StateRangeType StateRangeType;
+  typedef typename InterfaceType::LocalfunctionType LocalfunctionType;
+  typedef typename BaseType::MatrixType MatrixType;
+  typedef typename BaseType::FieldMatrixType FieldMatrixType;
+
+  using BaseType::default_config;
+
+  static std::string static_id()
+  {
+    return BaseType::static_id() + ".affineflux";
+  }
+
+  virtual size_t order() const override
+  {
+    return 1;
+  }
+
+  static std::unique_ptr<ThisType> create(const Common::Configuration config = default_config(),
+                                          const std::string sub_name = static_id())
+  {
+    // get correct config
+    const Common::Configuration cfg = config.has_sub(sub_name) ? config.sub(sub_name) : config;
+    const Common::Configuration default_cfg = default_config();
+    std::vector<MatrixType> A_vector(dimRangeCols);
+    for (size_t cc = 0; cc < dimRangeCols; ++cc) {
+      if (cc == 0 && cfg.has_key("A")) {
+        A_vector[0] = cfg.get<MatrixType>("A", dimRange, StateType::dimRange);
+      } else {
+        A_vector[cc] = cfg.get<MatrixType>("A." + Common::to_string(cc), dimRange, StateType::dimRange);
+      }
+    }
+    return Common::make_unique<ThisType>(
+        A_vector, cfg.get<RangeType>("b"), cfg.get("name", default_cfg.get<std::string>("name")));
+  } // ... create(...)
+
+  // constructor
+  explicit AffineFluxFunction(const std::vector<MatrixType> A,
+                              const RangeType b = RangeType(0),
+                              const std::string name_in = static_id())
+    : BaseType(A, b, name_in)
+  {
+  }
+
+  explicit AffineFluxFunction(const Dune::FieldVector<FieldMatrixType, dimRangeCols> A,
+                              const RangeType b = RangeType(0),
+                              const bool prune = true,
+                              const std::string name_in = static_id())
+    : BaseType(A, b, prune, name_in)
+  {
+  }
+
+  // constructor for dimRangeCols = 1.
+  explicit AffineFluxFunction(const MatrixType A,
+                              const RangeType b = RangeType(0),
+                              const std::string name_in = static_id())
+    : BaseType(A, b, name_in)
+  {
+  }
+
+  using InterfaceType::evaluate;
+
+  virtual void evaluate(const DomainType& /*x*/,
+                        const StateRangeType& u,
+                        RangeType& ret,
+                        const Common::Parameter& /*mu*/ = Common::Parameter()) const override final
+  {
+    BaseType::template helper<>::evaluate(A_, b_, b_zero_, u, ret);
+  }
+
+  using InterfaceType::jacobian_wrt_u;
+
+  virtual void jacobian_wrt_u(const DomainType& /*x*/,
+                              const StateRangeType& /*u*/,
+                              JacobianWrtURangeType& ret,
+                              const Common::Parameter& /*mu*/ = Common::Parameter()) const override final
+  {
+    BaseType::template helper<>::jacobian(A_, ret);
+  }
+
+private:
+  using BaseType::A_;
+  using BaseType::b_;
+  using BaseType::b_zero_;
+}; // class AffineFluxFunction<...>
+
 
 } // namespace Functions
 } // namespace XT
