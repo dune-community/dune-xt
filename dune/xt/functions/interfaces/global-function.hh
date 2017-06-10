f21b7e1679186e1620bfbd910ae66968c2b49007
Felix Schindler <felix.schindler@wwu.de>
2017-06-10 09:50:29 +0200

    [interfaces.global] do not derive from fem/pdelab functions
    This lead to numerous problems for newer gccs due to ambiguous types.

--- dune/xt/functions/interfaces/global-function.hh
+++ dune/xt/functions/interfaces/global-function.hh
@@ -22,7 +22,7 @@ class TransferredGlobalFunction;
 
 
 /**
- * base class for global matrix-valued valued functions that provides automatic local functions via
+ * base class for global scalar-, vector- or matrix-valued valued functions that provides automatic local functions via
  * LocalizableFunctionInterface
  */
 template <class EntityImp,
@@ -58,7 +58,7 @@ public:
                         JacobianRangeType& /*ret*/,
                         const Common::Parameter& /*mu*/ = Common::Parameter()) const
   {
-    DUNE_THROW(NotImplemented, "This does not make sense yet for matrix-valued functions!");
+    DUNE_THROW(NotImplemented, "You have to imlement it if you intend to use it!");
   }
 
   virtual RangeType evaluate(const DomainType& xx, const Common::Parameter& mu = Common::Parameter()) const
@@ -144,155 +144,6 @@ public:
     return typename Transfer<OtherEntityImp>::Type(*this);
   }
 }; // class GlobalFunctionInterface
-
-
-/**
- * base class for global valued functions that provides automatic local functions via LocalizableFunctionInterface
- */
-template <class EntityImp, class DomainFieldImp, int domainDim, class RangeFieldImp, int rangeDim>
-class GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>
-    : public LocalizableFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>
-#if HAVE_DUNE_FEM
-      ,
-      public Dune::Fem::
-          Function<Dune::Fem::FunctionSpace<DomainFieldImp, RangeFieldImp, domainDim, rangeDim>,
-                   GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>>
-#endif // HAVE_DUNE_FEM
-#if HAVE_DUNE_PDELAB
-      ,
-      public TypeTree::LeafNode,
-      public PDELab::
-          FunctionInterface<PDELab::FunctionTraits<DomainFieldImp,
-                                                   domainDim,
-                                                   FieldVector<DomainFieldImp, domainDim>,
-                                                   RangeFieldImp,
-                                                   rangeDim,
-                                                   FieldVector<RangeFieldImp, rangeDim>>,
-                            GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>>
-#endif
-{
-  typedef LocalizableFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1> BaseType;
-  typedef GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1> ThisType;
-
-public:
-  using typename BaseType::DomainFieldType;
-  using BaseType::dimDomain;
-  using typename BaseType::DomainType;
-  using typename BaseType::RangeFieldType;
-  using BaseType::dimRange;
-  using BaseType::dimRangeCols;
-  using typename BaseType::RangeType;
-  using typename BaseType::LocalfunctionType;
-  using typename BaseType::EntityType;
-#if HAVE_DUNE_FEM
-  typedef typename Dune::Fem::
-      Function<Dune::Fem::FunctionSpace<DomainFieldImp, RangeFieldImp, domainDim, rangeDim>,
-               GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, 1>>::
-          JacobianRangeType JacobianRangeType;
-#else
-  typedef typename BaseType::JacobianRangeType JacobianRangeType;
-#endif
-
-  static_assert(std::is_same<typename LocalfunctionType::RangeType, RangeType>::value, "RangeType mismatch");
-  static_assert(std::is_same<typename LocalfunctionType::DomainType, DomainType>::value, "DomainType mismatch");
-  static_assert(std::is_same<Dune::FieldVector<DomainFieldImp, domainDim>, DomainType>::value, "DomainType mismatch");
-
-  virtual ~GlobalFunctionInterface()
-  {
-  }
-
-  virtual size_t order() const = 0;
-
-  virtual void evaluate(const DomainType& /*x*/,
-                        RangeType& /*ret*/,
-                        const Common::Parameter& /*mu*/ = Common::Parameter()) const = 0;
-
-  virtual void jacobian(const DomainType& /*x*/,
-                        JacobianRangeType& /*ret*/,
-                        const Common::Parameter& /*mu*/ = Common::Parameter()) const
-  {
-    DUNE_THROW(NotImplemented, "You have to imlement it if you intend to use it!");
-  }
-
-  virtual RangeType evaluate(const DomainType& xx, const Common::Parameter& mu = Common::Parameter()) const
-  {
-    RangeType ret;
-    evaluate(xx, ret, mu);
-    return ret;
-  }
-
-  virtual JacobianRangeType jacobian(const DomainType& xx, const Common::Parameter& mu = Common::Parameter()) const
-  {
-    JacobianRangeType ret;
-    jacobian(xx, ret, mu);
-    return ret;
-  }
-
-  virtual std::unique_ptr<LocalfunctionType> local_function(const EntityImp& entity) const override final
-  {
-    return Common::make_unique<Localfunction>(entity, *this);
-  }
-
-  virtual std::string type() const override
-  {
-    return "globalfunction";
-  }
-
-  virtual std::string name() const override
-  {
-    return "globalfunction";
-  }
-
-private:
-  class Localfunction : public LocalfunctionType
-  {
-  public:
-    Localfunction(const EntityImp& entity_in, const ThisType& global_function)
-      : LocalfunctionType(entity_in)
-      , geometry_(entity_in.geometry())
-      , global_function_(global_function)
-    {
-    }
-
-    virtual void evaluate(const DomainType& xx,
-                          RangeType& ret,
-                          const Common::Parameter& mu = Common::Parameter()) const override final
-    {
-      const auto xx_global = geometry_.global(xx);
-      global_function_.evaluate(xx_global, ret, mu);
-    }
-
-    virtual void jacobian(const DomainType& xx,
-                          JacobianRangeType& ret,
-                          const Common::Parameter& mu = Common::Parameter()) const override final
-    {
-      const auto xx_global = geometry_.global(xx);
-      global_function_.jacobian(xx_global, ret, mu);
-    }
-
-    virtual size_t order() const override final
-    {
-      return global_function_.order();
-    }
-
-  private:
-    const typename EntityImp::Geometry geometry_;
-    const ThisType& global_function_;
-  }; // class Localfunction
-
-public:
-  template <class OtherEntityImp>
-  struct Transfer
-  {
-    typedef TransferredGlobalFunction<OtherEntityImp, ThisType> Type;
-  };
-
-  template <class OtherEntityImp>
-  typename Transfer<OtherEntityImp>::Type transfer() const
-  {
-    return typename Transfer<OtherEntityImp>::Type(*this);
-  }
-}; // class GlobalFunctionInterface< ..., 1 >
 
 
 template <class OtherEntityImp, class GlobalFunctionImp>
