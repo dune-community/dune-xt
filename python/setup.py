4d2746f8c87972be4d6d9f60e732b0ef4c5becc8
Rene Milk <rene.milk@wwu.de>
2017-08-23 09:57:20 +0200

    [py] setup dune python module infra

---
+++ python/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# This file is part of the dune-xt-common project:
+#   https://github.com/dune-community/dune-xt-common
+# Copyright 2009-2017 dune-xt-common developers and contributors. All rights reserved.
+# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+#          with "runtime exception" (http://www.dune-project.org/license.html)
+# Authors:
+#   Felix Schindler (2017)
+#   Rene Milk       (2016 - 2017)
+
+import sys
+from setuptools import setup
+
+setup(name='dune.xt.functions',
+      version='2.4',
+      namespace_packages=['dune'],
+      description='Python for Dune-Xt',
+      author='The dune-xt devs',
+      author_email='dune-xt-dev@listserv.uni-muenster.de',
+      url='https://github.com/dune-community/dune-xt-functions',
+      packages=['dune.xt'],
+      install_requires=[],
+      scripts=[])
