25b22cdfe93edbe3a1990ac36e937ddac3813387
Felix Schindler <felix.schindler@wwu.de>
2017-08-31 08:36:03 +0200

    [WIP|type_traits] disable most traits
    These have to be modernized once the functions are all up to date.

--- dune/xt/functions/type_traits.hh
+++ dune/xt/functions/type_traits.hh
@@ -92,143 +92,143 @@ struct DerivativeRangeTypeSelector<d, R, r, 1>
 };
 
 
-// forwards
-template <class E, class D, size_t d, class R, size_t r, size_t rC>
-class LocalfunctionSetInterface;
+//// forwards
+// template <class E, class D, size_t d, class R, size_t r, size_t rC>
+// class LocalfunctionSetInterface;
 
-template <class E, class D, size_t d, class R, size_t r, size_t rC>
-class LocalfunctionInterface;
+// template <class E, class D, size_t d, class R, size_t r, size_t rC>
+// class LocalfunctionInterface;
 
-template <class E, class D, size_t d, class R, size_t r, size_t rC>
-class LocalizableFunctionInterface;
+// template <class E, class D, size_t d, class R, size_t r, size_t rC>
+// class LocalizableFunctionInterface;
 
-template <class E, class D, size_t d, class U_, size_t s_, class R, size_t r, size_t rC>
-class LocalizableFluxFunctionInterface;
-
-
-namespace internal {
+// template <class E, class D, size_t d, class U_, size_t s_, class R, size_t r, size_t rC>
+// class LocalizableFluxFunctionInterface;
 
 
-template <class Tt>
-struct is_localfunction_set_helper
-{
-  DXTC_has_typedef_initialize_once(EntityType);
-  DXTC_has_typedef_initialize_once(DomainFieldType);
-  DXTC_has_typedef_initialize_once(RangeFieldType);
-  DXTC_has_static_member_initialize_once(dimDomain);
-  DXTC_has_static_member_initialize_once(dimRange);
-  DXTC_has_static_member_initialize_once(dimRangeCols);
-  static const bool is_candidate =
-      DXTC_has_typedef(EntityType)<Tt>::value && DXTC_has_typedef(DomainFieldType)<Tt>::value
-      && DXTC_has_typedef(RangeFieldType)<Tt>::value && DXTC_has_static_member(dimDomain)<Tt>::value
-      && DXTC_has_static_member(dimRange)<Tt>::value && DXTC_has_static_member(dimRangeCols)<Tt>::value;
-}; // struct is_localfunction_set_helper
-
-template <class Tt>
-struct is_localizable_flux_function_helper
-{
-  DXTC_has_typedef_initialize_once(EntityType);
-  DXTC_has_typedef_initialize_once(DomainFieldType);
-  DXTC_has_typedef_initialize_once(StateType);
-  DXTC_has_typedef_initialize_once(RangeFieldType);
-  DXTC_has_static_member_initialize_once(dimDomain);
-  DXTC_has_static_member_initialize_once(dimRange);
-  DXTC_has_static_member_initialize_once(dimRangeCols);
-  DXTC_has_static_member_initialize_once(stateDerivativeOrder);
-  static const bool is_candidate =
-      DXTC_has_typedef(EntityType)<Tt>::value && DXTC_has_typedef(DomainFieldType)<Tt>::value
-      && DXTC_has_typedef(StateType)<Tt>::value && DXTC_has_typedef(RangeFieldType)<Tt>::value
-      && DXTC_has_static_member(dimDomain)<Tt>::value && DXTC_has_static_member(dimRange)<Tt>::value
-      && DXTC_has_static_member(dimRangeCols)<Tt>::value && DXTC_has_static_member(stateDerivativeOrder)<Tt>::value;
-}; // struct is_localfunction_set_helper
+// namespace internal {
 
 
-} // namespace internal
+// template <class Tt>
+// struct is_localfunction_set_helper
+//{
+//  DXTC_has_typedef_initialize_once(EntityType);
+//  DXTC_has_typedef_initialize_once(DomainFieldType);
+//  DXTC_has_typedef_initialize_once(RangeFieldType);
+//  DXTC_has_static_member_initialize_once(dimDomain);
+//  DXTC_has_static_member_initialize_once(dimRange);
+//  DXTC_has_static_member_initialize_once(dimRangeCols);
+//  static const bool is_candidate =
+//      DXTC_has_typedef(EntityType)<Tt>::value && DXTC_has_typedef(DomainFieldType)<Tt>::value
+//      && DXTC_has_typedef(RangeFieldType)<Tt>::value && DXTC_has_static_member(dimDomain)<Tt>::value
+//      && DXTC_has_static_member(dimRange)<Tt>::value && DXTC_has_static_member(dimRangeCols)<Tt>::value;
+//}; // struct is_localfunction_set_helper
+
+// template <class Tt>
+// struct is_localizable_flux_function_helper
+//{
+//  DXTC_has_typedef_initialize_once(EntityType);
+//  DXTC_has_typedef_initialize_once(DomainFieldType);
+//  DXTC_has_typedef_initialize_once(StateType);
+//  DXTC_has_typedef_initialize_once(RangeFieldType);
+//  DXTC_has_static_member_initialize_once(dimDomain);
+//  DXTC_has_static_member_initialize_once(dimRange);
+//  DXTC_has_static_member_initialize_once(dimRangeCols);
+//  DXTC_has_static_member_initialize_once(stateDerivativeOrder);
+//  static const bool is_candidate =
+//      DXTC_has_typedef(EntityType)<Tt>::value && DXTC_has_typedef(DomainFieldType)<Tt>::value
+//      && DXTC_has_typedef(StateType)<Tt>::value && DXTC_has_typedef(RangeFieldType)<Tt>::value
+//      && DXTC_has_static_member(dimDomain)<Tt>::value && DXTC_has_static_member(dimRange)<Tt>::value
+//      && DXTC_has_static_member(dimRangeCols)<Tt>::value && DXTC_has_static_member(stateDerivativeOrder)<Tt>::value;
+//}; // struct is_localfunction_set_helper
 
 
-template <class T, bool candidate = internal::is_localfunction_set_helper<T>::is_candidate>
-struct is_localfunction_set : public std::is_base_of<LocalfunctionSetInterface<typename T::EntityType,
-                                                                               typename T::DomainFieldType,
-                                                                               T::dimDomain,
-                                                                               typename T::RangeFieldType,
-                                                                               T::dimRange,
-                                                                               T::dimRangeCols>,
-                                                     T>
-{
-};
-
-template <class T>
-struct is_localfunction_set<T, false> : public std::false_type
-{
-};
+//} // namespace internal
 
 
-template <class T, bool candidate = internal::is_localfunction_set_helper<T>::is_candidate>
-struct is_localfunction : public std::is_base_of<LocalfunctionInterface<typename T::EntityType,
-                                                                        typename T::DomainFieldType,
-                                                                        T::dimDomain,
-                                                                        typename T::RangeFieldType,
-                                                                        T::dimRange,
-                                                                        T::dimRangeCols>,
-                                                 T>
-{
-};
+// template <class T, bool candidate = internal::is_localfunction_set_helper<T>::is_candidate>
+// struct is_localfunction_set : public std::is_base_of<LocalfunctionSetInterface<typename T::EntityType,
+//                                                                               typename T::DomainFieldType,
+//                                                                               T::dimDomain,
+//                                                                               typename T::RangeFieldType,
+//                                                                               T::dimRange,
+//                                                                               T::dimRangeCols>,
+//                                                     T>
+//{
+//};
 
-template <class T>
-struct is_localfunction<T, false> : public std::false_type
-{
-};
-
-
-template <class T, bool candidate = internal::is_localfunction_set_helper<T>::is_candidate>
-struct is_localizable_function : public std::is_base_of<LocalizableFunctionInterface<typename T::EntityType,
-                                                                                     typename T::DomainFieldType,
-                                                                                     T::dimDomain,
-                                                                                     typename T::RangeFieldType,
-                                                                                     T::dimRange,
-                                                                                     T::dimRangeCols>,
-                                                        T>
-{
-};
-
-template <class T>
-struct is_localizable_function<T, false> : public std::false_type
-{
-};
+// template <class T>
+// struct is_localfunction_set<T, false> : public std::false_type
+//{
+//};
 
 
-template <class T, bool candidate = internal::is_localizable_flux_function_helper<T>::is_candidate>
-struct is_localizable_flux_function
-    : public std::is_base_of<LocalizableFluxFunctionInterface<typename T::EntityType,
-                                                              typename T::DomainFieldType,
-                                                              T::dimDomain,
-                                                              typename T::StateType,
-                                                              T::stateDerivativeOrder,
-                                                              typename T::RangeFieldType,
-                                                              T::dimRange,
-                                                              T::dimRangeCols>,
-                             T>
-{
-};
+// template <class T, bool candidate = internal::is_localfunction_set_helper<T>::is_candidate>
+// struct is_localfunction : public std::is_base_of<LocalfunctionInterface<typename T::EntityType,
+//                                                                        typename T::DomainFieldType,
+//                                                                        T::dimDomain,
+//                                                                        typename T::RangeFieldType,
+//                                                                        T::dimRange,
+//                                                                        T::dimRangeCols>,
+//                                                 T>
+//{
+//};
 
-template <class T>
-struct is_localizable_flux_function<T, false> : public std::false_type
-{
-};
+// template <class T>
+// struct is_localfunction<T, false> : public std::false_type
+//{
+//};
 
 
-//! Utility to generate a complete Function Type from an existing one and a template
-template <class FunctionImp, template <class, class, size_t, class, size_t, size_t> class OutTemplate>
-struct FunctionTypeGenerator
-{
-  typedef OutTemplate<typename FunctionImp::EntityType,
-                      typename FunctionImp::DomainFieldType,
-                      FunctionImp::dimDomain,
-                      typename FunctionImp::RangeFieldType,
-                      FunctionImp::dimRange,
-                      FunctionImp::dimRangeCols>
-      type;
-};
+// template <class T, bool candidate = internal::is_localfunction_set_helper<T>::is_candidate>
+// struct is_localizable_function : public std::is_base_of<LocalizableFunctionInterface<typename T::EntityType,
+//                                                                                     typename T::DomainFieldType,
+//                                                                                     T::dimDomain,
+//                                                                                     typename T::RangeFieldType,
+//                                                                                     T::dimRange,
+//                                                                                     T::dimRangeCols>,
+//                                                        T>
+//{
+//};
+
+// template <class T>
+// struct is_localizable_function<T, false> : public std::false_type
+//{
+//};
+
+
+// template <class T, bool candidate = internal::is_localizable_flux_function_helper<T>::is_candidate>
+// struct is_localizable_flux_function
+//    : public std::is_base_of<LocalizableFluxFunctionInterface<typename T::EntityType,
+//                                                              typename T::DomainFieldType,
+//                                                              T::dimDomain,
+//                                                              typename T::StateType,
+//                                                              T::stateDerivativeOrder,
+//                                                              typename T::RangeFieldType,
+//                                                              T::dimRange,
+//                                                              T::dimRangeCols>,
+//                             T>
+//{
+//};
+
+// template <class T>
+// struct is_localizable_flux_function<T, false> : public std::false_type
+//{
+//};
+
+
+////! Utility to generate a complete Function Type from an existing one and a template
+// template <class FunctionImp, template <class, class, size_t, class, size_t, size_t> class OutTemplate>
+// struct FunctionTypeGenerator
+//{
+//  typedef OutTemplate<typename FunctionImp::EntityType,
+//                      typename FunctionImp::DomainFieldType,
+//                      FunctionImp::dimDomain,
+//                      typename FunctionImp::RangeFieldType,
+//                      FunctionImp::dimRange,
+//                      FunctionImp::dimRangeCols>
+//      type;
+//};
 
 
 } // namespace Functions
