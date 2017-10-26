01b920a978f41c96561a19f79301ad076a4c7a65
Felix Schindler <felix.schindler@wwu.de>
2017-10-26 11:56:15 +0200

    [container.common] fix library instantiation of CommonSparseVector

--- dune/xt/la/container/common.cc
+++ dune/xt/la/container/common.cc
@@ -15,6 +15,7 @@
 
 template class Dune::XT::LA::CommonDenseVector<double>;
 template class Dune::XT::LA::CommonDenseMatrix<double>;
+template class Dune::XT::LA::CommonSparseVector<double>;
 template class Dune::XT::LA::CommonSparseMatrix<double>;
 // template void Dune::XT::LA::CommonSparseMatrix<double>::mv(const Dune::XT::LA::CommonDenseVector<double>&,
 //                                                           Dune::XT::LA::CommonDenseVector<double>) const;
