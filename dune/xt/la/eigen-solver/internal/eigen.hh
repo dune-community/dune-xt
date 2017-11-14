3f53ff54f2f11c562cfefd891edd50cf40fa7218
Felix Schindler <felix.schindler@wwu.de>
2017-11-14 12:52:11 +0100

    [eigen-solver...eigen] refactor internal calls to eigen

--- dune/xt/la/eigen-solver/internal/eigen.hh
+++ dune/xt/la/eigen-solver/internal/eigen.hh
@@ -17,7 +17,7 @@
 
 #include <dune/common/typetraits.hh>
 
-#include <dune/xt/la/container/eigen.hh>
+#include <dune/xt/common/type_traits.hh>
 #include <dune/xt/la/exceptions.hh>
 
 
@@ -31,15 +31,34 @@ namespace internal {
 
 
 template <class S>
-std::vector<std::complex<S>>
+void compute_all_eigenvalues_and_vectors_using_eigen(
+    const ::Eigen::Matrix<S, ::Eigen::Dynamic, ::Eigen::Dynamic>& matrix,
+    std::vector<XT::Common::complex_t<S>>& eigenvalues,
+    ::Eigen::Matrix<XT::Common::complex_t<S>, ::Eigen::Dynamic, ::Eigen::Dynamic>& eigenvectors)
+{
+  ::Eigen::EigenSolver<::Eigen::Matrix<S, ::Eigen::Dynamic, ::Eigen::Dynamic>> eigen_solver(
+      matrix, /*computeEigenvectors=*/true);
+  if (eigen_solver.info() != ::Eigen::Success)
+    DUNE_THROW(Exceptions::eigen_solver_failed, "The eigen backend reported '" << eigen_solver.info() << "'!");
+  const auto& evs = eigen_solver.eigenvalues(); // this should be an Eigen vector of std::complex<S>
+  if (eigenvalues.size() != evs.size())
+    eigenvalues.resize(evs.size());
+  for (size_t ii = 0; ii < size_t(evs.size()); ++ii)
+    eigenvalues[ii] = evs[ii];
+  eigenvectors = eigen_solver.eigenvectors();
+} // ... compute_all_eigenvalues_and_vectors_using_eigen(...)
+
+
+template <class S>
+std::vector<XT::Common::complex_t<S>>
 compute_all_eigenvalues_using_eigen(const ::Eigen::Matrix<S, ::Eigen::Dynamic, ::Eigen::Dynamic>& matrix)
 {
   ::Eigen::EigenSolver<::Eigen::Matrix<S, ::Eigen::Dynamic, ::Eigen::Dynamic>> eigen_solver(
       matrix, /*computeEigenvectors=*/false);
   if (eigen_solver.info() != ::Eigen::Success)
     DUNE_THROW(Exceptions::eigen_solver_failed, "The eigen backend reported '" << eigen_solver.info() << "'!");
-  const auto evs = eigen_solver.eigenvalues(); // this should be an Eigen vector of std::complex<S>
-  std::vector<std::complex<S>> ret(evs.size());
+  const auto& evs = eigen_solver.eigenvalues(); // this should be an Eigen vector of std::complex<S>
+  std::vector<XT::Common::complex_t<S>> ret(evs.size());
   for (size_t ii = 0; ii < size_t(evs.size()); ++ii)
     ret[ii] = evs[ii];
   return ret;
@@ -47,25 +66,47 @@ compute_all_eigenvalues_using_eigen(const ::Eigen::Matrix<S, ::Eigen::Dynamic, :
 
 
 template <class S>
-std::vector<EigenDenseVector<std::complex<typename EigenDenseVector<S>::RealType>>>
+::Eigen::Matrix<XT::Common::complex_t<S>, ::Eigen::Dynamic, ::Eigen::Dynamic>
 compute_all_eigenvectors_using_eigen(const ::Eigen::Matrix<S, ::Eigen::Dynamic, ::Eigen::Dynamic>& matrix)
 {
   ::Eigen::EigenSolver<::Eigen::Matrix<S, ::Eigen::Dynamic, ::Eigen::Dynamic>> eigen_solver(
       matrix, /*computeEigenvectors=*/true);
   if (eigen_solver.info() != ::Eigen::Success)
     DUNE_THROW(Exceptions::eigen_solver_failed, "The eigen backend reported '" << eigen_solver.info() << "'!");
-  const auto eigenvectors = eigen_solver.eigenvectors(); // This should be an Eigen Matrix of std::complex<S> which
-  //                                                        contains the eigenvectors in the columns.
-  std::vector<EigenDenseVector<std::complex<typename EigenDenseVector<S>::RealType>>> ret;
-  ret.reserve(eigenvectors.cols());
-  for (size_t ii = 0; ii < size_t(eigenvectors.cols()); ++ii)
-    ret.emplace_back(eigenvectors.col(ii));
-  return ret;
+  return eigen_solver.eigenvectors();
 } // ... compute_all_eigenvectors_using_eigen(...)
 
 
-#endif // HAVE_EIGEN
+#else // HAVE_EIGEN
 
+
+template <class S>
+void compute_all_eigenvalues_and_vectors_using_eigen(
+    const ::Eigen::Matrix<S, ::Eigen::Dynamic, ::Eigen::Dynamic>& /*matrix*/,
+    std::vector<XT::Common::complex_t<S>>& /*eigenvalues*/,
+    ::Eigen::Matrix<XT::Common::complex_t<S>, ::Eigen::Dynamic, ::Eigen::Dynamic>& /*eigenvectors*/)
+{
+  static_assert(AlwaysFalse<S>::value, "You are missing eigen!");
+}
+
+
+template <class S>
+std::vector<XT::Common::complex_t<S>>
+compute_all_eigenvalues_using_eigen(const ::Eigen::Matrix<S, ::Eigen::Dynamic, ::Eigen::Dynamic>& /*matrix*/)
+{
+  static_assert(AlwaysFalse<S>::value, "You are missing eigen!");
+}
+
+
+template <class S>
+::Eigen::Matrix<XT::Common::complex_t<S>, ::Eigen::Dynamic, ::Eigen::Dynamic>
+compute_all_eigenvectors_using_eigen(const ::Eigen::Matrix<S, ::Eigen::Dynamic, ::Eigen::Dynamic>& /*matrix*/)
+{
+  static_assert(AlwaysFalse<S>::value, "You are missing eigen!");
+}
+
+
+#endif // HAVE_EIGEN
 
 } // namespace internal
 } // namespace LA
