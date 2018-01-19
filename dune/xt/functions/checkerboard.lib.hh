35b53f02b08b7696fc54373d782d8e081f69878c
René Milk <rene.milk@wwu.de>
2018-01-19 16:01:26 +0100

    [pylicense] apply updated

--- dune/xt/functions/checkerboard.lib.hh
+++ dune/xt/functions/checkerboard.lib.hh
@@ -1,11 +1,12 @@
 // This file is part of the dune-xt-functions project:
 //   https://github.com/dune-community/dune-xt-functions
-// Copyright 2009-2017 dune-xt-functions developers and contributors. All rights reserved.
+// Copyright 2009-2018 dune-xt-functions developers and contributors. All rights reserved.
 // License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 //      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 //          with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
 //   Felix Schindler (2017)
+//   Rene Milk       (2018)
 
 #ifndef DUNE_XT_FUNCTIONS_CHECKERBOARD_LIB_HH
 #define DUNE_XT_FUNCTIONS_CHECKERBOARD_LIB_HH
