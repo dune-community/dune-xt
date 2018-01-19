952b384782cce3aa72c7c4d5e46f2277adebdde2
René Milk <rene.milk@wwu.de>
2018-01-19 15:45:14 +0100

    [pylicense] apply updated

--- dune/xt/la/matrix-inverter/internal/eigen.hh
+++ dune/xt/la/matrix-inverter/internal/eigen.hh
@@ -1,11 +1,13 @@
 // This file is part of the dune-xt-la project:
 //   https://github.com/dune-community/dune-xt-la
-// Copyright 2009-2017 dune-xt-la developers and contributors. All rights reserved.
+// Copyright 2009-2018 dune-xt-la developers and contributors. All rights reserved.
 // License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 //      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 //          with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
 //   Felix Schindler (2017)
+//   Rene Milk       (2018)
+//   Tobias Leibner  (2018)
 
 #ifndef DUNE_XT_LA_MATRIX_INVERTER_INTERNAL_EIGEN_HH
 #define DUNE_XT_LA_MATRIX_INVERTER_INTERNAL_EIGEN_HH
