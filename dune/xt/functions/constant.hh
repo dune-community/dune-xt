28dbbcf20e97dcf587efa7b2520b18dbbbb3856f
Felix Schindler <felix.schindler@wwu.de>
2018-02-16 16:30:16 +0100

    [constant.hh] adjusting ConstantFunctionType to new template arguments and bind of functions. New default_config() and create() method provided

--- dune/xt/functions/constant.hh
+++ dune/xt/functions/constant.hh
@@ -17,7 +17,7 @@
 
 #include <dune/xt/common/configuration.hh>
 
-#include <dune/xt/functions/interfaces.hh>
+#include "interfaces.hh"
 
 namespace Dune {
 namespace XT {
@@ -87,87 +87,20 @@ typename UnitMatrix<K, dim>::type unit_matrix()
 }
 
 
-template <class R, size_t r, size_t rC>
-struct Get
-{
-  static std::string value_str()
-  {
-    std::string str = "[";
-    for (size_t rr = 0; rr < r; ++rr) {
-      if (rr > 0)
-        str += "; ";
-      for (size_t cc = 0; cc < rC; ++cc) {
-        if (cc > 0)
-          str += " ";
-        if (cc == rr)
-          str += "1";
-        else
-          str += "0";
-      }
-    }
-    str += "]";
-    return str;
-  }
-};
-
-template <class R, size_t rC>
-struct Get<R, 1, rC>
-{
-  static std::string value_str()
-  {
-    std::string str = "[";
-    for (size_t cc = 0; cc < rC; ++cc) {
-      if (cc > 0)
-        str += " ";
-      str += "1";
-    }
-    str += "]";
-    return str;
-  }
-};
-
-template <class R, size_t r>
-struct Get<R, r, 1>
-{
-  static std::string value_str()
-  {
-    return Get<R, 1, r>::value_str();
-  }
-};
-
-template <class R>
-struct Get<R, 1, 1>
-{
-  static std::string value_str()
-  {
-    return "1";
-  }
-};
-
 } // namespace internal
 
-template <class EntityImp,
-          class DomainFieldImp,
-          size_t domainDim,
-          class RangeFieldImp,
-          size_t rangeDim,
-          size_t rangeDimCols = 1>
-class ConstantFunction
-    : public GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols>
+
+template <size_t d, size_t r = 1, size_t rC = 1, class RangeFieldImp = double>
+class ConstantFunction : public SmoothFunctionInterface<d, r, rC, RangeFieldImp>
 {
-  typedef GlobalFunctionInterface<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols> BaseType;
-  typedef ConstantFunction<EntityImp, DomainFieldImp, domainDim, RangeFieldImp, rangeDim, rangeDimCols> ThisType;
+  using BaseType = SmoothFunctionInterface<d, r, rC, RangeFieldImp>;
+  using ThisType = ConstantFunction<d, r, rC, RangeFieldImp>;
 
 public:
-  typedef typename BaseType::DomainType DomainType;
-  typedef typename BaseType::RangeType RangeType;
-  typedef typename BaseType::JacobianRangeType JacobianRangeType;
-
-  using typename BaseType::LocalfunctionType;
-
-  static_assert(std::is_same<typename LocalfunctionType::RangeType, RangeType>::value, "RangeType mismatch");
-  static_assert(std::is_same<typename LocalfunctionType::DomainType, DomainType>::value, "DomainType mismatch");
-  static_assert(std::is_same<Dune::FieldVector<DomainFieldImp, domainDim>, DomainType>::value, "RangeType mismatch");
+  using DerivativeRangeType = typename BaseType::DerivativeRangeType;
+  using DomainType = typename BaseType::DomainType;
+  using RangeType = typename BaseType::RangeType;
+  using SingleDerivativeRangeType = typename BaseType::SingleDerivativeRangeType;
 
   static const bool available = true;
 
@@ -179,7 +112,8 @@ public:
   static Common::Configuration default_config(const std::string sub_name = "")
   {
     Common::Configuration config;
-    config["value"] = internal::Get<RangeFieldImp, rangeDim, rangeDimCols>::value_str();
+    config["type"] = static_id();
+    config["value"] = "[1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1]";
     config["name"] = static_id();
     if (sub_name.empty())
       return config;
@@ -191,12 +125,12 @@ public:
   } // ... default_config(...)
 
   static std::unique_ptr<ThisType> create(const Common::Configuration config = default_config(),
-                                          const std::string sub_name = static_id())
+                                          const std::string sub_name = "")
   {
     // get correct config
     const Common::Configuration cfg = config.has_sub(sub_name) ? config.sub(sub_name) : config;
     const Common::Configuration default_cfg = default_config();
-    return Common::make_unique<ThisType>(cfg.get("value", default_cfg.get<RangeType>("value")),
+    return Common::make_unique<ThisType>(cfg.get<RangeType>("value"),
                                          cfg.get("name", default_cfg.get<std::string>("name")));
   } // ... create(...)
 
@@ -216,53 +150,30 @@ public:
   ConstantFunction(const ThisType& other) = default;
 #endif
 
-  virtual std::string type() const override final
-  {
-    return BaseType::static_id() + ".constant";
-  }
-
-  virtual size_t order(const XT::Common::Parameter& /*mu*/ = {}) const override final
+  int order(const XT::Common::Parameter& /*mu*/ = {}) const override final
   {
     return 0;
   }
 
-  virtual void
-  evaluate(const DomainType& /*x*/, RangeType& ret, const Common::Parameter& /*mu*/ = {}) const override final
+  RangeType evaluate(const DomainType& /*x*/, const Common::Parameter& /*mu*/ = {}) const override final
   {
-    ret = constant_;
+    return constant_;
   }
 
-  virtual void
-  jacobian(const DomainType& /*x*/, JacobianRangeType& ret, const Common::Parameter& /*mu*/ = {}) const override final
+  DerivativeRangeType jacobian(const DomainType& /*x*/, const Common::Parameter& /*mu*/ = {}) const override final
   {
-    //! TODO: why not ```ret *=0```?
-    clear_jacobian<rangeDim, rangeDimCols>()(ret);
+    return DerivativeRangeType(0.);
   }
 
-  virtual std::string name() const override final
+  std::string name() const override final
   {
     return name_;
   }
 
-private:
-  template <size_t _r, size_t _rC, class Anything = void>
-  struct clear_jacobian
+  std::string type() const
   {
-    void operator()(JacobianRangeType& ret)
-    {
-      for (auto& col_jacobian : ret)
-        col_jacobian *= 0.;
-    }
-  };
-
-  template <class Anything>
-  struct clear_jacobian<1, 1, Anything>
-  {
-    void operator()(JacobianRangeType& ret)
-    {
-      ret *= 0.;
-    }
-  };
+    return BaseType::static_id() + ".constant";
+  }
 
   const RangeType constant_;
   const std::string name_;
