38620b4cdf65b4ebffe3a18f9fd88ecc8413c1a3
Tobias Leibner <tobias.leibner@uni-muenster.de>
2018-02-22 15:37:51 +0100

    [container.pattern] move to dune-xt-common

--- dune/xt/la/container/pattern.hh
+++ dune/xt/la/container/pattern.hh
@@ -13,50 +13,15 @@
 #ifndef DUNE_XT_LA_CONTAINER_PATTERN_HH
 #define DUNE_XT_LA_CONTAINER_PATTERN_HH
 
-#include <cstddef>
-#include <vector>
-#include <set>
+#include <dune/xt/common/pattern.hh>
 
 namespace Dune {
 namespace XT {
 namespace LA {
 
-class SparsityPatternDefault
-{
-private:
-  typedef std::vector<std::vector<size_t>> BaseType;
 
-public:
-  typedef BaseType::value_type InnerType;
-  typedef typename BaseType::const_iterator ConstOuterIteratorType;
+typedef typename XT::Common::SparsityPatternDefault SparsityPatternDefault;
 
-  explicit SparsityPatternDefault(const size_t _size = 0);
-
-  size_t size() const;
-
-  InnerType& inner(const size_t ii);
-
-  const InnerType& inner(const size_t ii) const;
-
-  ConstOuterIteratorType begin() const;
-
-  ConstOuterIteratorType end() const;
-
-  bool operator==(const SparsityPatternDefault& other) const;
-
-  bool operator!=(const SparsityPatternDefault& other) const;
-
-  SparsityPatternDefault operator+(const SparsityPatternDefault& other) const;
-
-  void insert(const size_t outer_index, const size_t inner_index);
-
-  void sort(const size_t outer_index);
-
-  void sort();
-
-private:
-  BaseType vector_of_vectors_;
-}; // class SparsityPatternDefault
 
 } // namespace LA
 } // namespace XT
