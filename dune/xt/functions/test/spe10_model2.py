79b80c2cb3f6831689570255a9fc574460d3e6b1
TiKeil <keil.menden@web.de>
2018-03-08 16:00:31 +0100

    [test.spe10.model2] add proper test

---
+++ dune/xt/functions/test/spe10_model2.py
@@ -0,0 +1,12 @@
+import grids
+import itertools
+from dune.xt.codegen import typeid_to_typedef_name
+
+dim_range = [3]
+dim_range_cols = [3]
+
+multi_out = {grids.pretty_print(g[0], g[1]) : g[0] for g in grids.type_and_dim_3d(cache)}
+
+multi_out = {filename + '.cc': {'types': [(filename, grid, r, rC)
+                             for r, rC in itertools.product(dim_range, dim_range_cols)]}
+             for filename, grid in multi_out.items()}
