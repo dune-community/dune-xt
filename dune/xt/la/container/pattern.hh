81fe974de18da680a6677e0d160bcbb5cfd064fa
Tobias Leibner <tobias.leibner@uni-muenster.de>
2018-04-06 15:52:46 +0200

    [pattern] undo removal

--- dune/xt/la/container/pattern.hh
+++ dune/xt/la/container/pattern.hh
@@ -13,14 +13,71 @@
 #ifndef DUNE_XT_LA_CONTAINER_PATTERN_HH
 #define DUNE_XT_LA_CONTAINER_PATTERN_HH
 
-#include <dune/xt/common/pattern.hh>
+#include <cstddef>
+#include <vector>
+
+#include <dune/xt/common/type_traits.hh>
 
 namespace Dune {
 namespace XT {
 namespace LA {
 
 
-typedef typename XT::Common::SparsityPatternDefault SparsityPatternDefault;
+class SparsityPatternDefault
+{
+private:
+  typedef std::vector<std::vector<size_t>> BaseType;
+
+public:
+  typedef BaseType::value_type InnerType;
+  typedef typename BaseType::iterator OuterIteratorType;
+  typedef typename BaseType::const_iterator ConstOuterIteratorType;
+
+  explicit SparsityPatternDefault(const size_t _size = 0);
+
+  size_t size() const;
+
+  InnerType& inner(const size_t ii);
+
+  const InnerType& inner(const size_t ii) const;
+
+  OuterIteratorType begin();
+
+  ConstOuterIteratorType begin() const;
+
+  OuterIteratorType end();
+
+  ConstOuterIteratorType end() const;
+
+  bool operator==(const SparsityPatternDefault& other) const;
+
+  bool operator!=(const SparsityPatternDefault& other) const;
+
+  SparsityPatternDefault operator+(const SparsityPatternDefault& other) const;
+
+  void insert(const size_t outer_index, const size_t inner_index);
+
+  void sort(const size_t outer_index);
+
+  void sort();
+
+private:
+  BaseType vector_of_vectors_;
+}; // class SparsityPatternDefault
+
+SparsityPatternDefault dense_pattern(const size_t rows, const size_t cols);
+
+SparsityPatternDefault tridiagonal_pattern(const size_t rows, const size_t cols);
+
+SparsityPatternDefault triangular_pattern(const size_t rows,
+                                          const size_t cols,
+                                          const Common::MatrixPattern& uplo = Common::MatrixPattern::lower_triangular);
+
+SparsityPatternDefault diagonal_pattern(const size_t rows, const size_t cols, int offset = 0);
+
+SparsityPatternDefault multiplication_pattern(const SparsityPatternDefault& lhs_pattern,
+                                              const SparsityPatternDefault& rhs_pattern,
+                                              const size_t rhs_cols);
 
 
 } // namespace LA
