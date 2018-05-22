74cdefdf250046222b8a67864490531d67b7e05c
TiKeil <keil.menden@web.de>
2018-05-22 17:13:07 +0200

    [global] change include

--- dune/xt/functions/global.hh
+++ dune/xt/functions/global.hh
@@ -12,4 +12,4 @@
 
 #warning This header is deprecated, include <dune/xt/functions/lambda/global-function.hh> instead (18.05.2017)!
 
-#include <dune/xt/functions/lambda/global-function.hh>
+#include <dune/xt/functions/lambda/smooth-function.hh>
