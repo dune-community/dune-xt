bf13fe4002cdd5506e99c5a5310235f5deac56d0
TiKeil <keil.menden@web.de>
2018-05-23 12:26:50 +0200

    [indicator] Update includes

--- dune/xt/functions/indicator.hh
+++ dune/xt/functions/indicator.hh
@@ -12,15 +12,9 @@
 #ifndef DUNE_XT_FUNCTIONS_INDICATOR_HH
 #define DUNE_XT_FUNCTIONS_INDICATOR_HH
 
-#include <memory>
 #include <utility>
-#include <vector>
 
 #include <dune/xt/common/configuration.hh>
-#include <dune/xt/common/fvector.hh>
-#include <dune/xt/common/memory.hh>
-#include <dune/xt/common/string.hh>
-#include <dune/xt/common/type_traits.hh>
 #include <dune/xt/common/numeric_cast.hh>
 
 #include <dune/xt/functions/interfaces.hh>
