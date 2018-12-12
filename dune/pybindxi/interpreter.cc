7ddf4c35fa0f0a17b389d077c50f08d9c6aa5010
Felix Schindler <felix.schindler@wwu.de>
2018-12-12 21:04:11 +0100

    apply clang-format-6

--- dune/pybindxi/interpreter.cc
+++ dune/pybindxi/interpreter.cc
@@ -1,9 +1,7 @@
 // This file is part of the dune-pybindxi project:
 //   https://github.com/dune-community/dune-pybindxi
-// Copyright 2009-2017 dune-xt-la developers and contributors. All rights
-// reserved.
-// License: Dual licensed as BSD 2-Clause License
-// (http://opensource.org/licenses/BSD-2-Clause)
+// Copyright 2009-2017 dune-pybindxi developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 //      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
 //          with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
@@ -14,8 +12,9 @@
 namespace Dune {
 namespace PybindXI {
 
-pybind11::module
-ScopedInterpreter::import_module(const std::string &module_name) {
+
+pybind11::module ScopedInterpreter::import_module(const std::string& module_name)
+{
   auto result = modules_.find(module_name);
   if (result != modules_.end())
     return result->second;
@@ -25,10 +24,13 @@ ScopedInterpreter::import_module(const std::string &module_name) {
   }
 } // ... load_module(...)
 
-ScopedInterpreter &GlobalInterpreter() {
+
+ScopedInterpreter& GlobalInterpreter()
+{
   static ScopedInterpreter global_interpreter;
   return global_interpreter;
 }
+
 
 } // namespace PybindXI
 } // namespace Dune
