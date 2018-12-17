cea4ffb78514286690d4ea2b9ac030298e28f6ac
René Fritze <rene.fritze@wwu.de>
2018-12-17 13:59:36 +0100

    [py] fix boundaryinfo available for multiple types

--- python/dune/xt/grid/boundaryinfo.bindings.hh
+++ python/dune/xt/grid/boundaryinfo.bindings.hh
@@ -43,18 +43,20 @@ public:
   static void bind(pybind11::module& m)
   {
     using namespace pybind11::literals;
+    const auto gridname = XT::Grid::bindings::grid_name<typename GP::GridType>::value();
 
     try { // guard since we might not be the first to do so for this grid/intersection
-      m.def(std::string("available_boundary_infos_on_" + layer_names[layer] + "_layer").c_str(),
+      std::string postfix{layer_names[layer] + "_layer_" + gridname};
+      m.def((std::string("available_boundary_infos_on_") + postfix) .c_str(),
             [](const GP& /*grid_provider*/) { return XT::Grid::BoundaryInfoFactory<I>::available(); },
             "grid_provider"_a);
-      m.def(std::string("default_boundary_info_config_on_" + layer_names[layer] + "_layer").c_str(),
+      m.def((std::string("default_boundary_info_config_on_")+ postfix) .c_str(),
             [](const GP& /*grid_provider*/, const std::string& type) {
               return XT::Grid::BoundaryInfoFactory<I>::default_config(type);
             },
             "grid_provider"_a,
             "type"_a);
-      m.def(std::string("make_boundary_info_on_" + layer_names[layer] + "_layer").c_str(),
+      m.def((std::string("make_boundary_info_on_")+ postfix) .c_str(),
             [](const GP& /*grid_provider*/, const Common::Configuration& cfg) {
               return XT::Grid::BoundaryInfoFactory<I>::create(cfg).release();
             },
