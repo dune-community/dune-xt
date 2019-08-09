74f8ae980cd6f84dd6f98a915e75085976f7e058
Felix Schindler <felix.schindler@wwu.de>
2019-08-09 14:29:32 +0200

    [test.grid-function] add failing test

---
+++ dune/xt/functions/test/grid_function.cc
@@ -0,0 +1,31 @@
+// This file is part of the dune-xt-functions project:
+//   https://github.com/dune-community/dune-xt-functions
+// Copyright 2009-2018 dune-xt-functions developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler (2019)
+
+#include <dune/xt/common/test/main.hxx> // <- Has to come first, includes the config.h!
+
+#include <dune/xt/common/unused.hh>
+#include <dune/xt/grid/type_traits.hh>
+#include <dune/xt/grid/grids.hh>
+#include <dune/xt/grid/gridprovider/cube.hh>
+#include <dune/xt/functions/generic/grid-function.hh>
+#include <dune/xt/functions/grid-function.hh>
+
+
+GTEST_TEST(MatrixGridFunction, constructible_from_scalar_grid_function_rvalueref)
+{
+  using namespace Dune;
+  using namespace Dune::XT::Functions;
+
+  using G = CUBEGRID_2D;
+  using E = XT::Grid::extract_entity_t<G>;
+
+  const GenericGridFunction<E> scalar_grid_function{0};
+
+  GridFunction<E, 2, 2> DXTC_UNUSED(func){scalar_grid_function};
+} // GTEST_TEST(MatrixGridFunction, constructible_from_scalar_grid_function_rvalueref)
