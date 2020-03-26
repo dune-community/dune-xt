05d9e0f0a03185f84ad475a188bf1e043766517b
René Fritze <rene.fritze@wwu.de>
2020-03-26 09:40:05 +0100

    [cmake] cleanout stry license text

--- cmake/modules/DuneXtMacros.cmake
+++ cmake/modules/DuneXtMacros.cmake
@@ -110,47 +110,5 @@ if(NOT DS_HEADERCHECK_DISABLE)
 endif(NOT DS_HEADERCHECK_DISABLE)
 set(DXT_TEST_TIMEOUT 180 CACHE STRING "per-test timeout in seconds")
 set(DXT_TEST_PROCS 1 CACHE STRING "run N tests in parallel")
-# ~~~
-# This file is part of the dune-xt project:
-#   https://github.com/dune-community/dune-xt
-# Copyright 2009-2019 dune-xt developers and contributors. All rights reserved.
-# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
-#          with "runtime exception" (http://www.dune-project.org/license.html)
-# Authors:
-#   Felix Schindler (2016 - 2017)
-#   René Fritze     (2018)
-#   Tim Keil        (2018)
-#   Tobias Leibner  (2018)
-#
-# File for module specific CMake tests.
-# ~~~
-# ~~~
-# This file is part of the dune-xt project:
-#   https://github.com/dune-community/dune-xt
-# Copyright 2009-2019 dune-xt developers and contributors. All rights reserved.
-# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
-#          with "runtime exception" (http://www.dune-project.org/license.html)
-# Authors:
-#   Felix Schindler (2016 - 2018)
-#   René Fritze     (2017 - 2018)
-#   Tobias Leibner  (2018)
-#
-# File for module specific CMake tests.
-# ~~~
 
 set(DUNE_GRID_EXPERIMENTAL_GRID_EXTENSIONS TRUE)
-# ~~~
-# This file is part of the dune-xt project:
-#   https://github.com/dune-community/dune-xt
-# Copyright 2009-2019 dune-xt developers and contributors. All rights reserved.
-# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
-#          with "runtime exception" (http://www.dune-project.org/license.html)
-# Authors:
-#   Felix Schindler (2016 - 2017)
-#   René Fritze     (2018)
-#
-# File for module specific CMake tests.
-# ~~~
