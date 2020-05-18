f802ba43ee9e0c292333f7f600f3f36636ab97b9
Felix Schindler <felix.schindler@wwu.de>
2020-05-18 14:10:56 +0200

    [common.print] add print() and repr(), WARNING: replaces unused print()

--- dune/xt/common/print.hh
+++ dune/xt/common/print.hh
@@ -16,66 +16,252 @@
 #include <string>
 #include <iostream>
 
+#include <dune/xt/common/configuration.hh>
 #include <dune/xt/common/filesystem.hh>
 #include <dune/xt/common/ranges.hh>
 #include <dune/xt/common/string.hh>
+#include <dune/xt/common/type_traits.hh>
 
 namespace Dune {
 namespace XT {
 namespace Common {
+namespace internal {
 
-template <class V>
-typename std::enable_if<is_vector<V>::value, void>::type
-print(const V& vec, const std::string name, std::ostream& out, const std::string prefix = "")
-{
-  out << prefix << name << " = ";
-  if (vec.size() == 0)
-    out << "[]";
-  else if (vec.size() == 1)
-    out << vec[0];
-  else {
-    out << "[" << vec[0];
-    for (decltype(vec.size()) ii = 1; ii < vec.size(); ++ii)
-      out << " " << vec[ii];
-    out << "]";
-  }
-} // ... print(...)
 
-template <class M>
-typename std::enable_if<is_matrix<M>::value, void>::type
-print(const M& mat, const std::string name, std::ostream& out, const std::string prefix = "")
+/// \note Should be used to derive from (except for vectors and matrices), when specializing Printer.
+/// \sa Printer
+/// \sa VectorPrinter
+template <class T, bool use_repr = false>
+class DefaultPrinter
 {
-  typedef MatrixAbstraction<M> MM;
-  const size_t rows = MM::rows(mat);
-  const size_t cols = MM::cols(mat);
-  out << prefix << name << " = ";
-  if (rows == 0 && cols == 0)
-    out << "[]";
-  else if (rows == 1) {
-    out << "[" << MM::get_entry(mat, 0, 0);
-    for (size_t jj = 1; jj < cols; ++jj)
-      out << " " << MM::get_entry(mat, 0, jj);
-    out << "]";
-  } else if (cols == 1) {
-    out << "[" << MM::get_entry(mat, 0, 0);
-    for (size_t ii = 1; ii < rows; ++ii)
-      out << " " << MM::get_entry(mat, ii, 0);
-    out << "]";
-  } else {
-    out << "[";
-    auto local_prefix = prefix + " ";
-    for (size_t ii = 0; ii < rows - 1; ++ii) {
-      out << MM::get_entry(mat, ii, 0);
-      for (size_t jj = 1; jj < cols; ++jj)
-        out << " " << MM::get_entry(mat, ii, jj);
-      out << ";\n" << local_prefix;
+  using ThisType = DefaultPrinter;
+
+  template <bool has_ostream = is_printable<T>::value, bool anything = true>
+  struct call_ostream;
+
+  template <bool anything>
+  struct call_ostream<true, anything>
+  {
+    static void or_print_error(std::ostream& out, const T& val)
+    {
+      out << val;
     }
-    out << MM::get_entry(mat, rows - 1, 0);
-    for (size_t jj = 1; jj < cols; ++jj)
-      out << " " << MM::get_entry(mat, rows - 1, jj);
-    out << "]";
+  };
+
+  template <bool anything>
+  struct call_ostream<false, anything>
+  {
+    static void or_print_error(std::ostream& out, const T& /*val*/)
+    {
+      out << "missing specialization for Printer<T> with T=" << Typename<T>::value();
+    }
+  };
+
+public:
+  using ValueType = T;
+
+  const ValueType& value;
+  const Configuration opts;
+
+  DefaultPrinter(const ValueType& val, const Configuration& cfg)
+    : value(val)
+    , opts(cfg)
+  {}
+
+  DefaultPrinter(const ThisType&) = default;
+  DefaultPrinter(ThisType&&) = default;
+
+  virtual ~DefaultPrinter() = default;
+
+  virtual void repr(std::ostream& out) const
+  {
+    call_ostream<>::or_print_error(out, value);
   }
-} // ... print(...)
+
+  virtual void str(std::ostream& out) const
+  {
+    this->repr(out);
+  }
+
+  void print(std::ostream& out) const
+  {
+    if (use_repr)
+      this->repr(out);
+    else
+      this->str(out);
+  }
+}; // class DefaultPrinter
+
+
+/// \note Should be used to derive from when specializing Printer for vectors.
+/// \sa Printer
+/// \sa DefaultPrinter
+template <class T, bool use_repr>
+class VectorPrinter : public internal::DefaultPrinter<T, use_repr>
+{
+  static_assert(is_vector<T>::value, "");
+
+  using V = VectorAbstraction<T>;
+
+public:
+  const std::string class_name;
+
+  VectorPrinter(const T& val, const Configuration& cfg = {}, const std::string& clss_nm = Typename<T>::value())
+    : internal::DefaultPrinter<T, use_repr>(val, cfg)
+    , class_name(clss_nm)
+  {}
+
+  void repr(std::ostream& out) const override
+  {
+    out << class_name << "(";
+    const auto sz = this->value.size();
+    if (sz > 0) {
+      const std::string delim =
+          (std::use_facet<std::numpunct<char>>(std::cout.getloc()).decimal_point() == ',') ? ";" : ",";
+      out << "{" << V::get_entry(this->value, 0);
+      for (auto&& ii : value_range(decltype(sz)(1), sz))
+        out << delim << " " << V::get_entry(this->value, ii);
+      out << "}";
+    }
+    out << ")";
+  } // ... repr(...)
+
+  void str(std::ostream& out) const override
+  {
+    const auto sz = this->value.size();
+    if (sz == 0)
+      out << "[]";
+    else {
+      const std::string delim =
+          (std::use_facet<std::numpunct<char>>(std::cout.getloc()).decimal_point() == ',') ? ";" : ",";
+      out << "[" << V::get_entry(this->value, 0);
+      for (auto&& ii : value_range(decltype(sz)(1), sz))
+        out << delim << " " << V::get_entry(this->value, ii);
+      out << "]";
+    }
+  } // ... str(...)
+}; // class VectorPrinter
+
+
+} // namespace internal
+
+
+/**
+ * \brief Specialize this class for your type to benefit from the print() and repr() functions.
+ *
+ * Any specialization of Printer is expected to have the following print() method
+<code>
+  void print(std::ostream& out) const
+  {
+    if (use_repr)
+      this->repr(out);
+    else
+      this->str(out);
+  }
+</code>
+ * the behaviour of which is supposed to change with use_repr. A possible implemenation is given in
+ * internal::DefaultPrinter, based on the following two methods:
+<code>
+  void repr(std::ostream& out) const
+  {
+    out << "detailed and aunambigous description";
+  }
+
+  void str(std::ostream& out) const
+  {
+    out << "compact and readabale description";
+  }
+</code>
+ * It is therefore recommended to derive from internal::DefaultPrinter or internal::VectorPrinter, where some of this
+ * is already provided.
+ *
+ * Similar to python, one can use repr(obj) to obtain an unambigous representation of obj (with as much detail as
+ * possible), while print(obj) mimics Pythons __str__ to obtain a compact and readable representation of obj.
+ *
+ * \sa internal::DefaultPrinter
+ * \sa print
+ * \sa repr
+ */
+template <class T, bool use_repr = false, typename anything = void>
+class Printer : public internal::DefaultPrinter<T, use_repr>
+{
+public:
+  Printer(const T& val, const Configuration& param = {})
+    : internal::DefaultPrinter<T, use_repr>(val, param)
+  {}
+};
+
+
+/// Specialization of Printer for std::vector<T> with arithmetic T
+template <class T, bool use_repr>
+class Printer<std::vector<T>, use_repr, std::enable_if_t<std::is_arithmetic<T>::value>>
+  : public internal::VectorPrinter<std::vector<T>, use_repr>
+{
+public:
+  Printer(const std::vector<T>& val, const Configuration& param = {})
+    : internal::VectorPrinter<std::vector<T>, use_repr>(val, param)
+  {}
+}; // class Printer
+
+
+/// \sa Printer
+template <class T, bool use_repr>
+std::ostream& operator<<(std::ostream& out, const Printer<T, use_repr>& printer)
+{
+  printer.print(out);
+  return out;
+}
+
+
+/**
+ * The purpose of print(obj) is to provide a flexible and customizable means of modifying the behaviour of printing an
+ * object to a stream, without conflicting with implementations of operator<< for obj, which are often beyond the reach
+ * of the user. Can be used as in:
+<code>
+std::cout << print(complicated_object, {"tabular", "true"}) << std::endl;
+</code>
+ * The output can be determined by specializing Printer<TypeOfComplicatedObject>, which keeps a reference to
+ * complicated_object and a Configuration({"tabular", "true"}), which can be used by the implementor to customize the
+ * output. Defaults to
+<code>
+std::cout << complicated_object << std::endl;
+</code>
+ * if operator<< is available for complicated_object and no specialization of Printer is found.
+ *
+ * \sa Printer
+ * \sa repr
+ */
+template <class T>
+Printer<T, false> print(const T& value, const Configuration& param = {})
+{
+  return Printer<T, false>(value, param);
+}
+
+
+/**
+ * The purpose of repr(obj) is to provide a flexible and customizable means of modifying the behaviour of printing an
+ * object to a stream, without conflicting with implementations of operator<< for obj, which are often beyond the reach
+ * of the user. Can be used as in:
+<code>
+std::cout << print(complicated_object, {"tabular", "true"}) << std::endl;
+</code>
+ * The output can be determined by specializing Printer<TypeOfComplicatedObject>, which keeps a reference to
+ * complicated_object and a Configuration({"tabular", "true"}), which can be used by the implementor to customize the
+ * output. Defaults to
+<code>
+std::cout << complicated_object << std::endl;
+</code>
+ * if operator<< is available for complicated_object and no specialization of Printer is found.
+ *
+ * \sa Printer
+ * \sa print
+ */
+template <class T>
+Printer<T, true> repr(const T& value, const Configuration& param = {})
+{
+  return Printer<T, true>(value, param);
+}
+
 
 //! useful for visualizing sparsity patterns of matrices
 template <class Matrix>
@@ -95,6 +281,7 @@ void matrix_to_gnuplot_stream(const Matrix& matrix, std::ostream& stream)
   stream << "#total non zeros " << nz << " of " << matrix.rows() * matrix.cols() << " entries\n";
 } // matrix_to_gnuplot_stream
 
+
 //! maps 1,2,3 to x,y,z / X,Y,Z
 inline std::string dim_to_axis_name(const size_t dim, const bool capitalize = false)
 {
@@ -105,6 +292,7 @@ inline std::string dim_to_axis_name(const size_t dim, const bool capitalize = fa
     c -= 32;
   return std::string() += c;
 } // matrix_to_gnuplot_stream
+
 
 /** Outputiterator to emulate python's str.join(iterable)
  * \see http://codereview.stackexchange.com/questions/30132/comma-formatted-stl-vectors/30181#30181
@@ -152,7 +340,8 @@ public:
       ostream << prefix << value;
     }
   }
-};
+}; // class PrefixOutputIterator
+
 
 } // namespace Common
 } // namespace XT
