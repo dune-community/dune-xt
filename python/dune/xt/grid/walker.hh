0389a2d0327c0f7552291df14bd45b2d71f99132
Felix Schindler <felix.schindler@wwu.de>
2020-05-18 14:15:34 +0200

    [python|grid.walker] allow derived classes to inherit methods

--- python/dune/xt/grid/walker.hh
+++ python/dune/xt/grid/walker.hh
@@ -33,6 +33,39 @@ public:
   using base_type = Grid::ElementAndIntersectionFunctor<GV>;
   using bound_type = pybind11::class_<type, base_type>;
 
+  template <class T, typename... options>
+  static void addbind_methods(pybind11::class_<T, options...>& c)
+  {
+    namespace py = pybind11;
+    using namespace pybind11::literals;
+
+    c.def("append",
+          [](T& self, ElementFunctor<GV>& functor, const ElementFilter<GV>& filter) { self.append(functor, filter); },
+          "element_functor"_a,
+          "element_filter"_a = ApplyOn::AllElements<GV>());
+    c.def("append",
+          [](T& self, IntersectionFunctor<GV>& functor, const IntersectionFilter<GV>& filter) {
+            self.append(functor, filter);
+          },
+          "intersection_functor"_a,
+          "intersection_filter"_a = ApplyOn::AllIntersections<GV>());
+    c.def("append",
+          [](T& self,
+             ElementAndIntersectionFunctor<GV>& functor,
+             const IntersectionFilter<GV>& intersection_filter,
+             const ElementFilter<GV>& element_filter) { self.append(functor, intersection_filter, element_filter); },
+          "element_and_intersection_functor"_a,
+          "intersection_filter"_a = ApplyOn::AllIntersections<GV>(),
+          "element_filter"_a = ApplyOn::AllElements<GV>());
+    c.def("walk",
+          [](T& self, const bool thread_parallel = false, const bool clear_functors_after_walk = true) {
+            self.walk(thread_parallel, clear_functors_after_walk);
+          },
+          "thread_parallel"_a = false,
+          "clear_functors_after_walk"_a = true,
+          py::call_guard<py::gil_scoped_release>());
+  } // ... addbind_methods(...)
+
   static bound_type
   bind(pybind11::module& m, const std::string& class_id = "walker", const std::string& grid_id = grid_name<G>::value())
   {
@@ -42,33 +75,10 @@ public:
     auto ClassName = Common::to_camel_case(class_id + "_" + grid_id);
     bound_type c(m, ClassName.c_str(), ClassName.c_str());
     c.def(py::init([](GridProvider<G>& grid_provider) { return type(grid_provider.leaf_view()); }), "grid_provider"_a);
-    c.def(
-        "append",
-        [](type& self, ElementFunctor<GV>& functor, const ElementFilter<GV>& filter) { self.append(functor, filter); },
-        "element_functor"_a,
-        "element_filter"_a = ApplyOn::AllElements<GV>());
-    c.def("append",
-          [](type& self, IntersectionFunctor<GV>& functor, const IntersectionFilter<GV>& filter) {
-            self.append(functor, filter);
-          },
-          "intersection_functor"_a,
-          "intersection_filter"_a = ApplyOn::AllIntersections<GV>());
-    c.def("append",
-          [](type& self,
-             ElementAndIntersectionFunctor<GV>& functor,
-             const IntersectionFilter<GV>& intersection_filter,
-             const ElementFilter<GV>& element_filter) { self.append(functor, intersection_filter, element_filter); },
-          "element_and_intersection_functor"_a,
-          "intersection_filter"_a = ApplyOn::AllIntersections<GV>(),
-          "element_filter"_a = ApplyOn::AllElements<GV>());
-    c.def("walk",
-          [](type& self, const bool thread_parallel = false, const bool clear_functors_after_walk = true) {
-            self.walk(thread_parallel, clear_functors_after_walk);
-          },
-          "thread_parallel"_a = false,
-          "clear_functors_after_walk"_a = true,
-          py::call_guard<py::gil_scoped_release>());
 
+    addbind_methods(c);
+
+    // factories
     m.def(Common::to_camel_case(class_id).c_str(),
           [](GridProvider<G>& grid_provider) { return type(grid_provider.leaf_view()); },
           "grid_provider"_a);
