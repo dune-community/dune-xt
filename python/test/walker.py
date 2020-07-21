746916c106db093c7a9cf39c158bf8041842b661
Felix Schindler <felix.schindler@wwu.de>
2020-07-21 15:35:25 +0200

    [P|test] refactor walker test, does not test much yet

--- python/test/walker.py
+++ python/test/walker.py
@@ -10,101 +10,30 @@
 #   Tobias Leibner (2018 - 2020)
 # ~~~
 
-import functools
-import itertools
+from pymortests.base import runmodule
 
-import pytest
-import dune.xt.common as xtc
-import dune.xt.grid as xtg
-from dune.xt.grid import provider, types, walker
+from dune.xt.grid import Dim, Cube, Simplex, make_cube_grid, Walker
 
-
-def _grid_provider_factory(grid_type, mpi, maker_str='make_cube_grid__{}'):
-    if not mpi:
-        fn = maker_str.format(grid_type)
-        maker = getattr(xtg.provider, fn)
-        return maker()
-
-    try:
-        from mpi4py import MPI
-    except ImportError:
-        pytest.skip('optional mpi4py is missing')
-        return
-    opts = provider.default_options_cube_grid(grid_type)
-    fn = maker_str.format(grid_type)
-    maker = getattr(xtg.provider, fn)
-    return maker(opts, MPI.COMM_WORLD)
+init_args = (
+    (Dim(1), [0], [1], [2]),
+    (Dim(2), Cube(), [0, 0], [1, 1], [2, 2]),
+    (Dim(2), Simplex(), [0, 0], [1, 1], [2, 2]),
+    (Dim(3), Cube(), [0, 0, 0], [1, 1, 1], [2, 2, 2]),
+    (Dim(3), Simplex(), [0, 0, 0], [1, 1, 1], [2, 2, 2]),
+)
 
 
-_dd_subdomain_grid_provider_factory = functools.partial(
-    _grid_provider_factory, maker_str='make_cube_dd_subdomain_grid__{}')
-'''
-@pytest.fixture(params=xtg.types.available_types)
-def mpi_grid_provider(request):
-    return _grid_provider_factory(request.param, mpi=True)
+def test_init():
+    for args in init_args:
+        grid = make_cube_grid(*args)
+        walker = Walker(grid)
 
+def test_walk():
+    for args in init_args:
+        grid = make_cube_grid(*args)
+        walker = Walker(grid)
+        walker.walk()
 
-@pytest.fixture(params=xtg.types.available_types)
-def grid_provider(request):
-    return _grid_provider_factory(request.param, mpi=False)
+if __name__ == "__main__":
+    runmodule(filename=__file__)
 
-
-@pytest.fixture(params=itertools.product(xtg.types.available_types, (True, False)))
-def combined_grid_provider(request):
-    return _grid_provider_factory(*request.param)
-
-
-@pytest.fixture(params=itertools.product(xtg.types.available_types, (True, False)))
-def combined_dd_subdomain_grid_provider(request):
-    return _grid_provider_factory(*request.param)
-'''
-
-
-def test_available():
-    assert len(xtg.types.available_types) > 0
-
-
-'''
-def test_grid_provider(combined_grid_provider):
-    grid_provider = combined_grid_provider
-    assert grid_provider.max_level() >= 0
-    num_el = grid_provider.num_elements
-    assert num_el > 1
-    grid_provider.global_refine(1)
-    try:
-        grid_provider.visualize()
-    except xtc.DuneError as e:
-        if 'NotImplemented' not in str(e):
-            raise e
-    assert grid_provider.num_elements > num_el
-    assert grid_provider.num_subdomains == 1
-    assert grid_provider.max_entity_diameter() > 0
-    assert grid_provider.dim > 0
-
-
-def test_dd_subdomain_grid_provider(combined_dd_subdomain_grid_provider):
-    grid_provider = combined_dd_subdomain_grid_provider
-    assert grid_provider.max_level() >= 0
-    num_el = grid_provider.num_elements
-    assert num_el > 1
-    grid_provider.global_refine(1)
-    try:
-        grid_provider.visualize()
-    except xtc.DuneError as e:
-        if 'NotImplemented' not in str(e):
-            raise e
-    assert grid_provider.num_elements > num_el
-    assert grid_provider.num_subdomains == 1
-    assert grid_provider.max_entity_diameter() > 0
-    assert grid_provider.dim > 0
-
-
-def test_walker(grid_provider):
-    walker = xtg.walker.make_walker(grid_provider)
-    walker.walk()
-    walker.clear()
-'''
-
-
-def test_count():
-    pass
