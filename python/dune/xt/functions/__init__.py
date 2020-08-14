440339a90c0be6e9b6d0fb08d0532b6ea597e632
Felix Schindler <felix.schindler@wwu.de>
2020-08-14 06:52:32 +0200

    [P|functions] add ElementwiseDiameterFunction

--- python/dune/xt/functions/__init__.py
+++ python/dune/xt/functions/__init__.py
@@ -22,6 +22,7 @@ for mod_name in (
         '_functions_function_interface_2d',
         '_functions_checkerboard',
         '_functions_constant',
+        '_functions_elementwise_diameter',
         '_functions_elementwise_minimum',
         '_functions_expression',
         '_functions_divergence',
