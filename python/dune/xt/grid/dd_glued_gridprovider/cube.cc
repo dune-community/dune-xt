bb5110ff3f9406455cc4d63a7a875b9faa6894c4
Tim Keil <tim.keil@wwu.de>
2020-11-17 19:27:46 +0100

    [python] introduce keep_alive

--- python/dune/xt/grid/dd_glued_gridprovider/cube.cc
+++ python/dune/xt/grid/dd_glued_gridprovider/cube.cc
@@ -35,40 +35,35 @@ struct make_cube_dd_grid
     m.def("make_cube_dd_grid",
           [](XT::Grid::GridProvider<G>& macro_grid,
           const unsigned int num_refinements) {
-      return XT::Grid::DD::Glued<G,G,XT::Grid::Layers::leaf>(macro_grid, num_refinements, false, true
-      );},
+      return std::make_unique<XT::Grid::DD::Glued<G,G,XT::Grid::Layers::leaf>>(macro_grid, num_refinements, false, true);
+    },
     "macro_grid"_a,
-//    "element_type"_a,
-    "num_refinements"_a = 0
-//    "overlap_size"_a = XT::Grid::cube_gridprovider_default_config().get<std::array<unsigned int, d>>("overlap_size")/*,
-//    "mpi_comm"_a = Common::MPI_Comm_Wrapper()*/
+    "num_refinements"_a = 0,
+    py::keep_alive<0, 1>()
     );
   } // ... bind(...)
 }; // struct make_cube_dd_grid<...>
 
-template <class G>
-struct make_cube_dd_grid<G, void>
-{
-  static void bind(pybind11::module& m)
-  {
-    namespace py = pybind11;
-    using namespace pybind11::literals;
-    using D = typename G::ctype;
-    static const size_t d = G::dimension;
+//template <class G>
+//struct make_cube_dd_grid<G, void>
+//{
+//  static void bind(pybind11::module& m)
+//  {
+//    namespace py = pybind11;
+//    using namespace pybind11::literals;
+//    using D = typename G::ctype;
+//    static const size_t d = G::dimension;
 
-    m.def("make_cube_dd_grid",
-          [](XT::Grid::GridProvider<G>& macro_grid,
-          const unsigned int num_refinements) {
-      return XT::Grid::DD::Glued<G,G,XT::Grid::Layers::leaf>(macro_grid, num_refinements, false, true)
-      ;},
-    "macro_grid"_a,
-//    "element_type"_a,
-    "num_refinements"_a = 0
-//    "overlap_size"_a = XT::Grid::cube_gridprovider_default_config().get<std::array<unsigned int, d>>("overlap_size")/*,
-//    "mpi_comm"_a = Common::MPI_Comm_Wrapper()*/
-    );
-  } // ... bind(...)
-}; // struct make_cube_dd_grid<..., void>
+//    m.def("make_cube_dd_grid",
+//          [](XT::Grid::GridProvider<G>& macro_grid,
+//          const unsigned int num_refinements) {
+//      return std::make_unique<XT::Grid::DD::Glued<G,G,XT::Grid::Layers::leaf>>(macro_grid, num_refinements, false, true);
+//    },
+//    "macro_grid"_a,
+//    "num_refinements"_a = 0
+//    );
+//  } // ... bind(...)
+//}; // struct make_cube_dd_grid<..., void>
 
 
 PYBIND11_MODULE(_grid_dd_glued_gridprovider_cube, m)
