1139e825b7de8b24007f08870157d24c45a096c3
Felix Schindler <felix.schindler@wwu.de>
2020-12-08 13:41:28 +0100

    [functions.grid-functions] fix operator/

--- dune/xt/functions/interfaces/grid-function.hh
+++ dune/xt/functions/interfaces/grid-function.hh
@@ -218,7 +218,7 @@ public:
   operator/(const OtherType& other) const
   {
     LOG_(debug) << "operator/(other=" << &other << ")" << std::endl;
-    return Functions::ProductGridFunction<ThisType, as_grid_function_interface_t<OtherType>>(
+    return Functions::FractionGridFunction<ThisType, as_grid_function_interface_t<OtherType>>(
         *this,
         other,
         "(" + this->name() + ")/(" + other.name() + ")",
