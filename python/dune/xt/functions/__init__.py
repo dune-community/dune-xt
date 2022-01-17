5759446674cea9393dd111cd9a917f462ae32f4f
Felix Schindler <felix.schindler@wwu.de>
2022-01-17 13:15:45 +0100

    [P|functions] extend visualize for time series and 3d

--- python/dune/xt/functions/__init__.py
+++ python/dune/xt/functions/__init__.py
@@ -12,9 +12,6 @@
 #   Tobias Leibner  (2019 - 2020)
 # ~~~
 
-from tempfile import NamedTemporaryFile
-import numpy as np
-
 from dune.xt import guarded_import
 from dune.xt.common.config import config
 
@@ -44,20 +41,53 @@ for mod_name in (
 ):
     guarded_import(globals(), 'dune.xt.functions', mod_name)
 
+
 if config.HAVE_K3D:
-    from tempfile import NamedTemporaryFile
+    import os
+    import tempfile
     from dune.xt.common.vtk.plot import plot
     from dune.xt.functions._functions_gridfunction import GridFunction
 
-    def visualize_function(function, grid, subsampling=False):
-        assert function.dim_domain == 2, f'Not implemented yet for {function.dim_domain}-dimensional grids!'
-        assert function.dim_range == 1, f'Not implemented yet for {function.dim_range}-dimensional functions!'
-        tmpfile = NamedTemporaryFile(mode='wb', delete=False, suffix='.vtu').name
-        try:
-            function.visualize(grid, filename=tmpfile[:-4], subsampling=subsampling)
-        except AttributeError:
-            GridFunction(grid, function).visualize(grid, filename=tmpfile[:-4], subsampling=subsampling)
-        return plot(tmpfile, color_attribute_name=function.name)
+
+    def visualize_function(functions, grid, interactive=False, subsampling=False):
+        if not isinstance(functions, (list, tuple)):
+            functions = (functions,)
+        # this is a (bad) implicit check that these are functions
+        dim_domain = functions[0].dim_domain
+        dim_range = functions[0].dim_range
+        name = functions[0].name
+        assert(all(f.dim_domain == dim_domain for f in functions))
+        assert(all(f.dim_range == dim_range for f in functions))
+        assert(all(f.name == name for f in functions))
+
+        def visualize_single(func, filename):
+            try:
+                func.visualize(grid, filename=filename, subsampling=subsampling)
+            except AttributeError:
+                GridFunction(grid, func).visualize(grid, filename=filename, subsampling=subsampling)
+
+        prefix = os.path.join(tempfile.gettempdir(), next(tempfile._get_candidate_names()))
+        suffix = 'vt{}'.format('p' if dim_domain == 1 else 'u')
+        if len(functions) == 0:
+            return
+        for ii, func in enumerate(functions):
+            visualize_single(func, f'{prefix}_{ii}')
+        if len(functions) == 1:
+            filename = f'{prefix}_0.{suffix}'
+        else:
+            with open(f'{prefix}.pvd', 'w') as pvd_file:
+                pvd_file.write('<?xml version=\'1.0\'?>\n')
+                pvd_file.write('<VTKFile type=\'Collection\' version=\'0.1\'>\n')
+                pvd_file.write('<Collection>\n')
+                for ii, func in enumerate(functions):
+                    pvd_file.write(
+            f'<DataSet timestep=\'{ii}\' part=\'1\' name=\'{name}\' file=\'{prefix}_{ii}.{suffix}\'/>\n')
+                pvd_file.write('</Collection>\n')
+                pvd_file.write('</VTKFile>\n')
+            filename = f'{prefix}.pvd'
+
+        return plot(filename, color_attribute_name=name, interactive=True)
+
 
     def visualize_function_on_dd_grid(function, dd_grid, subdomains=None):
         assert function.dim_domain == 2, f'Not implemented yet for {function.dim_domain}-dimensional grids!'
@@ -66,7 +96,7 @@ if config.HAVE_K3D:
         assert isinstance(subdomains, list), 'Please provide a list of subdomains'
         assert all(sd < dd_grid.num_subdomains for sd in subdomains)
         assert all(isinstance(sd, int) for sd in subdomains)
-        tmpfile = NamedTemporaryFile(mode='wb', delete=False, suffix='.vtu').name
+        tmpfile = tempfile.NamedTemporaryFile(mode='wb', delete=False, suffix='.vtu').name
         dd_grid.write_global_visualization(tmpfile[5:-4], function, subdomains)
         prestring = f's{dd_grid.num_subdomains:04d}-'
         if len(subdomains) == 1:
